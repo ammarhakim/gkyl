935fbea6cc7806855bcaad7c3058365c7193ae3e
Ammar Hakim <a.hakim777@gmail.com>
2016-06-03 23:33:28 -0700

    Added executable. Does not compile yet, but getting thee

---
+++ exec/gkyl.cxx
@@ -0,0 +1,41 @@
+/** Top-level entry point into Gkyl */
+
+// luajit includes
+#include <lua.hpp>
+
+// std include
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "Usage: gkyl LUA-SCRIPT" << std::endl;
+    return 1;
+  }
+  
+  lua_State *L = luaL_newstate();
+  if (L==NULL)
+  {
+    std::cerr << "Unable to create a new LuaJIT interpreter state. Quitting" << std::endl;
+    return 1;
+  }
+  lua_gc(L, LUA_GCSTOP, 0);  // stop collector during initialization
+  luaL_openlibs(L);  // open libraries
+  lua_gc(L, LUA_GCRESTART, -1);
+
+// run supplied script 
+  if (luaL_loadfile(L, argv[1]) || lua_pcall(L, 0, LUA_MULTRET, 0))
+  { // some error occured
+    const char* ret = lua_tostring(L, -1);
+    std::cerr << "ERROR: " << ret << std::endl;
+    lua_close(L);
+    return 1;
+  }
+
+  const char* ret = lua_tostring(L, -1); // value returned by script
+  std::cout << ret << std::endl;
+  lua_close(L);
+  
+  return 0;
+}
