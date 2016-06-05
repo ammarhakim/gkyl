7315dee76e3726aaa80e516ed3c531023773254e
Ammar <a.hakim777@gmail.com>
2016-06-05 00:05:42 -0700

    More updates to readme and getting close to figuring out Waf

--- install-deps/buildluajit.sh
+++ install-deps/buildluajit.sh
@@ -1,6 +1,6 @@
-# These are the simplest instructions to build LuaJIT 2.1.
+# Simpl instructions to build LuaJIT 2.1
 git clone http://luajit.org/git/luajit-2.0.git
 cd luajit-2.0
 git checkout v2.1
 make
-make install PREFIX=$HOME/gkyldeps/luajit
+make install PREFIX=$HOME/gkylsoft/luajit
