ad7089c8d50efe6922cd5aa1d5f0a8b9a3ef8fa8
Ammar <a.hakim777@gmail.com>
2016-06-13 14:55:36 -0700

    Simplifying Range

--- Lib/Range.lua
+++ Lib/Range.lua
@@ -15,10 +15,19 @@ local new, copy, fill, sizeof, typeof, metatype = xsys.from(ffi,
 -- A range object, representing a ndim integer index set. 
 --------------------------------------------------------------------------------
 
--- Range object
-local function new_Range_ct()
-   local rct = typeof("struct { uint8_t _ndim; int _lower[6]; int _upper[6]; }")
-   local range_mf = {
+-- Range object meta-type
+local range_mt = {
+   __new = function (self, lower, upper)
+      -- lower and upper are tables of integers. Indices are inclusive
+      local r = new(self)
+      r._ndim = #lower
+      for d = 1, #lower do
+	 r._lower[d-1] = lower[d]
+	 r._upper[d-1] = upper[d]
+      end
+      return r
+   end,
+   __index = {
       ndim = function (self)
 	 return self._ndim
       end,
@@ -30,24 +39,13 @@ local function new_Range_ct()
       end,
       shape = function (self, dir)
 	 return self._upper[dir-1]-self._lower[dir-1]+1
-      end,
+      end,      
    }
-   local range_mt = {
-      __new = function (self, lower, upper)
-	 -- lower and upper are tables of integers. Indices are inclusive
-	 local r = new(rct)
-	 r._ndim = #lower
-	 for d = 1, #lower do
-	    r._lower[d-1] = lower[d]
-	    r._upper[d-1] = upper[d]
-	 end
-	 return r
-      end,
-      __index = range_mf
-   }
-   return metatype(rct, range_mt)
-end
+}
+-- construct Range object, attaching meta-type to it
+local rct = typeof("struct { uint8_t _ndim; int _lower[6]; int _upper[6]; }")
+Range = metatype(rct, range_mt)
 
 return {
-   Range = new_Range_ct()
+   Range = Range
 }
