8f054cf4b62729678df992a9554f9c1ed9c3e05d
Ammar Hakim <a.hakim777@gmail.com>
2016-06-15 18:58:31 -0700

    - Added get() method to Field. This takes a linear index into the field - Added tests for seting a field

--- Unit/test_CartField.lua
+++ Unit/test_CartField.lua
@@ -1,6 +1,6 @@
 -- Gkyl ------------------------------------------------------------------------
 --
--- Test for cartesian grid objects
+-- Test for fields on cartesian grids
 --    _______     ___
 -- + 6 @ |||| # P ||| +
 --------------------------------------------------------------------------------
@@ -13,6 +13,47 @@ local assert_equal = Unit.assert_equal
 local stats = Unit.stats
 
 function test_1()
+   local grid = Grid.RectCart {
+      lower = {0.0},
+      upper = {1.0},
+      cells = {10},
+   }
+   local field = DataStruct.Field {
+      onGrid = grid,
+      numComponents = 3,
+      ghost = {1, 1},
+   }
+
+   assert_equal(1, field:ndim(), "Checking dimensions")
+   assert_equal(1, field:lowerGhost(), "Checking lower ghost")
+   assert_equal(1, field:upperGhost(), "Checking upper ghost")
+   assert_equal((10+2)*3, field:size(), "Checking size")
+
+   local localRange = field:localRange()
+   assert_equal(1, localRange:lower(1), "Checking range lower")
+   assert_equal(10, localRange:upper(1), "Checking range upper")
+
+   local localExtRange = field:localExtRange()
+   assert_equal(0, localExtRange:lower(1), "Checking range lower")
+   assert_equal(11, localExtRange:upper(1), "Checking range upper")
+
+   local indexer = field:indexer()
+   for i = localRange:lower(1), localRange:upper(1) do
+      local fitr = field:get(indexer(i))
+      fitr[1] = i+1
+      fitr[2] = i+2
+      fitr[3] = i+3
+   end
+
+   for i = localRange:lower(1), localRange:upper(1) do
+      local fitr = field:get(indexer(i))
+      assert_equal(i+1, fitr[1], "Checking field value")
+      assert_equal(i+2, fitr[2], "Checking field value")
+      assert_equal(i+3, fitr[3], "Checking field value")
+   end
+end
+
+function test_2()
    local grid = Grid.RectCart {
       lower = {0.0, 0.0},
       upper = {1.0, 1.0},
@@ -27,6 +68,7 @@ function test_1()
    assert_equal(2, field:ndim(), "Checking dimensions")
    assert_equal(1, field:lowerGhost(), "Checking lower ghost")
    assert_equal(2, field:upperGhost(), "Checking upper ghost")
+   assert_equal((10+3)*(10+3)*3, field:size(), "Checking size")
 
    local localRange = field:localRange()
    assert_equal(1, localRange:lower(1), "Checking range lower")
@@ -35,15 +77,35 @@ function test_1()
    assert_equal(10, localRange:upper(1), "Checking range upper")
    assert_equal(10, localRange:upper(2), "Checking range upper")
 
-   local localExtRange = field:localExtRange(1, 1)
+   local localExtRange = field:localExtRange()
    assert_equal(0, localExtRange:lower(1), "Checking range lower")
    assert_equal(0, localExtRange:lower(2), "Checking range lower")
 
    assert_equal(12, localExtRange:upper(1), "Checking range upper")
    assert_equal(12, localExtRange:upper(2), "Checking range upper")
+
+   local indexer = field:indexer()
+   for i = localRange:lower(1), localRange:upper(1) do
+      for j = localRange:lower(2), localRange:upper(2) do
+	 local fitr = field:get(indexer(i,j))
+	 fitr[1] = i+2*j+1
+	 fitr[2] = i+2*j+2
+	 fitr[3] = i+2*j+3
+      end
+   end
+
+   for i = localRange:lower(1), localRange:upper(1) do
+      for j = localRange:lower(2), localRange:upper(2) do
+	 local fitr = field:get(indexer(i,j))
+	 assert_equal(i+2*j+1, fitr[1], "Checking field value")
+	 assert_equal(i+2*j+2, fitr[2], "Checking field value")
+	 assert_equal(i+2*j+3, fitr[3], "Checking field value")
+      end
+   end
 end
 
 test_1()
+test_2()
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
