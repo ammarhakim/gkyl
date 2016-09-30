5486e9caa7fef25e83a2cc8f41996481161ef31f
Ammar Hakim <a.hakim777@gmail.com>
2016-09-30 00:14:53 -0700

    Added MPI_Datatype wrappers

--- Comm/GkMpiMacros.h
+++ Comm/GkMpiMacros.h
@@ -11,4 +11,6 @@
 #define GET_MPI_OBJECT(type, value) MPI_##type get_##value() { return value; }
 #define DECL_GET_MPI_OBJECT(type, value) MPI_##type get_##value()
 
+#define LUA_SET_MPI_OBJECT(type, value) _M.##value = ffi.C.get_##value()
+
 #endif // GK_MPI_MACROS_H
