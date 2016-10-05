bd80282d6a6e9d5c4da37f643fde186f4673948b
Ammar Hakim <a.hakim777@gmail.com>
2016-10-04 21:11:58 -0700

    Updated ADIOS build instructions

--- install-deps/build-adios.sh
+++ install-deps/build-adios.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# Edit to suite your system
+PREFIX=$HOME/gkylsoft/adios
+# You may have to explicitly set the compilers to use, in which case,
+# uncomment the following lines
+#CC=gcc
+#CXX=g++
+#MPICC=mpicc
+#MPICXX=mpicxx
+
 wget http://users.nccs.gov/~pnorbert/adios-1.10.0.tar.gz
 gunzip adios-1.10.0.tar.gz
 tar -xvf adios-1.10.0.tar
 cd adios-1.10.0
-./configure --prefix=$HOME/gkylsoft/adios --disable-fortran
+./configure --prefix=$PREFIX --disable-fortran
 make install
