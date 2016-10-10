89703d3d0f6c59352c21f59205bf2a5c2f62f36d
Ammar Hakim <a.hakim777@gmail.com>
2016-10-09 19:49:44 -0700

    - Hooked parallel decomp into field - Added parallel field test

---
+++ Unit/test_ParCartField.lua
@@ -0,0 +1,166 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Test for fields on cartesian grids
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+local ffi = require "ffi"
+local Unit = require "Unit"
+local Grid = require "Grid"
+local DecompRegionCalc = require "Lib.CartDecomp"
+local DataStruct = require "DataStruct"
+local Mpi = require "Comm.Mpi"
+
+local ffi  = require "ffi"
+local xsys = require "xsys"
+local new, copy, fill, sizeof, typeof, metatype = xsys.from(ffi,
+     "new, copy, fill, sizeof, typeof, metatype")
+
+local assert_equal = Unit.assert_equal
+local stats = Unit.stats
+
+function log(msg)
+   local rank = Mpi.Comm_rank(Mpi.COMM_WORLD)
+   if rank == 0 then
+      print(msg)
+   end
+end
+
+function test_1()
+   local nz = Mpi.Comm_size(Mpi.COMM_WORLD)
+   if nz ~= 2 then
+      log("Not running test_1 as numProcs not exactly 2")
+      return
+   end
+   local rank = Mpi.Comm_rank(Mpi.COMM_WORLD)
+   
+   local decomp = DecompRegionCalc.CartProd { cuts = {2} }
+   local grid = Grid.RectCart {
+      lower = {0.0},
+      upper = {1.0},
+      cells = {10},
+      decomposition = decomp,
+   }
+   local field = DataStruct.Field {
+      onGrid = grid,
+      numComponents = 3,
+      ghost = {1, 1},
+   }
+
+   assert_equal(1, field:ndim(), "Checking dimensions")
+   assert_equal(1, field:lowerGhost(), "Checking lower ghost")
+   assert_equal(1, field:upperGhost(), "Checking upper ghost")
+   --assert_equal((10+2)*3, field:size(), "Checking size")
+
+   assert_equal("col-major", field:layout(), "Checking layout")
+
+   local localRange = field:localRange()
+   if rank == 0 then
+      assert_equal(1, localRange:lower(1), "Checking range lower")
+      assert_equal(5, localRange:upper(1), "Checking range upper")
+   elseif rank == 1 then
+      assert_equal(6, localRange:lower(1), "Checking range lower")
+      assert_equal(10, localRange:upper(1), "Checking range upper")
+   end
+
+   local localExtRange = field:localExtRange()
+   if rank == 0 then
+      assert_equal(0, localExtRange:lower(1), "Checking ext range lower")
+      assert_equal(6, localExtRange:upper(1), "Checking ext range upper")
+   elseif rank == 1 then
+      assert_equal(5, localExtRange:lower(1), "Checking ext range lower")
+      assert_equal(11, localExtRange:upper(1), "Checking ext range upper")
+   end
+
+   local indexer = field:indexer()
+   for i = localRange:lower(1), localRange:upper(1) do
+      local fitr = field:get(indexer(i))
+      fitr[1] = i+1
+      fitr[2] = i+2
+      fitr[3] = i+3
+   end
+
+   for i = localRange:lower(1), localRange:upper(1) do
+      local fitr = field:get(indexer(i))
+      assert_equal(i+1, fitr[1], "Checking field value")
+      assert_equal(i+2, fitr[2], "Checking field value")
+      assert_equal(i+3, fitr[3], "Checking field value")
+   end
+end
+
+function test_2()
+   local nz = Mpi.Comm_size(Mpi.COMM_WORLD)
+   if nz ~= 4 then
+      log("Not running test_2 as numProcs not exactly 4")
+      return
+   end
+   local rank = Mpi.Comm_rank(Mpi.COMM_WORLD)
+
+   local decomp = DecompRegionCalc.CartProd { cuts = {2,2} }
+   local grid = Grid.RectCart {
+      lower = {0.0, 0.0},
+      upper = {1.0, 1.0},
+      cells = {10, 10},
+      decomposition = decomp,
+   }
+   local field = DataStruct.Field {
+      onGrid = grid,
+      numComponents = 3,
+      ghost = {1, 2},
+   }
+
+   assert_equal(2, field:ndim(), "Checking dimensions")
+   assert_equal(1, field:lowerGhost(), "Checking lower ghost")
+   assert_equal(2, field:upperGhost(), "Checking upper ghost")
+   --assert_equal((10+3)*(10+3)*3, field:size(), "Checking size")
+
+   assert_equal("col-major", field:layout(), "Checking layout")
+
+   local localRange = field:localRange()
+   local globalRange = field:globalRange()
+   local localExtRange = field:localExtRange()
+
+   assert_equal(100, globalRange:volume(), "Testing global volume")
+   assert_equal(25, localRange:volume(), "Testing local volume")
+   assert_equal(64, localExtRange:volume(), "Testing local ext volume")
+
+   local indexer = field:indexer()
+   for i = localRange:lower(1), localRange:upper(1) do
+      for j = localRange:lower(2), localRange:upper(2) do
+	 local fitr = field:get(indexer(i,j))
+	 fitr[1] = i+2*j+1
+	 fitr[2] = i+2*j+2
+	 fitr[3] = i+2*j+3
+      end
+   end
+
+   for i = localRange:lower(1), localRange:upper(1) do
+      for j = localRange:lower(2), localRange:upper(2) do
+	 local fitr = field:get(indexer(i,j))
+	 assert_equal(i+2*j+1, fitr[1], "Checking field value")
+	 assert_equal(i+2*j+2, fitr[2], "Checking field value")
+	 assert_equal(i+2*j+3, fitr[3], "Checking field value")
+      end
+   end
+end
+
+test_1()
+test_2()
+
+function allReduceOneInt(localv)
+   local sendbuf, recvbuf = new("int[1]"), new("int[1]")
+   sendbuf[0] = localv
+   Mpi.Allreduce(sendbuf, recvbuf, 1, Mpi.INT, Mpi.SUM, Mpi.COMM_WORLD)
+   return recvbuf[0]
+end
+
+totalFail = allReduceOneInt(stats.fail)
+totalPass = allReduceOneInt(stats.pass)
+
+if totalFail > 0 then
+   log(string.format("\nPASSED %d tests", totalPass))   
+   log(string.format("**** FAILED %d tests", totalFail))
+else
+   log(string.format("PASSED ALL %d tests!", totalPass))
+end
