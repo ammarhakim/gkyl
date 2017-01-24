4f053863b2f058ea43e258a5967588e585135d53
ammarhakim <a.hakim777@gmail.com>
2017-01-24 09:54:33 -0500

    - Added skeletons for modal Serendipity and Max-Order basis - Added code to computer "moment matrices"

---
+++ Basis/CartModalSerendipity.lua
@@ -0,0 +1,12 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Modal Serendipity elements on Cartesian meshes.
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+-- system libraries
+local ffi = require "ffi"
+local xsys = require "xsys"
+local new, copy, fill, sizeof, typeof, metatype = xsys.from(ffi,
+     "new, copy, fill, sizeof, typeof, metatype")
