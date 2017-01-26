25fcd2a35231875cbc56fdfafcd1396d16cbee2e
Ammar <a.hakim777@gmail.com>
2017-01-25 22:28:52 -0500

    Added bunch of tests for quadrature scheme

--- count-lines.sh
+++ count-lines.sh
@@ -1,3 +1,3 @@
-cmd="wc -l gkyl.cxx DataStruct/*.lua Eq/*.lua Grid/*.lua Lib/*.lua Updater/*.lua Comm/*.lua Comm/*.h Comm/*.cpp Io/*.lua Basis/*.lua Basis/_data/*.lua"
+cmd="wc -l gkyl.cxx DataStruct/*.lua Eq/*.lua Grid/*.lua Lib/*.lua Updater/*.lua Comm/*.lua Comm/*.h Comm/*.cpp Io/*.lua Basis/*.lua"
 echo $cmd
 $cmd
