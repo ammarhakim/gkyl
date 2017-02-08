02840cbe20a57d5c531b03c84cbcb97247d760a9
ammarhakim <a.hakim777@gmail.com>
2017-02-07 20:28:12 -0500

    - Added CAS scripts to generated pressure and energy moments code - Added generated Lua code

---
+++ Unit/test_DistFuncMomentCalc.lua
@@ -0,0 +1,116 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Test for updater to compute moments
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+local Unit = require "Unit"
+local Grid = require "Grid"
+local DataStruct = require "DataStruct"
+local Basis = require "Basis"
+local Updater = require "Updater"
+local Lin = require "Lib.Linalg"
+
+local assert_equal = Unit.assert_equal
+local stats = Unit.stats
+
+function test_ser_1x1v()
+   -- phase-space and config-space grids
+   local phaseGrid = Grid.RectCart {
+      lower = {0.0, -6.0},
+      upper = {1.0, 6.0},
+      cells = {1, 16},
+   }
+   local confGrid = Grid.RectCart {
+      lower = { phaseGrid:lower(1) },
+      upper = { phaseGrid:upper(1) },
+      cells = { phaseGrid:numCells(1) },
+   }
+   -- basis functions
+   local phaseBasis = Basis.CartModalSerendipity { ndim = 2, polyOrder = 2 }
+   local confBasis = Basis.CartModalSerendipity { ndim = 1, polyOrder = 2 }
+   -- fields
+   local distf = DataStruct.Field {
+      onGrid = phaseGrid,
+      numComponents = phaseBasis:numBasis(),
+      ghost = {0, 0},
+   }
+   local numDensity = DataStruct.Field {
+      onGrid = confGrid,
+      numComponents = confBasis:numBasis(),
+      ghost = {0, 0},
+   }
+
+   -- updater to initialize distribution function
+   local project = Updater.ProjectOnBasis {
+      onGrid = phaseGrid,
+      basis = phaseBasis,
+      evaluate = function (t, xn)
+	 return 1/(phaseGrid:upper(2)-phaseGrid:lower(2))
+      end
+   }
+   project:advance(0.0, 0.0, {}, {distf})
+   distf:write("distf.bp", 0.0)
+
+   -- moment updater
+   local calcNumDensity = Updater.DistFuncMomentCalc {
+      onGrid = phaseGrid,
+      phaseBasis = phaseBasis,
+      confBasis = confBasis,
+      moment = "number-density",
+   }
+   calcNumDensity:advance(0.0, 0.0, {distf}, {numDensity})
+
+   local momIdxr = numDensity:genIndexer()
+   local nItr = numDensity:get(momIdxr( {1} ))
+   assert_equal(1, nItr[1]/math.sqrt(2), "Checking moment")
+   
+end
+
+function test_max_1x1v()
+   -- phase-space and config-space grids
+   local phaseGrid = Grid.RectCart {
+      lower = {0.0, -6.0},
+      upper = {1.0, 6.0},
+      cells = {1, 16},
+   }
+   local confGrid = Grid.RectCart {
+      lower = { phaseGrid:lower(1) },
+      upper = { phaseGrid:upper(1) },
+      cells = { phaseGrid:numCells(1) },
+   }
+   -- basis functions
+   local phaseBasis = Basis.CartModalMaxOrder { ndim = 2, polyOrder = 2 }
+   local confBasis = Basis.CartModalMaxOrder { ndim = 1, polyOrder = 2 }
+   -- fields
+   local distf = DataStruct.Field {
+      onGrid = phaseGrid,
+      numComponents = phaseBasis:numBasis(),
+      ghost = {0, 0},
+   }
+   local numDensity = DataStruct.Field {
+      onGrid = confGrid,
+      numComponents = confBasis:numBasis(),
+      ghost = {0, 0},
+   }
+
+   -- moment updater
+   local calcNumDensity = Updater.DistFuncMomentCalc {
+      onGrid = phaseGrid,
+      phaseBasis = phaseBasis,
+      confBasis = confBasis,
+      moment = "number-density",
+   }
+   
+end
+
+test_ser_1x1v()
+test_max_1x1v()
+
+if stats.fail > 0 then
+   print(string.format("\nPASSED %d tests", stats.pass))
+   print(string.format("**** FAILED %d tests", stats.fail))
+else
+   print(string.format("PASSED ALL %d tests!", stats.pass))
+end
---
+++ count-all-lines.sh
@@ -0,0 +1,3 @@
+cmd="wc -l gkyl.cxx DataStruct/*.lua Eq/*.lua Grid/*.lua Lib/*.lua Updater/*.lua Updater/*/*.lua Comm/*.lua Comm/*.h Comm/*.cpp Io/*.lua Basis/*.lua Basis/*/*.lua"
+echo $cmd
+$cmd
