ecdc44e04160cb654e359ad051c9ddad3e3d20d7
Ammar <a.hakim777@gmail.com>
2017-05-02 21:57:18 -0400

    - 10M flux function tested. - Corresponding tests added
    Need to test RP solver now

--- Unit/test_TenMoment.lua
+++ Unit/test_TenMoment.lua
@@ -19,9 +19,60 @@ function test_1()
    assert_equal(10, tenMoment:numEquations(), "No of equations")
    assert_equal(5, tenMoment:numWaves(), "No of waves")
 
+   local rho, u, v, w, pxx, pxy, pxz, pyy, pyz, pzz = 1.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0
+   local q = {rho, rho*u, rho*v, rho*w, pxx+rho*u*u, pxy+rho*u*v, pxz+rho*u*w, pyy+rho*v*v, pyz+rho*v*w, pzz+rho*w*w}
+   local flux = Lin.Vec(10)
+   tenMoment:flux(1, q, flux)
+
+   local myFlux = {
+      rho*u,
+      rho*u^2 + pxx,
+      rho*u*v + pxy,
+      rho*u*w + pxz,
+      rho*u^3 + 3*u*pxx,
+      rho*u^2*v + 2*u*pxy + v*pxx,
+      rho*u^2*w + 2*u*pxz + w*pxx,
+      rho*u*v^2 + u*pyy + 2*v*pxy,
+      rho*u*v*w + u*pyz + v*pxz + w*pxy,
+      rho*u*w^2 + u*pzz + 2*w*pxz
+   }
+
+   -- check fluxes
+   for i = 1, 10 do
+      assert_equal(myFlux[i], flux[i], "Checking flux")
+   end
+
+   local rho, u, v, w, pxx, pxy, pxz, pyy, pyz, pzz = 1.0, 0.5, 0.6, 0.7, 1.0, 0.1, 0.2, 2.0, 0.1, 3.0
+   local q = {rho, rho*u, rho*v, rho*w, pxx+rho*u*u, pxy+rho*u*v, pxz+rho*u*w, pyy+rho*v*v, pyz+rho*v*w, pzz+rho*w*w}
+   local flux = Lin.Vec(10)
+   tenMoment:flux(1, q, flux)
+
+   local myFlux = {
+      rho*u,
+      rho*u^2 + pxx,
+      rho*u*v + pxy,
+      rho*u*w + pxz,
+      rho*u^3 + 3*u*pxx,
+      rho*u^2*v + 2*u*pxy + v*pxx,
+      rho*u^2*w + 2*u*pxz + w*pxx,
+      rho*u*v^2 + u*pyy + 2*v*pxy,
+      rho*u*v*w + u*pyz + v*pxz + w*pxy,
+      rho*u*w^2 + u*pzz + 2*w*pxz
+   }
+
+   -- check fluxes
+   for i = 1, 10 do
+      assert_equal(myFlux[i], flux[i], "Checking flux")
+   end   
+   
+end
+
+function test_2()
+   
 end
 
 test_1()
+test_2()
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
