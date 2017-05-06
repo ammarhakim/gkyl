b709568328f578504eb420ab25ef474df0c9f886
ammarhakim <a.hakim777@gmail.com>
2017-05-05 23:01:55 -0400

    - Ten-moment RP is now in C++. This speeds up the FV solver bigly - Added tests and CAS scripts used to generate the code

---
+++ Eq/TenMomentRp.h
@@ -0,0 +1,9 @@
+#ifndef HAVE_TENMOM_RP_H
+#define HAVE_TENMOM_RP_H
+
+extern "C" {
+    void gkylTenMomentRp(int dir, double delta[10], double ql[10], double qr[10], double *waves, double s[5]);
+    void gkylTenMomentQFluct(double *waves, double *s, double *amdq, double *apdq);
+}
+
+#endif // HAVE_TENMOM_RP_H
