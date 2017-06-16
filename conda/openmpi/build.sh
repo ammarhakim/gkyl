a119b1e542b449d5ff89446414eb95eddfc1861d
ammarhakim <a.hakim777@gmail.com>
2017-06-16 14:40:31 -0400

    Using curl instead of wget

--- conda/openmpi/build.sh
+++ conda/openmpi/build.sh
@@ -1,5 +1,5 @@
-wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz
+curl https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz > openmpi-2.1.1.tar.gz
 gunzip -c openmpi-2.1.1.tar.gz | tar xf -
 cd openmpi-2.1.1
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX 
 make all install
