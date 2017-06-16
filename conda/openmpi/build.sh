3d1fcfa7aa92622d10a0c33f9653098c5684ab6d
Petr Cagas <pcagas@vt.edu>
2017-06-16 10:47:18 -0400

    conda build with own openmpi

---
+++ conda/openmpi/build.sh
@@ -0,0 +1,5 @@
+wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz
+gunzip -c openmpi-2.1.1.tar.gz | tar xf -
+cd openmpi-2.1.1
+./configure --prefix=$PREFIX
+make all install
