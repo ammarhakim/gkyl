f96665c4e70e517896de5f8367fe85bd7a1011de
Petr Cagas <pcagas@vt.edu>
2017-06-29 09:48:33 -0400

    patching the Conda build of the ADIOS wrapper #4

--- conda/adiospy/build.sh
+++ conda/adiospy/build.sh
@@ -3,6 +3,6 @@ gunzip adios-1.11.0.tar.gz
 tar -xvf adios-1.11.0.tar
 cd adios-1.11.0/wrappers/numpy
 # removing '-rlt' from one line in the Makefile that breaks the build on Macs
-sed -e '33s/.*/\tpython setup.py build_ext/' -i ''  Makefile 
+sed 's/ -lrt//g' Makefile -i
 make python
 $PYTHON setup.py install --prefix=$PREFIX
