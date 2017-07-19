54cc59cdfa1bda9c6516f5448621ae7c7f0b6ccd
Ammar <a.hakim777@gmail.com>
2017-07-19 14:43:10 -0400

    Added accumulate/combine methods to CartField; optimized

---
+++ DataStruct/CartFieldImpl.cpp
@@ -0,0 +1,23 @@
+// Gkyl ------------------------------------------------------------------------
+//
+// C++ back-end for CartField
+//    _______     ___
+// + 6 @ |||| # P ||| +
+//------------------------------------------------------------------------------
+
+#include <cmath>
+#include <CartFieldImpl.h>
+
+void gkylCartFieldAccumulate(unsigned nv, double fact, const double *inp, double *out)
+{
+  for (unsigned n=0; n<nv; ++n)
+    out[n] += fact*inp[n];
+}
+
+void gkylCartFieldAssign(unsigned nv, double fact, const double *inp, double *out)
+{
+  for (unsigned n=0; n<nv; ++n)
+    out[n] = fact*inp[n];
+}
+
+
