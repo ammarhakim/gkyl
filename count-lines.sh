b1ee26d71812a99ddc7fdbcadc5655b061ab5e01
Ammar <a.hakim777@gmail.com>
2017-11-13 12:11:47 -0500

    - Added diagnostics to HyperEqnOnCartGrid sim object. Hooked in   diagnostics into input files for testing. Not 100% working yet

--- count-lines.sh
+++ count-lines.sh
@@ -1,3 +1,3 @@
-cmd="wc -l gkyl.cxx Sim/*.lua DataStruct/*.lua DataStruct/*.h DataStruct/*.cpp Eq/*.lua Eq/*.h Eq/*.cpp Grid/*.lua Lib/*.lua Updater/*.lua Comm/*.lua Comm/*.h Comm/*.cpp Io/*.lua Basis/*.lua"
+cmd="wc -l gkylx.cxx Sim/*.lua DataStruct/*.lua DataStruct/*.h DataStruct/*.cpp Eq/*.lua Eq/*.h Eq/*.cpp Grid/*.lua Lib/*.lua Updater/*.lua Comm/*.lua Comm/*.h Comm/*.cpp Io/*.lua Basis/*.lua"
 echo $cmd
 $cmd
