af266a87d2e26d18bca2742ba4f9e15c1a451746
Petr Cagas <pcagas@vt.edu>
2017-11-14 11:01:49 -0500

    conda build

--- conda/openmpi/build.sh
+++ conda/openmpi/build.sh
@@ -1,5 +1,5 @@
-curl https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz > openmpi-2.1.1.tar.gz
-gunzip -c openmpi-2.1.1.tar.gz | tar xf -
-cd openmpi-2.1.1
+curl https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz > openmpi-3.0.0.tar.gz
+gunzip -c openmpi-3.0.0.tar.gz | tar xf -
+cd openmpi-3.0.0
 ./configure --prefix=$PREFIX --enable-mpi-fortran=none
 make all install
