02ba359da738fdbc582fc27e614a0871ec2573a9
Ammar <a.hakim777@gmail.com>
2017-11-14 11:32:36 -0500

    Removing the -v from the build

--- conda/gkyl/build.sh
+++ conda/gkyl/build.sh
@@ -1,4 +1,3 @@
-mpiexec --version
 # Build directory
 OUT=build-par
 
@@ -22,6 +21,6 @@ ADIOS_INC_DIR=$PREFIX/include
 ADIOS_LIB_DIR=$PREFIX/lib
 
 # You probably do not need to modify the command itself
-cmd="./waf --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure -v build -v install"
+cmd="./waf --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure build install"
 echo $cmd
 $cmd
