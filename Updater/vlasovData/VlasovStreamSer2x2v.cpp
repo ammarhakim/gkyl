d03788f77f9948a13bbd540df61f372c54e551ac
Ammar <a.hakim777@gmail.com>
2017-12-22 22:49:45 -0500

    Making volume terms more systematic so as to avoid repetition

--- Updater/vlasovData/VlasovStreamSer2x2v.cpp
+++ Updater/vlasovData/VlasovStreamSer2x2v.cpp
@@ -2,11 +2,10 @@
 void VlasovVolStream2x2vSerP1(const double *w, const double *dxv, const double *f, double *out) 
 { 
 // w[NDIM]: Cell-center coordinates. dxv[NDIM]: Cell spacing. f: Input distribution function. out: Incremented output 
-  const unsigned int X=0, Y=1, VX=2, VY=3; 
-  double dv0dx0 = dxv[VX]/dxv[X]; 
-  double dv1dx1 = dxv[VY]/dxv[Y]; 
-  double w0dx0 = w[VX]/dxv[X]; 
-  double w1dx1 = w[VY]/dxv[Y]; 
+  double dv0dx0 = dxv[2]/dxv[0]; 
+  double w0dx0 = w[2]/dxv[0]; 
+  double dv1dx1 = dxv[3]/dxv[1]; 
+  double w1dx1 = w[3]/dxv[1]; 
   out[1] += 3.464101615137754*f[0]*w0dx0+f[3]*dv0dx0; 
   out[2] += 3.464101615137754*f[0]*w1dx1+f[4]*dv1dx1; 
   out[5] += 3.464101615137754*f[1]*w1dx1+3.464101615137754*f[2]*w0dx0+f[8]*dv1dx1+f[7]*dv0dx0; 
@@ -23,11 +22,10 @@ void VlasovVolStream2x2vSerP1(const double *w, const double *dxv, const double *
 void VlasovVolStream2x2vSerP2(const double *w, const double *dxv, const double *f, double *out) 
 { 
 // w[NDIM]: Cell-center coordinates. dxv[NDIM]: Cell spacing. f: Input distribution function. out: Incremented output 
-  const unsigned int X=0, Y=1, VX=2, VY=3; 
-  double dv0dx0 = dxv[VX]/dxv[X]; 
-  double dv1dx1 = dxv[VY]/dxv[Y]; 
-  double w0dx0 = w[VX]/dxv[X]; 
-  double w1dx1 = w[VY]/dxv[Y]; 
+  double dv0dx0 = dxv[2]/dxv[0]; 
+  double w0dx0 = w[2]/dxv[0]; 
+  double dv1dx1 = dxv[3]/dxv[1]; 
+  double w1dx1 = w[3]/dxv[1]; 
   out[1] += 3.464101615137754*f[0]*w0dx0+f[3]*dv0dx0; 
   out[2] += 3.464101615137754*f[0]*w1dx1+f[4]*dv1dx1; 
   out[5] += 3.464101615137754*f[1]*w1dx1+3.464101615137754*f[2]*w0dx0+f[8]*dv1dx1+f[7]*dv0dx0; 
