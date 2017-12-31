0645e2231b4f3b0ba7e86c0d109ee010fff5ebcd
ammarhakim <a.hakim777@gmail.com>
2017-12-31 00:24:21 -0500

    - Electric field acceleration terms recomputed. Perhaps more   optimizations can be done later - Added pre-computed basis as lisp functions. This allows running   various code generation functions very quickly as the most expensive   part is computing the basis functions themselves.

---
+++ cas-scripts/basis-precalc/basisSer1x1v.lisp
@@ -0,0 +1,95 @@
+;;; -*- Mode: LISP; package:maxima; syntax:common-lisp; -*- 
+(in-package :maxima)
+(DSKSETQ |$varsC| '((MLIST SIMP) $X)) 
+(ADD2LNC '|$varsC| $VALUES) 
+(DSKSETQ |$varsP| '((MLIST SIMP) $X $VX)) 
+(ADD2LNC '|$varsP| $VALUES) 
+(DSKSETQ |$basisC|
+         '((MLIST SIMP
+            (10.
+             "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/basis-precalc/basis-pre-calc.mac"
+             SRC |$writeBasisToFile| 7.))
+           ((MLIST SIMP
+             (31.
+              "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+              SRC |$gsOrthoNorm| 29.))
+            ((MEXPT SIMP) 2. ((RAT SIMP) -1. 2.))
+            ((MTIMES SIMP) ((MEXPT SIMP) 2. ((RAT SIMP) -1. 2.))
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $X))
+           ((MLIST SIMP
+             (31.
+              "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+              SRC |$gsOrthoNorm| 29.))
+            ((MEXPT SIMP) 2. ((RAT SIMP) -1. 2.))
+            ((MTIMES SIMP) ((MEXPT SIMP) 2. ((RAT SIMP) -1. 2.))
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $X)
+            ((MTIMES SIMP) 3. ((MEXPT SIMP) 2. ((RAT SIMP) -3. 2.))
+             ((MEXPT SIMP) 5. ((RAT SIMP) 1. 2.))
+             ((MPLUS SIMP) ((RAT SIMP) -1. 3.)
+              ((MEXPT SIMP
+                (61.
+                 "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+                 SRC |$calcPowers| 61.))
+               $X 2.)))))) 
+(ADD2LNC '|$basisC| $VALUES) 
+(DSKSETQ |$basisP|
+         '((MLIST SIMP
+            (10.
+             "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/basis-precalc/basis-pre-calc.mac"
+             SRC |$writeBasisToFile| 7.))
+           ((MLIST SIMP
+             (31.
+              "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+              SRC |$gsOrthoNorm| 29.))
+            ((RAT SIMP) 1. 2.)
+            ((MTIMES SIMP) ((RAT SIMP) 1. 2.)
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $X)
+            ((MTIMES SIMP) ((RAT SIMP) 1. 2.)
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $VX)
+            ((MTIMES SIMP) ((RAT SIMP) 3. 2.) $VX $X))
+           ((MLIST SIMP
+             (31.
+              "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+              SRC |$gsOrthoNorm| 29.))
+            ((RAT SIMP) 1. 2.)
+            ((MTIMES SIMP) ((RAT SIMP) 1. 2.)
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $X)
+            ((MTIMES SIMP) ((RAT SIMP) 1. 2.)
+             ((MEXPT SIMP) 3. ((RAT SIMP) 1. 2.)) $VX)
+            ((MTIMES SIMP) ((RAT SIMP) 3. 2.) $VX $X)
+            ((MTIMES SIMP) ((RAT SIMP) 3. 4.)
+             ((MEXPT SIMP) 5. ((RAT SIMP) 1. 2.))
+             ((MPLUS SIMP) ((RAT SIMP) -1. 3.)
+              ((MEXPT SIMP
+                (61.
+                 "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+                 SRC |$calcPowers| 61.))
+               $X 2.)))
+            ((MTIMES SIMP) ((RAT SIMP) 3. 4.)
+             ((MEXPT SIMP) 5. ((RAT SIMP) 1. 2.))
+             ((MPLUS SIMP) ((RAT SIMP) -1. 3.)
+              ((MEXPT SIMP
+                (61.
+                 "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+                 SRC |$calcPowers| 61.))
+               $VX 2.)))
+            ((MTIMES SIMP) ((RAT SIMP) 3. 4.)
+             ((MEXPT SIMP) 15. ((RAT SIMP) 1. 2.))
+             ((MPLUS SIMP) ((MTIMES SIMP) ((RAT SIMP) -1. 3.) $VX)
+              ((MTIMES SIMP) $VX
+               ((MEXPT SIMP
+                 (61.
+                  "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+                  SRC |$calcPowers| 61.))
+                $X 2.))))
+            ((MTIMES SIMP) ((RAT SIMP) 3. 4.)
+             ((MEXPT SIMP) 15. ((RAT SIMP) 1. 2.))
+             ((MPLUS SIMP) ((MTIMES SIMP) ((RAT SIMP) -1. 3.) $X)
+              ((MTIMES SIMP)
+               ((MEXPT SIMP
+                 (61.
+                  "/Users/ahakim/research/gkyl-project/gkyl/cas-scripts/modal-basis.mac"
+                  SRC |$calcPowers| 61.))
+                $VX 2.)
+               $X)))))) 
+(ADD2LNC '|$basisP| $VALUES) 
