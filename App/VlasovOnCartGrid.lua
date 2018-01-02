bd96d97792c7ec9808e081bc7a1ea99a06e2acbf
ammarhakim <a.hakim777@gmail.com>
2018-01-02 12:32:56 -0500

    More work on Vlasov app and updater

--- App/VlasovOnCartGrid.lua
+++ App/VlasovOnCartGrid.lua
@@ -19,13 +19,15 @@ local Mpi = require "Comm.Mpi"
 local Time = require "Lib.Time"
 local Updater = require "Updater"
 local date = require "Lib.date"
+local xsys = require "xsys"
 
-local function showTbl(nm, tbl)
-   io.write(string.format("--- %s ---\n", nm))
-   for _, v in ipairs(tbl) do
-      io.write(v); io.write(" ")
-   end
-   io.write("\n---\n")
+-- function to create basis functions
+local function createBasis(nm, ndim, polyOrder)
+   if nm == "serendipity" then
+      return Basis.CartModalSerendipity { ndim = ndim, polyOrder = polyOrder }
+   elseif nm == "maximal-order" then
+      return Basis.CartModalMaxOrder { ndim = ndim, polyOrder = polyOrder }
+   end   
 end
 
 -- For returning module table
@@ -33,7 +35,6 @@ local M = {}
 
 -- Class to store species-specific info
 local Species = {}
-
 function Species:new(tbl)
    local self = setmetatable({}, Species)
    self.type = "species" -- to identify objects of this (Species) type
@@ -45,6 +46,9 @@ function Species:new(tbl)
    self.lower, self.upper = tbl.lower, tbl.upper
    self.cells = tbl.cells
    self.vdim = #self.cells -- velocity dimensions
+   self.ioMethod = "MPI"
+   self.evolve = xsys.pickBool(tbl.evolve, true) -- by default, evolve species
+   self.confBasis = nil -- Will be set later
 
    assert(#self.lower == self.vdim, "'lower' must have " .. self.vdim .. " entries")
    assert(#self.upper == self.vdim, "'upper' must have " .. self.vdim .. " entries")
@@ -78,6 +82,12 @@ Species.__index = {
    setCfl = function(self, cfl)
       self.cfl = cfl
    end,
+   setIoMethod = function(self, ioMethod)
+      self.ioMethod = ioMethod
+   end,
+   setConfBasis = function (self, basis)
+      self.confBasis = basis
+   end,   
    createGrid = function(self, cLo, cUp, cCells, cDecompCuts, cPeriodicDirs)
       self.cdim = #cCells
       self.ndim = self.cdim+self.vdim
@@ -112,13 +122,7 @@ Species.__index = {
       }
    end,
    createBasis = function(self, nm, polyOrder)
-      if nm == "serendipity" then
-	 self.basis = Basis.CartModalSerendipity { ndim = self.ndim, polyOrder = polyOrder }
-	 self.confBasis = Basis.CartModalSerendipity { ndim = self.cdim, polyOrder = polyOrder }
-      elseif nm == "maximal-order" then
-	 self.basis = Basis.CartModalMaxOrder { ndim = self.ndim, polyOrder = polyOrder }
-	 self.confBasis = Basis.CartModalMaxOrder { ndim = self.cdim, polyOrder = polyOrder }
-      end
+      self.basis = createBasis(nm, self.ndim, polyOrder)
    end,
    alloc = function(self)
       -- allocate fields needed in RK update
@@ -130,6 +134,13 @@ Species.__index = {
 	    ghost = {1, 1}
 	 }
       end
+      -- create Adios object for field I/O
+      self.distIo = AdiosCartFieldIo {
+	 elemType = self.distf[1]:elemType(),
+	 method = self.ioMethod,
+      }
+   end,
+   createSolver = function (self, hasE, hasB)
       -- create updater to advance solution by one time-step
       self.vlasovSlvr = Updater.VlasovDisCont {
 	 onGrid = self.grid,
@@ -138,13 +149,8 @@ Species.__index = {
 	 charge = self.charge,
 	 mass = self.mass,
 	 cfl = self.cfl,
-	 hasElectricField = false,
-	 hasMagneticField = false,
-      }
-      
-      -- create Adios object for field I/O
-      self.distIo = AdiosCartFieldIo {
-	 elemType = self.distf[1]:elemType()
+	 hasElectricField = hasE,
+	 hasMagneticField = hasB,
       }
    end,
    initDist = function(self)
@@ -156,13 +162,25 @@ Species.__index = {
       project:advance(0.0, 0.0, {}, {self.distf[1]})
    end,
    write = function(self, frame, tm)
-      self.distIo:write(self.distf[1], string.format("%s_%d.bp", self.name, frame), tm)
+      if self.evolve then
+	 self.distIo:write(self.distf[1], string.format("%s_%d.bp", self.name, frame), tm)
+      else
+	 -- if not evolving species, don't write anything except initial conditions
+	 if frame == 0 then
+	    self.distIo:write(self.distf[1], string.format("%s_%d.bp", self.name, frame), tm)
+	 end
+      end
    end,
    rkStepperFields = function(self)
       return self.distf[1], self.distf[2], self.distf[3]
    end,
    forwardEuler = function(self, tCurr, dt, fIn, fOut)
-      return self.vlasovSlvr:advance(tCurr, dt, {fIn}, {fOut})
+      if self.evolve then
+	 return self.vlasovSlvr:advance(tCurr, dt, {fIn}, {fOut})
+      else
+	 fOut:copy(fIn) -- just copy stuff over
+	 return true, GKYL_MAX_DOUBLE
+      end
    end,
    applyBc = function(self, tCurr, dt, fIn)
       fIn:sync()
@@ -181,6 +199,106 @@ Species.__index = {
 -- add to module table
 M.Species = Species
 
+-- Class to store Maxwell field info
+local EmField = {}
+function EmField:new(tbl)
+   local self = setmetatable({}, EmField)
+   self.type = "field" -- to identify objects of this type
+
+   self.epsilon0 = tbl.epsilon0
+   self.mu0 = tbl.mu0
+   self.ioMethod = "MPI"
+   self.evolve = xsys.pickBool(tbl.evolve, true) -- by default evolve field
+
+   -- store initial condition function
+   self.initFunc = tbl.init
+
+   self.lightSpeed = 1/math.sqrt(self.epsilon0*self.mu0)
+
+   return self
+end
+-- make object callable, and redirect call to the :new method
+setmetatable(EmField, { __call = function (self, o) return self.new(self, o) end })
+-- methods for EM field object
+EmField.__index = {
+   hasEB = function (self)
+      return true, false
+   end,
+   setCfl = function(self, cfl)
+      self.cfl = cfl
+   end,
+   setIoMethod = function(self, ioMethod)
+      self.ioMethod = ioMethod
+   end,
+   setBasis = function (self, basis)
+      self.basis = basis
+   end,
+   setGrid = function(self, grid)
+      self.grid = grid
+   end,
+   alloc = function(self)
+      -- allocate fields needed in RK update
+      self.em = {}
+      for i = 1, 3 do
+	 self.em[i] = DataStruct.Field {
+	    onGrid = self.grid,
+	    numComponents = self.basis:numBasis(),
+	    ghost = {1, 1}
+	 }
+      end
+      
+      -- create Adios object for field I/O
+      self.fieldIo = AdiosCartFieldIo {
+	 elemType = self.em[1]:elemType(),
+	 method = self.ioMethod,
+      }
+   end,
+   initField = function(self)
+      local project = Updater.ProjectOnBasis {
+	 onGrid = self.grid,
+	 basis = self.basis,
+	 evaluate = self.initFunc
+      }
+      project:advance(0.0, 0.0, {}, {self.em[1]})
+   end,
+   write = function(self, frame, tm)
+      if self.evolve then
+	 self.fieldIo:write(self.em[1], string.format("field_%d.bp", frame), tm)
+      else
+	 -- if not evolving species, don't write anything except initial conditions
+	 if frame == 0 then
+	    self.fieldIo:write(self.em[1], string.format("field_%d.bp", frame), tm)
+	 end
+      end
+   end,
+   rkStepperFields = function(self)
+      return self.em[1], self.em[2], self.em[3]
+   end,
+   forwardEuler = function(self, tCurr, dt, emIn, emOut)
+      if self.evolve then
+	 assert(false, "EmField:forwardEuler. NYI!")
+      else
+	 emOut:copy(emIn) -- just copy stuff over
+	 return true, GKYL_MAX_DOUBLE
+      end
+   end,
+   applyBc = function(self, tCurr, dt, emIn)
+      emIn:sync()
+   end,
+   totalSolverTime = function(self)
+      return 0.0
+   end,
+   volTime = function(self)
+      return 0.0
+   end,
+   surfTime = function(self)
+      return 0.0
+   end,   
+}
+
+-- add to module table
+M.EmField = EmField
+
 -- function to check "type" of obj.
 local function isType(obj, typeString)
    if type(obj) == typeString then
@@ -197,7 +315,7 @@ end
 local function buildApplication(self, tbl)
    -- create logger
    local log = Logger {
-      logToFile = tbl.logToFile and tbl.logToFile or false
+      logToFile = xsys.pickBool(tbl.logToFile, true)
    }
 
    log(date(false):fmt()) -- time-stamp for sim start
@@ -225,14 +343,23 @@ local function buildApplication(self, tbl)
    -- polynomial order
    local polyOrder = tbl.polyOrder
 
+   -- create basis function for configuration space
+   local confBasis = createBasis(basisNm, cdim, polyOrder)
+
+   -- I/O method
+   local ioMethod = tbl.ioMethod and tbl.ioMethod or "MPI"
+   if ioMethod ~= "POSIX" and ioMethod ~= "MPI" then
+      assert(false, "ioMethod must be one of 'MPI' or 'POSIX'. Provided '" .. ioMethod .. "' instead")
+   end
+
    -- time-stepper
    local timeStepperNm = warnDefault(tbl.timeStepper, "timeStepper", "rk3")
-   if timeStepperNm ~= "rk1" and timeStepperNm ~= "rk2" and timeStepperNm ~= "rk3" then
+   if timeStepperNm ~= "rk2" and timeStepperNm ~= "rk3" and timeStepperNm ~= "rk3s4" then
       assert(false, "Incorrect timeStepper type " .. timeStepperNm .. " specified")
    end
 
    -- CFL fractions for various steppers
-   local stepperCFLFracs = { rk2 = 1.0, rk3 = 1.0, rk4 = 2.0 }
+   local stepperCFLFracs = { rk2 = 1.0, rk3 = 1.0, rk3s4 = 2.0 }
 
    local cflFrac = tbl.cflFrac
    -- Compute CFL fraction if not specified
@@ -249,7 +376,7 @@ local function buildApplication(self, tbl)
       decompCuts = {}
       for d = 1, cdim do decompCuts[d] = 1 end
    end
-   local useShared = tbl.useShared and tbl.useShared or false
+   local useShared = xsys.pickBool(tbl.useShared, false)
 
    -- extract periodic directions
    local periodicDirs = {}
@@ -268,6 +395,7 @@ local function buildApplication(self, tbl)
       if isType(val, "species") then
 	 species[nm] = val
 	 species[nm]:setName(nm)
+	 species[nm]:setIoMethod(ioMethod)
       end
    end
 
@@ -275,15 +403,16 @@ local function buildApplication(self, tbl)
    -- compute CFL numbers
    for _, s in pairs(species) do
       local ndim = cdim+s:ndim()
-      local myCfl = tbl.cfl and tbl.cfl or 1/(ndim*(2*polyOrder+1))
+      local myCfl = tbl.cfl and tbl.cfl or cflFrac/(ndim*(2*polyOrder+1))
       cflMin = math.min(cflMin, myCfl)
-      s:setCfl(myCfl)
+      s:setCfl(cflMin)
    end
    log(string.format("Using CFL number %g", cflMin))
 
    -- setup each species
    for _, s in pairs(species) do
       s:createGrid(tbl.lower, tbl.upper, tbl.cells, decompCuts, periodicDirs)
+      s:setConfBasis(confBasis)
       s:createBasis(basisNm, polyOrder)
       s:alloc()
    end
@@ -297,14 +426,35 @@ local function buildApplication(self, tbl)
       -- NEED TO CREATE DECOMPOSITION
    }
 
+   -- setup information about fields
+   local field = tbl.field -- must always be called "field"
+   field:setIoMethod(ioMethod)
+   field:setBasis(confBasis)
+   field:setGrid(grid)
+   do
+      local myCfl = tbl.cfl and tbl.cfl or cflFrac/(cdim*(2*polyOrder+1))
+      field:setCfl(myCfl)
+   end
+   -- allocate field data
+   field:alloc()
+
+   -- initialize species solvers
+   for _, s in pairs(species) do
+      local hasE, hasB = field:hasEB()
+      s:createSolver(hasE, hasB)
+   end   
+
    -- initialize species distributions and write them out
    for _, s in pairs(species) do
       s:initDist()
    end
+   -- initialize fields
+   field:initField()
 
    -- function to write data to file
    local function writeData(frame, tCurr)
       for _, s in pairs(species) do s:write(frame, tCurr) end
+      field:write(frame, tCurr)
    end
 
    writeData(0, 0.0) -- write initial conditions
@@ -337,19 +487,6 @@ local function buildApplication(self, tbl)
    local timeSteppers = {}
 
    -- function to advance solution using SSP-RK2 scheme
-   function timeSteppers.rk1(tCurr, dt)
-      local status, dtSuggested
-      -- RK stage 1
-      status, dtSuggested = fowardEuler(tCurr, dt, 1, 2)
-      if status == false then return status, dtSuggested end
-
-      for nm, s in pairs(species) do
-	 speciesRkFields[nm][1]:copy(speciesRkFields[nm][2])
-      end
-      return status, dtSuggested
-   end   
-
-   -- function to advance solution using SSP-RK2 scheme
    function timeSteppers.rk2(tCurr, dt)
       local status, dtSuggested
       -- RK stage 1
@@ -373,6 +510,11 @@ local function buildApplication(self, tbl)
 
    end
 
+   -- function to advance solution using 4-stage SSP-RK3 scheme
+   function timeSteppers.rk3s4(tCurr, dt)
+      assert(false, "timeSteppers.rk3s4: NYI!")
+   end   
+
    local tmEnd = Time.clock()
    log(string.format("Initializing completed in %g sec\n", tmEnd-tmStart))
 
@@ -389,7 +531,6 @@ local function buildApplication(self, tbl)
       local myDt = initDt
 
       local tmSimStart = Time.clock()
-      -- main simulation loop
       -- main simulation loop
       while true do
 	 -- if needed adjust dt to hit tEnd exactly
