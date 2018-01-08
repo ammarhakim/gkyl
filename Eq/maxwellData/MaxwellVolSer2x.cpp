03ad8a1e3adc4381625152584d2fefaa95ccb933
Ammar <a.hakim777@gmail.com>
2018-01-07 23:51:34 -0500

    - Redoing Maxwell updates in the "correct way". I.e. just one C++   function to do vol update and one set of functions to do surf   update. Need to mod DG updater to use a single loop rather than   loops over vol and surf. - Added all Maxima scripts for the above

---
+++ Eq/maxwellData/MaxwellVolSer2x.cpp
@@ -0,0 +1,153 @@
+#include <MaxwellModDecl.h> 
+void MaxwellVol2xSerP1(const MaxwellEq_t *meq, const double *w, const double *dx, const double *q, double *out) 
+{ 
+  const double c2 = meq->c*meq->c, chi = meq->chi, gamma = meq->gamma; 
+  const double c2chi = c2*meq->chi, c2gamma = c2*meq->gamma; 
+ 
+  const double *ex = &q[0]; 
+  const double *ey = &q[4]; 
+  const double *ez = &q[8]; 
+  const double *bx = &q[12]; 
+  const double *by = &q[16]; 
+  const double *bz = &q[20]; 
+  const double *ph = &q[24]; 
+  const double *ps = &q[28]; 
+ 
+  double *outEx = &out[0]; 
+  double *outEy = &out[4]; 
+  double *outEz = &out[8]; 
+  double *outBx = &out[12]; 
+  double *outBy = &out[16]; 
+  double *outBz = &out[20]; 
+  double *outPh = &out[24]; 
+  double *outPs = &out[28]; 
+ 
+  double dx0 = 2.0/dx[0]; 
+  double dx1 = 2.0/dx[1]; 
+
+ 
+  outEx[1] += 1.732050807568877*ph[0]*c2chi*dx0; 
+  outEx[2] += -1.732050807568877*bz[0]*c2*dx1; 
+  outEx[3] += 1.732050807568877*ph[2]*c2chi*dx0-1.732050807568877*bz[1]*c2*dx1; 
+
+  outEy[1] += 1.732050807568877*bz[0]*c2*dx0; 
+  outEy[2] += 1.732050807568877*ph[0]*c2chi*dx1; 
+  outEy[3] += 1.732050807568877*ph[1]*c2chi*dx1+1.732050807568877*bz[2]*c2*dx0; 
+
+  outEz[1] += -1.732050807568877*by[0]*c2*dx0; 
+  outEz[2] += 1.732050807568877*bx[0]*c2*dx1; 
+  outEz[3] += 1.732050807568877*bx[1]*c2*dx1-1.732050807568877*by[2]*c2*dx0; 
+
+  outBx[1] += 1.732050807568877*ps[0]*dx0*gamma; 
+  outBx[2] += 1.732050807568877*ez[0]*c2*dx1; 
+  outBx[3] += 1.732050807568877*ps[2]*dx0*gamma+1.732050807568877*ez[1]*c2*dx1; 
+
+  outBy[1] += -1.732050807568877*ez[0]*dx0; 
+  outBy[2] += 1.732050807568877*ps[0]*c2gamma*dx1; 
+  outBy[3] += 1.732050807568877*ps[1]*c2gamma*dx1-1.732050807568877*ez[2]*dx0; 
+
+  outBz[1] += 1.732050807568877*ey[0]*dx0; 
+  outBz[2] += -1.732050807568877*ex[0]*c2*dx1; 
+  outBz[3] += 1.732050807568877*ey[2]*dx0-1.732050807568877*ex[1]*c2*dx1; 
+
+  outPh[1] += 1.732050807568877*ex[0]*chi*dx0; 
+  outPh[2] += 1.732050807568877*ey[0]*chi*dx1; 
+  outPh[3] += 1.732050807568877*ey[1]*chi*dx1+1.732050807568877*ex[2]*chi*dx0; 
+
+  outPs[1] += 1.732050807568877*bx[0]*c2gamma*dx0; 
+  outPs[2] += 1.732050807568877*by[0]*c2gamma*dx1; 
+  outPs[3] += 1.732050807568877*by[1]*c2gamma*dx1+1.732050807568877*bx[2]*c2gamma*dx0; 
+
+} 
+void MaxwellVol2xSerP2(const MaxwellEq_t *meq, const double *w, const double *dx, const double *q, double *out) 
+{ 
+  const double c2 = meq->c*meq->c, chi = meq->chi, gamma = meq->gamma; 
+  const double c2chi = c2*meq->chi, c2gamma = c2*meq->gamma; 
+ 
+  const double *ex = &q[0]; 
+  const double *ey = &q[8]; 
+  const double *ez = &q[16]; 
+  const double *bx = &q[24]; 
+  const double *by = &q[32]; 
+  const double *bz = &q[40]; 
+  const double *ph = &q[48]; 
+  const double *ps = &q[56]; 
+ 
+  double *outEx = &out[0]; 
+  double *outEy = &out[8]; 
+  double *outEz = &out[16]; 
+  double *outBx = &out[24]; 
+  double *outBy = &out[32]; 
+  double *outBz = &out[40]; 
+  double *outPh = &out[48]; 
+  double *outPs = &out[56]; 
+ 
+  double dx0 = 2.0/dx[0]; 
+  double dx1 = 2.0/dx[1]; 
+
+ 
+  outEx[1] += 1.732050807568877*ph[0]*c2chi*dx0; 
+  outEx[2] += -1.732050807568877*bz[0]*c2*dx1; 
+  outEx[3] += 1.732050807568877*ph[2]*c2chi*dx0-1.732050807568877*bz[1]*c2*dx1; 
+  outEx[4] += 3.872983346207417*ph[1]*c2chi*dx0; 
+  outEx[5] += -3.872983346207417*bz[2]*c2*dx1; 
+  outEx[6] += 3.872983346207417*ph[3]*c2chi*dx0-1.732050807568877*bz[4]*c2*dx1; 
+  outEx[7] += 1.732050807568877*ph[5]*c2chi*dx0-3.872983346207417*bz[3]*c2*dx1; 
+
+  outEy[1] += 1.732050807568877*bz[0]*c2*dx0; 
+  outEy[2] += 1.732050807568877*ph[0]*c2chi*dx1; 
+  outEy[3] += 1.732050807568877*ph[1]*c2chi*dx1+1.732050807568877*bz[2]*c2*dx0; 
+  outEy[4] += 3.872983346207417*bz[1]*c2*dx0; 
+  outEy[5] += 3.872983346207417*ph[2]*c2chi*dx1; 
+  outEy[6] += 1.732050807568877*ph[4]*c2chi*dx1+3.872983346207417*bz[3]*c2*dx0; 
+  outEy[7] += 3.872983346207417*ph[3]*c2chi*dx1+1.732050807568877*bz[5]*c2*dx0; 
+
+  outEz[1] += -1.732050807568877*by[0]*c2*dx0; 
+  outEz[2] += 1.732050807568877*bx[0]*c2*dx1; 
+  outEz[3] += 1.732050807568877*bx[1]*c2*dx1-1.732050807568877*by[2]*c2*dx0; 
+  outEz[4] += -3.872983346207417*by[1]*c2*dx0; 
+  outEz[5] += 3.872983346207417*bx[2]*c2*dx1; 
+  outEz[6] += 1.732050807568877*bx[4]*c2*dx1-3.872983346207417*by[3]*c2*dx0; 
+  outEz[7] += 3.872983346207417*bx[3]*c2*dx1-1.732050807568877*by[5]*c2*dx0; 
+
+  outBx[1] += 1.732050807568877*ps[0]*dx0*gamma; 
+  outBx[2] += 1.732050807568877*ez[0]*c2*dx1; 
+  outBx[3] += 1.732050807568877*ps[2]*dx0*gamma+1.732050807568877*ez[1]*c2*dx1; 
+  outBx[4] += 3.872983346207417*ps[1]*dx0*gamma; 
+  outBx[5] += 3.872983346207417*ez[2]*c2*dx1; 
+  outBx[6] += 3.872983346207417*ps[3]*dx0*gamma+1.732050807568877*ez[4]*c2*dx1; 
+  outBx[7] += 1.732050807568877*ps[5]*dx0*gamma+3.872983346207417*ez[3]*c2*dx1; 
+
+  outBy[1] += -1.732050807568877*ez[0]*dx0; 
+  outBy[2] += 1.732050807568877*ps[0]*c2gamma*dx1; 
+  outBy[3] += 1.732050807568877*ps[1]*c2gamma*dx1-1.732050807568877*ez[2]*dx0; 
+  outBy[4] += -3.872983346207417*ez[1]*dx0; 
+  outBy[5] += 3.872983346207417*ps[2]*c2gamma*dx1; 
+  outBy[6] += 1.732050807568877*ps[4]*c2gamma*dx1-3.872983346207417*ez[3]*dx0; 
+  outBy[7] += 3.872983346207417*ps[3]*c2gamma*dx1-1.732050807568877*ez[5]*dx0; 
+
+  outBz[1] += 1.732050807568877*ey[0]*dx0; 
+  outBz[2] += -1.732050807568877*ex[0]*c2*dx1; 
+  outBz[3] += 1.732050807568877*ey[2]*dx0-1.732050807568877*ex[1]*c2*dx1; 
+  outBz[4] += 3.872983346207417*ey[1]*dx0; 
+  outBz[5] += -3.872983346207417*ex[2]*c2*dx1; 
+  outBz[6] += 3.872983346207417*ey[3]*dx0-1.732050807568877*ex[4]*c2*dx1; 
+  outBz[7] += 1.732050807568877*ey[5]*dx0-3.872983346207417*ex[3]*c2*dx1; 
+
+  outPh[1] += 1.732050807568877*ex[0]*chi*dx0; 
+  outPh[2] += 1.732050807568877*ey[0]*chi*dx1; 
+  outPh[3] += 1.732050807568877*ey[1]*chi*dx1+1.732050807568877*ex[2]*chi*dx0; 
+  outPh[4] += 3.872983346207417*ex[1]*chi*dx0; 
+  outPh[5] += 3.872983346207417*ey[2]*chi*dx1; 
+  outPh[6] += 1.732050807568877*ey[4]*chi*dx1+3.872983346207417*ex[3]*chi*dx0; 
+  outPh[7] += 3.872983346207417*ey[3]*chi*dx1+1.732050807568877*ex[5]*chi*dx0; 
+
+  outPs[1] += 1.732050807568877*bx[0]*c2gamma*dx0; 
+  outPs[2] += 1.732050807568877*by[0]*c2gamma*dx1; 
+  outPs[3] += 1.732050807568877*by[1]*c2gamma*dx1+1.732050807568877*bx[2]*c2gamma*dx0; 
+  outPs[4] += 3.872983346207417*bx[1]*c2gamma*dx0; 
+  outPs[5] += 3.872983346207417*by[2]*c2gamma*dx1; 
+  outPs[6] += 1.732050807568877*by[4]*c2gamma*dx1+3.872983346207417*bx[3]*c2gamma*dx0; 
+  outPs[7] += 3.872983346207417*by[3]*c2gamma*dx1+1.732050807568877*bx[5]*c2gamma*dx0; 
+
+} 
