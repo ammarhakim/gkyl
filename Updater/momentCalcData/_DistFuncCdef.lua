667f290da5954538a77f4a1fa3b2ef30a675fa89
Ammar <a.hakim777@gmail.com>
2018-01-21 23:24:41 -0500

    Added heat-flux vector to list of supported moments

--- Updater/momentCalcData/_DistFuncCdef.lua
+++ Updater/momentCalcData/_DistFuncCdef.lua
@@ -12,66 +12,78 @@ void MomentCalc1x1vSer_M0_P1(const double *w, const double *dxv, const double *f
 void MomentCalc1x1vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x1vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x1vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x1vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc1x2vSer_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x2vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x2vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x2vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc1x3vSer_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x3vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x3vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x3vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc2x2vSer_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x2vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc2x2vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x2vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc2x3vSer_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x3vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc2x3vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x3vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc3x3vSer_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc3x3vSer_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc3x3vSer_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vSer_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc3x3vSer_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 
@@ -80,64 +92,77 @@ void MomentCalc1x1vMax_M0_P1(const double *w, const double *dxv, const double *f
 void MomentCalc1x1vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x1vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x1vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x1vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x1vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc1x2vMax_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x2vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x2vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x2vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x2vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc1x3vMax_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x3vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc1x3vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc1x3vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc1x3vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc2x2vMax_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x2vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc2x2vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x2vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x2vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc2x3vMax_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x3vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc2x3vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc2x3vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc2x3vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
 
 
 void MomentCalc3x3vMax_M0_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M1i_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M2ij_P1(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M2_P1(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc3x3vMax_M3i_P1(const double *w, const double *dxv, const double *f, double *out); 
 
 void MomentCalc3x3vMax_M0_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M1i_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M2ij_P2(const double *w, const double *dxv, const double *f, double *out); 
 void MomentCalc3x3vMax_M2_P2(const double *w, const double *dxv, const double *f, double *out); 
+void MomentCalc3x3vMax_M3i_P2(const double *w, const double *dxv, const double *f, double *out); 
+
 ]]
