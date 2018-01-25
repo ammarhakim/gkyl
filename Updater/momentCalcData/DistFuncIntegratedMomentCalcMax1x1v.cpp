0670cc4680d7ad640e31401802c4046bc0a59569
Ammar <a.hakim777@gmail.com>
2018-01-24 23:20:37 -0500

    Generated max-order basis integrated moments

--- Updater/momentCalcData/DistFuncIntegratedMomentCalcMax1x1v.cpp
+++ Updater/momentCalcData/DistFuncIntegratedMomentCalcMax1x1v.cpp
@@ -5,8 +5,8 @@ void IntMomentCalc1x1vMax_P1(const double *w, const double *dxv, const double *f
   const double wx1 = w[1], dv1 = dxv[1]; 
   const double wx1_sq = wx1*wx1, dv1_sq = dv1*dv1; 
  
-  out[0] += 2.0*(%e^((-vDrift^2/(2*vt^2))+(v*vDrift)/vt^2-v^2/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-(v*vDrift)/vt^2-v^2/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*volFact; 
-  out[4] += 2.0*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*volFact*wx1_sq+1.154700538379252*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[2]*dv1*volFact*wx1+0.1666666666666667*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*dv1_sq*volFact; 
+  out[0] += 2.0*f[0]*volFact; 
+  out[4] += 2.0*f[0]*volFact*wx1_sq+1.154700538379252*f[2]*dv1*volFact*wx1+0.1666666666666667*f[0]*dv1_sq*volFact; 
 } 
 void IntMomentCalc1x1vMax_P2(const double *w, const double *dxv, const double *f, double *out) 
 { 
@@ -14,6 +14,6 @@ void IntMomentCalc1x1vMax_P2(const double *w, const double *dxv, const double *f
   const double wx1 = w[1], dv1 = dxv[1]; 
   const double wx1_sq = wx1*wx1, dv1_sq = dv1*dv1; 
  
-  out[0] += 2.0*(%e^((-vDrift^2/(2*vt^2))+(v*vDrift)/vt^2-v^2/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-(v*vDrift)/vt^2-v^2/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*volFact; 
-  out[4] += 2.0*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*volFact*wx1_sq+1.154700538379252*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[2]*dv1*volFact*wx1+0.149071198499986*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[5]*dv1_sq*volFact+0.1666666666666667*(%e^((-vDrift^2/(2*vt^2))+vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt)+%e^((-vDrift^2/(2*vt^2))-vDrift/vt^2-1/(2*vt^2))/(2^(3/2)*sqrt(%pi)*vt))[0]*dv1_sq*volFact; 
+  out[0] += 2.0*f[0]*volFact; 
+  out[4] += 2.0*f[0]*volFact*wx1_sq+1.154700538379252*f[2]*dv1*volFact*wx1+0.149071198499986*f[5]*dv1_sq*volFact+0.1666666666666667*f[0]*dv1_sq*volFact; 
 } 
