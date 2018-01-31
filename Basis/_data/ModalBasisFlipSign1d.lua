d741f824391fc37df3e2288beab7f700c44bdb42
James Juno <7997308+JunoRavin@users.noreply.github.com>
2018-01-31 14:08:27 -0500

    Adding generated lua code for flipping the sign of the solution (for use in a variety of boundary conditions) as well as the maxima scripts to generate the code

---
+++ Basis/_data/ModalBasisFlipSign1d.lua
@@ -0,0 +1,32 @@
+local _M = {} 
+_M[1] = function (dir, f, out) 
+   if dir == 1  then 
+   out[1] = f[1] 
+   out[2] = -1.0*f[2] 
+   end 
+end 
+_M[2] = function (dir, f, out) 
+   if dir == 1  then 
+   out[1] = f[1] 
+   out[2] = -1.0*f[2] 
+   out[3] = f[3] 
+   end 
+end 
+_M[3] = function (dir, f, out) 
+   if dir == 1  then 
+   out[1] = f[1] 
+   out[2] = -1.0*f[2] 
+   out[3] = f[3] 
+   out[4] = -1.0*f[4] 
+   end 
+end 
+_M[4] = function (dir, f, out) 
+   if dir == 1  then 
+   out[1] = f[1] 
+   out[2] = -1.0*f[2] 
+   out[3] = f[3] 
+   out[4] = -1.0*f[4] 
+   out[5] = f[5] 
+   end 
+end 
+return _M 
