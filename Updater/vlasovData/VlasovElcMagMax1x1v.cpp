3de08b587b8d94edcf6ea4ed3968727377bb0051
James Juno <jjuno@umd.edu>
2018-03-13 17:05:07 -0400

    Committing generated C++ code for Vlasov update with global lax fluxes

--- Updater/vlasovData/VlasovElcMagMax1x1v.cpp
+++ Updater/vlasovData/VlasovElcMagMax1x1v.cpp
@@ -10,7 +10,25 @@ void VlasovVolElcMag1x1vMaxP1(const double *w, const double *dxv, const double *
   const double *B1 = &EM[8]; 
   const double *B2 = &EM[10]; 
 
-  out[2] += 1.224744871391589*E0[1]*f[1]*dv10+1.224744871391589*E0[0]*f[0]*dv10; 
+  double abar0[2]; 
+
+
+  abar0[0] = E0[0]; 
+  abar0[1] = E0[1]; 
+
+  double incr0[3]; 
+
+  for(unsigned int i=0; i<3; ++i){ 
+
+    incr0[i]=0.0; 
+
+  }; 
+
+  incr0[2] = 1.224744871391589*abar0[1]*f[1]+1.224744871391589*abar0[0]*f[0]; 
+
+  out[0] += incr0[0]*dv10; 
+  out[1] += incr0[1]*dv10; 
+  out[2] += incr0[2]*dv10; 
 } 
 void VlasovVolElcMag1x1vMaxP2(const double *w, const double *dxv, const double *EM, const double *f, double *out) 
 { 
@@ -23,7 +41,29 @@ void VlasovVolElcMag1x1vMaxP2(const double *w, const double *dxv, const double *
   const double *B1 = &EM[12]; 
   const double *B2 = &EM[15]; 
 
-  out[2] += 1.224744871391589*E0[2]*f[4]*dv10+1.224744871391589*E0[1]*f[1]*dv10+1.224744871391589*E0[0]*f[0]*dv10; 
-  out[3] += 1.095445115010332*E0[1]*f[4]*dv10+1.095445115010332*f[1]*E0[2]*dv10+1.224744871391589*E0[0]*f[1]*dv10+1.224744871391589*f[0]*E0[1]*dv10; 
-  out[5] += 2.738612787525831*E0[1]*f[3]*dv10+2.738612787525831*E0[0]*f[2]*dv10; 
+  double abar0[3]; 
+
+
+  abar0[0] = E0[0]; 
+  abar0[1] = E0[1]; 
+  abar0[2] = E0[2]; 
+
+  double incr0[6]; 
+
+  for(unsigned int i=0; i<6; ++i){ 
+
+    incr0[i]=0.0; 
+
+  }; 
+
+  incr0[2] = 1.224744871391589*abar0[2]*f[4]+1.224744871391589*abar0[1]*f[1]+1.224744871391589*abar0[0]*f[0]; 
+  incr0[3] = 1.095445115010332*abar0[1]*f[4]+1.095445115010332*f[1]*abar0[2]+1.224744871391589*abar0[0]*f[1]+1.224744871391589*f[0]*abar0[1]; 
+  incr0[5] = 2.738612787525831*abar0[1]*f[3]+2.738612787525831*abar0[0]*f[2]; 
+
+  out[0] += incr0[0]*dv10; 
+  out[1] += incr0[1]*dv10; 
+  out[2] += incr0[2]*dv10; 
+  out[3] += incr0[3]*dv10; 
+  out[4] += incr0[4]*dv10; 
+  out[5] += incr0[5]*dv10; 
 } 
