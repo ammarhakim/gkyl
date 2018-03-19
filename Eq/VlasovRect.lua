927db29ad3974d73266856b646746563003e49bb
Ammar <a.hakim777@gmail.com>
2018-03-19 13:57:39 -0400

    Vlasov eqn object is complete. Now to hook into App system and test

--- Eq/VlasovRect.lua
+++ Eq/VlasovRect.lua
@@ -12,6 +12,11 @@ local VlasovModDecl = require "Updater.vlasovData.VlasovModDecl"
 local ffi = require "ffi"
 local xsys = require "xsys"
 
+-- compute emOut = q/m*emIn
+local function rescaleEmField(qbym, emIn, emOut)
+   ffi.C.vlasovIncr(#emOut, emIn:data(), qbym, emOut:data())
+end
+
 -- Vlasov equation on a rectangular mesh
 local VlasovRect = Proto()
 
@@ -53,7 +58,16 @@ function VlasovRect:init(tbl)
    else
       self._volForceUpdate = VlasovModDecl.selectVolElc(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
       self._surfForceUpdate = VlasovModDecl.selectSurfElc(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
-   end   
+   end
+
+   -- EM field object and pointers to cell values
+   self._emField = nil
+   -- (these will be set on the first call to setAuxFields() method)
+   self._emPtr, self._emIdxr = nil, nil
+   self._emAccel = nil
+
+   -- flag to indicate if we are being called for first time
+   self._isFirst = true
 end
 
 -- Methods
@@ -88,14 +102,53 @@ function VlasovRect:maxSpeed(dir, w, dx, q)
 end       
 -- Volume integral term for use in DG scheme
 function VlasovRect:volTerm(w, dx, idx, q, out)
-   -- TODO
+   -- streaming term
+   local cflFreqStream = self._volStreamUpdate(w:data(), dx:data(), q:data(), out:data())
+   local cflFreqForce = 0.0
+   -- force term
+   if self._hasForceTerm then
+      -- set pointer to EM field and scale by q/m
+      self._emIn:fill(self._emIdxr(idx), self._emPtr) -- get pointer to EM field
+      rescaleEmField(self._qbym, self._emPtr, self._emAccel) -- multiply EM field by q/m
+      -- compute vol term
+      cflFreqForce = self._volForceUpdate(w:data(), dx:data(), self._emAccel:data(), q:data(), out:data())
+   end
+   return cflFreqStream+cflFreqForce
 end
+
 -- Surface integral term for use in DG scheme
 function VlasovRect:surfTerm(dir, w, dx, maxs, idxl, idxr, ql, qr, outl, outr)
-   -- TODO
+   local amax = 0.0
+   if dir <= self._cdim then
+      -- streaming term
+      amax = self._surfStreamUpdate[dir](
+	 w:data(), dx:data(), ql:data(), qr:data(), outl:data(), outr:data())
+   else
+      if self._hasForceTerm then
+	 -- force term
+	 -- set pointer to EM field and scale by q/m
+	 self._emIn:fill(self._emIdxr(idxl), self._emPtr) -- get pointer to EM field
+	 rescaleEmField(self._qbym, self._emPtr, self._emAccel) -- multiply EM field by q/mself._surfForceUpdate[dir-cdim](
+	 amax = self._surfForceUpdate[dir-self._cdim](
+	    w:data(), dx:data(), maxs, self._emAccel:data(), ql:data(), qr:data(), outl:data(), outr:data())
+      end
+   end
+   return amax
 end
 
 function VlasovRect:setAuxFields(auxFields)
+   if self._hasForceTerm then -- (no fields for neutral particles)
+      -- single aux field that has the full EM field
+      self._emField = auxFields[1]
+
+      if self._isFirst then
+	 -- allocate pointers to field object
+	 self._emPtr = self._emField:get(1)
+	 self._emIdxr = self._emField:genIndexer()
+	 self._emAccel = Lin.Vec(self._emField:numComponents())
+	 self._isFirst = false -- no longer first time
+      end
+   end
 end
 
 return VlasovRect
