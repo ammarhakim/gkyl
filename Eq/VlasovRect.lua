f73b858d7b2d7ebb795f5b8c65676eea7995f700
ammarhakim <a.hakim777@gmail.com>
2018-03-19 10:34:54 -0400

    - Added test to ensure Vlasov eqn can be constructed in the first   place

--- Eq/VlasovRect.lua
+++ Eq/VlasovRect.lua
@@ -8,7 +8,9 @@
 -- system libraries
 local BoundaryCondition = require "Updater.BoundaryCondition"
 local Proto = require "Lib.Proto"
+local VlasovModDecl = require "Updater.vlasovData.VlasovModDecl"
 local ffi = require "ffi"
+local xsys = require "xsys"
 
 -- Vlasov equation on a rectangular mesh
 local VlasovRect = Proto()
@@ -16,13 +18,53 @@ local VlasovRect = Proto()
 -- ctor
 function VlasovRect:init(tbl)
 
+   self._phaseBasis = assert(
+      tbl.phaseBasis, "Eq.VlasovRect: Must specify phase-space basis functions to use using 'phaseBasis'")
+   self._confBasis = assert(
+      tbl.confBasis, "Eq.VlasovRect: Must specify configuration-space basis functions to use using 'confBasis'")
+   
+   local charge = assert(tbl.charge, "Eq.VlasovRect: must specify charge using 'charge' ")
+   local mass = assert(tbl.mass, "Eq.VlasovRect: must specify mass using 'mass' ")
+
+   self._qbym = charge/mass -- only q/m ratio is ever needed
+
+   self._pdim = self._phaseBasis:ndim()
+   self._cdim = self._confBasis:ndim()
+   self._vdim = self._pdim-self._cdim
+
+   -- functions to perform streaming updates
+   self._volStreamUpdate = VlasovModDecl.selectVolStream(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+   self._surfStreamUpdate = VlasovModDecl.selectSurfStream(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+
+   -- check if we have a electric and magnetic field
+   local hasElcField = xsys.pickBool(tbl.hasElectricField, true)
+   local hasMagField = xsys.pickBool(tbl.hasMagneticField, true)
+
+   self._hasForceTerm = false -- flag to indicate if we have any force terms at all
+   if hasElcField or hasMagField then
+      self._hasForceTerm = true
+   end
+
+   self._volForceUpdate, self._surfForceUpdate = nil, nil
+   -- functions to perform force updates from electric field
+   if hasMagField then 
+      self._volForceUpdate = VlasovModDecl.selectVolElcMag(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+      self._surfForceUpdate = VlasovModDecl.selectSurfElcMag(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+   else
+      self._volForceUpdate = VlasovModDecl.selectVolElc(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+      self._surfForceUpdate = VlasovModDecl.selectSurfElc(self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
+   end   
 end
 
 -- Methods
 function VlasovRect:numEquations() return 1 end
 function VlasovRect:numWaves() return 1 end
 function VlasovRect:isPositive(q)
-   if q[0] > 0.0 return true else return false
+   if q[0] > 0.0 then
+      return true
+   else
+      return false
+   end
 end
 
 -- flux in direction dir
