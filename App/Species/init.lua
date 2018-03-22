f7c432b1372df5c853868aa64a40ed547b2b0384
ammarhakim <a.hakim777@gmail.com>
2018-03-22 16:23:25 -0400

    Commented out fluid species as there are none at present

--- App/Species/init.lua
+++ App/Species/init.lua
@@ -1,7 +1,7 @@
 -- for accessing any species object
 local SpeciesBase = require "App.Species.SpeciesBase"
 local KineticSpecies = require "App.Species.KineticSpecies"
-local FluidSpecies = require "App.Species.FluidSpecies"
+--local FluidSpecies = require "App.Species.FluidSpecies"
 local VlasovSpecies = require "App.Species.VlasovSpecies"
 --local GkSpecies = require "App.Species.GkSpecies"
 --local IncompEulerSpecies = require "App.Species.IncompEulerSpecies"
