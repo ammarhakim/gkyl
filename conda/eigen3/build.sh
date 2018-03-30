cb67ed9a54fcb40d26a156e2f543904baea6419e
Petr Cagas <pcagas@vt.edu>
2018-03-30 10:27:20 -0400

    conda build

--- conda/eigen3/build.sh
+++ conda/eigen3/build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
+curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz > eigen-3.3.4.tar.gz
+gunzip eigen-3.3.4.tar.gz
+tar -xvf eigen-3.3.4.tar
+
+cd eigen-eigen-*
+mkdir build-dir; cd build-dir
+cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
 make install
--- conda/gkyl/build.sh
+++ conda/gkyl/build.sh
@@ -2,7 +2,7 @@
 OUT=build-par
 
 # Compile flags (set optimization/debug flags here)
-CXXFLAGS='-O3,-Wall'
+CXXFLAGS='-O3,-Wall,-std=c++14'
 
 # LuaJIT options
 LUAJIT_INC_DIR=$PREFIX/include/luajit-2.1
@@ -20,7 +20,10 @@ ENABLE_ADIOS="--enable-adios" # set to blank to disable ADIOS
 ADIOS_INC_DIR=$PREFIX/include
 ADIOS_LIB_DIR=$PREFIX/lib
 
+# EIGEN options
+EIGEN_INC_DIR=$PREFIX/include/eigen3
+
 # You probably do not need to modify the command itself
-cmd="./waf --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure build install"
+cmd="./waf --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR --eigen-inc-dir=$EIGEN_INC_DIR configure build install"
 echo $cmd
 $cmd
