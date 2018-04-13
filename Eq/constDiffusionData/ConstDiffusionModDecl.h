227e0399d462c90a7a1b6ba55f31cac480b39af2
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2018-04-13 11:49:05 -0400

    Fix misspelling in ConstDiffusion header, and remove std:: in ConstDiffusion volume kernels.

--- Eq/constDiffusionData/ConstDiffusionModDecl.h
+++ Eq/constDiffusionData/ConstDiffusionModDecl.h
@@ -3,54 +3,54 @@
 #include <cmath> 
 extern "C" { 
 double ConstDiffusionVol1xMaxP1(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf1xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf1xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol1xMaxP2(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf1xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf1xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol1xMaxP3(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf1xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf1xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol1xMaxP4(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf1xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf1xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 
 double ConstDiffusionVol2xMaxP1(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf2xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf2xMax_Y_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_Y_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol2xMaxP2(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf2xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf2xMax_Y_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_Y_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol2xMaxP3(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf2xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf2xMax_Y_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_Y_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol2xMaxP4(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf2xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf2xMax_Y_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf2xMax_Y_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 
 double ConstDiffusionVol3xMaxP1(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf3xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Y_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Z_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Y_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Z_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol3xMaxP2(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf3xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Y_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Z_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Y_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Z_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol3xMaxP3(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf3xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Y_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Z_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_X_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Y_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Z_P3(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 double ConstDiffusionVol3xMaxP4(const double *w, const double *dxv, const double nu, const double *f, double *out); 
-void ConstDiffusionSurf3xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Y_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
-void ConstDiffusionSurf3xMax_Z_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const doubel nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_X_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Y_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
+void ConstDiffusionSurf3xMax_Z_P4(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double nu, const double *fl, const double *fr, double *outl, double *outr); 
 
 
 
