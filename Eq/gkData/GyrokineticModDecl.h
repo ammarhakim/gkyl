65cd22e4de9d6ed17a2c3de9c6fb3d490d8a0cd0
Noah Mandell <nrmandell@gmail.com>
2018-04-17 23:13:52 -0400

    adding 1x1v GK code

--- Eq/gkData/GyrokineticModDecl.h
+++ Eq/gkData/GyrokineticModDecl.h
@@ -2,6 +2,13 @@
 #define GYROKINETIC_MOD_DECL_H 
 #include <cmath> 
 extern "C" { 
+double GyrokineticVol1x1vSerP1(const double q_, const double m_, const double *w, const double *dxv, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *f, double *out); 
+double GyrokineticSurf1x1vSer_X_P1(const double q_, const double m_, const double *w, const double *dxv, const double amax_in, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *fl, const double *fr, double *outl, double *outr); 
+double GyrokineticSurf1x1vSer_Vpar_P1(const double q_, const double m_, const double *w, const double *dxv, const double amax_in, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *fl, const double *fr, double *outl, double *outr); 
+double calcSheathDeltaPhi1xSer_P1(const double *phi, const double *phiWall, const double zVal);
+void calcSheathPartialReflection1x1vSer_P1(const double wv, const double dv, const double zVal, const double vcut, const double *f, double *fhat);
+
+
 double GyrokineticVol2x2vSerP1(const double q_, const double m_, const double *w, const double *dxv, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *f, double *out); 
 double GyrokineticSurf2x2vSer_X_P1(const double q_, const double m_, const double *w, const double *dxv, const double amax_in, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *fl, const double *fr, double *outl, double *outr); 
 double GyrokineticSurf2x2vSer_Y_P1(const double q_, const double m_, const double *w, const double *dxv, const double amax_in, const double *Bmag, const double *BmagInv, const double *BcurvY, const double *Phi, const double *fl, const double *fr, double *outl, double *outr); 
