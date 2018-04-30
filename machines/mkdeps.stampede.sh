b0dda764e87db75b3b4547f8169f186cfbe3f0f1
James Juno <7997308+JunoRavin@users.noreply.github.com>
2018-04-30 14:23:34 -0400

    Small change to Stampede mkdeps script which should make the dependency build work better.

--- machines/mkdeps.stampede.sh
+++ machines/mkdeps.stampede.sh
@@ -1,2 +1,4 @@
+module swap intel/18.0.0
+export GKYLSOFT='~/gkylsoft'
 cd install-deps
 ./mkdeps.sh CC=mpicc CXX=mpicxx MPICC=mpicc MPICXX=mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes
