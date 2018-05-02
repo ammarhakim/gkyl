ae74a8b8d245ec61ebd3daec0ec8f4f77961a1ae
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2018-05-02 09:45:16 -0400

    "Add DistFuncCalcPrimMoment to replace ExtraMoments. Add writeCExprs to out-scripts.mac."

---
+++ Updater/primMomentCalcData/DistFuncPrimMomentCalcModDecl.h
@@ -0,0 +1,12 @@
+#ifndef DIST_FUNC_PRIM_MOMENT_CALC_MOD_DECL_H 
+#define DIST_FUNC_PRIM_MOMENT_CALC_MOD_DECL_H 
+ 
+// Eigen include statements. 
+#include <Eigen/Dense> 
+ 
+extern "C" { 
+void GkPrimMomentCalc1x1vSer_Upar_P1(const double *m0, const double *m1, const double *m2, double *out); 
+
+
+} 
+#endif 
