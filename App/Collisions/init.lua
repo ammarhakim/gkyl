d09a325375725be05a972fe6f45c389d4cedf0b0
Petr Cagas <pcagas@vt.edu>
2018-05-15 18:09:38 -0400

    Added the updater for Voronov approximation of electron impact ionization

--- App/Collisions/init.lua
+++ App/Collisions/init.lua
@@ -1,8 +1,10 @@
 -- for accessing any collisions object
 local CollisionsBase = require "App.Collisions.CollisionsBase"
 local BgkCollisions = require "App.Collisions.BgkCollisions"
+local VoronovIonization = require "App.Collisions.VoronovIonization"
 
 return {
   CollisionsBase = CollisionsBase,
   BgkCollisions = BgkCollisions,
+  VoronovIonization = VoronovIonization,
 }
