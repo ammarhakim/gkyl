485739d39c040390e2d4a3a36a4b7ef3244dcaa9
Ammar <a.hakim777@gmail.com>
2018-06-04 09:30:23 -0400

    Ci before mv

---
+++ Unit/test_AdiosRead.lua
@@ -0,0 +1,42 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Test for ADIOS IO
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+local AdiosReader = require "Io.AdiosReader"
+local Lin = require "Lib.Linalg"
+local Mpi = require "Comm.Mpi"
+local Unit = require "Unit"
+
+local ffi  = require "ffi"
+local xsys = require "xsys"
+local new, copy, fill, sizeof, typeof, metatype = xsys.from(ffi,
+     "new, copy, fill, sizeof, typeof, metatype")
+
+local assert_equal = Unit.assert_equal
+local stats = Unit.stats
+
+function test_a(comm)
+end
+
+function allReduceOneInt(localv)
+   local sendbuf, recvbuf = new("int[1]"), new("int[1]")
+   sendbuf[0] = localv
+   Mpi.Allreduce(sendbuf, recvbuf, 1, Mpi.INT, Mpi.SUM, Mpi.COMM_WORLD)
+   return recvbuf[0]
+end
+
+-- Run tests
+test_1(Mpi.COMM_WORLD)
+
+totalFail = allReduceOneInt(stats.fail)
+totalPass = allReduceOneInt(stats.pass)
+
+if totalFail > 0 then
+   log(string.format("\nPASSED %d tests", totalPass))   
+   log(string.format("**** FAILED %d tests", totalFail))
+else
+   log(string.format("PASSED ALL %d tests!", totalPass))
+end
