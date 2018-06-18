32d1508879f4c9f0a67c49e4ec5e9ef00e7135de
nmandell <nmandell@localhost>
2018-06-18 15:03:58 -0400

    removing unnecessary cmake-dependent step for "building" eigen

--- install-deps/build-eigen.sh
+++ install-deps/build-eigen.sh
@@ -11,11 +11,14 @@ rm -rf eigen-3.3.4.tar* eigen-eigen-*
 curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz > eigen-3.3.4.tar.gz
 gunzip eigen-3.3.4.tar.gz
 tar -xvf eigen-3.3.4.tar
-
-cd eigen-eigen-*
-mkdir build-dir; cd build-dir
-cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
-make install
+mv eigen-eigen-* $PREFIX
 
 # softlink to make finding easier
 ln -sf $PREFIX $GKYLSOFT/eigen3
+
+# build tests and docs (not necessary)
+#cd $PREFIX
+#mkdir build-dir; cd build-dir
+#cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
+#make install
+
