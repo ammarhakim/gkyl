2e4d441429fbd0a83dc38884ade8bd356c15fdf4
Noah Mandell <nrmandell@gmail.com>
2018-06-18 15:19:31 -0400

    undoing eigen build change for now (didn't work...)

--- install-deps/build-eigen.sh
+++ install-deps/build-eigen.sh
@@ -11,14 +11,12 @@ rm -rf eigen-3.3.4.tar* eigen-eigen-*
 curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz > eigen-3.3.4.tar.gz
 gunzip eigen-3.3.4.tar.gz
 tar -xvf eigen-3.3.4.tar
-mv eigen-eigen-* $PREFIX
+
+cd eigen-eigen-*
+mkdir build-dir; cd build-dir
+cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
+make install
 
 # softlink to make finding easier
 ln -sf $PREFIX $GKYLSOFT/eigen3
-
-# build tests and docs (not necessary)
-#cd $PREFIX
-#mkdir build-dir; cd build-dir
-#cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
-#make install
 
