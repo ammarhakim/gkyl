ffdf4e2c5fd3f473833a0b2807885c9e8ce07ba6
Noah Mandell <nrmandell@gmail.com>
2018-06-28 09:19:28 -0500

    generalizing mkdeps and configure scripts in machines directory.

--- machines/configure.stampede.sh
+++ machines/configure.stampede.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Edit the paths and options in the following command to suit your system
+module load intel/18.0.0
+module load impi/18.0.0
 
 # Build directory
 OUT=build-par
@@ -8,8 +10,8 @@ OUT=build-par
 PREFIX=$HOME/gkylsoft/gkyl
 
 # Compile flags (set optimization/debug flags here)
-export CC='icc'
-export CXX='icpc'
+CC=icc
+CXX=icpc
 CXXFLAGS='-O3,-std=c++14,-axMIC-AVX512'
 
 # LuaJIT options
@@ -18,11 +20,11 @@ LUAJIT_LIB_DIR=$HOME/gkylsoft/luajit/lib
 LUAJIT_SHARE_DIR=$HOME/gkylsoft/luajit/share/luajit-2.1.0-beta3
 
 ## MPI options
-export MPICC='mpicc'
-export MPICXX='mpicxx'
+MPICC=mpicc
+MPICXX=mpicxx
 ENABLE_MPI="--enable-mpi"
-MPI_INC_DIR=/opt/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/include
-MPI_LIB_DIR=/opt/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/lib
+MPI_INC_DIR=$TACC_IMPI_INC
+MPI_LIB_DIR=$TACC_IMPI_LIB
 MPI_LINK_LIBS="mpi,mpicxx"
 
 # ADIOS options
@@ -34,6 +36,11 @@ ADIOS_LIB_DIR=$HOME/gkylsoft/adios/lib
 EIGEN_INC_DIR=$HOME/gkylsoft/eigen/include/eigen3
 
 # You probably do not need to modify the command itself
-cmd="./waf --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure"
+cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure"
+# if we are in machines directory, go up a directory before executing cmd
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
 echo $cmd
 $cmd
--- machines/mkdeps.edison.sh
+++ machines/mkdeps.edison.sh
@@ -5,5 +5,10 @@ CXX=icpc
 MPICC=cc
 MPICXX=CC
 export GKYLSOFT='~/gkylsoft'
+# if we are in machines directory, go up a directory
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
 cd install-deps
 ./mkdeps.sh CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --build-luajit=yes --build-adios=no --build-eigen=no --build-openmpi=no
