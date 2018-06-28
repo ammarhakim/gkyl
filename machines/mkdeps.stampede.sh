49bcc99cb995d089f3f1f4ddeffac1aeccf7dcc9
nmandell <nmandell@localhost>
2018-06-28 09:19:28 -0500

    generalizing mkdeps and configure scripts in machines directory.

--- machines/mkdeps.stampede.sh
+++ machines/mkdeps.stampede.sh
@@ -1,4 +1,10 @@
-module swap intel/18.0.0
+module load intel/18.0.0
+module load impi/18.0.0
+# if we are in machines directory, go up a directory
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
 export GKYLSOFT='~/gkylsoft'
 cd install-deps
-./mkdeps.sh CC=mpicc CXX=mpicxx MPICC=mpicc MPICXX=mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes --build-openmpi=no
+#./mkdeps.sh CC=mpicc CXX=mpicxx MPICC=mpicc MPICXX=mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes --build-openmpi=no
