42e7d786edf470f148994b60defd2ee3ce796b9a
Ammar <a.hakim777@gmail.com>
2018-07-07 11:50:18 -0400

    FIxed errors in Eigen path for curl command. Had a typo in it.

--- install-deps/build-eigen.sh
+++ install-deps/build-eigen.sh
@@ -12,10 +12,10 @@ curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz > eigen-3.3.4.tar.gz
 gunzip eigen-3.3.4.tar.gz
 tar -xvf eigen-3.3.4.tar
 
-mkdir -p $GKYLSOFT/eigen3/include/eigen3
-mv -f eigen-eigen*/Eigen $GKYLSOFT/eigen3/include/eigen3
-mv -f eigen-eigen*/signature_of_eigen3_matrix_library $GKYLSOFT/eigen3/include/eigen3
-mv -f eigen-eigen*/unsupported $GKYLSOFT/eigen3/include/eigen3
+mkdir -p $PREFIX/include/eigen3
+cp -R eigen-eigen*/Eigen $PREFIX/include/eigen3
+cp -R eigen-eigen*/signature_of_eigen3_matrix_library $PREFIX/include/eigen3
+cp -R eigen-eigen*/unsupported $PREFIX/include/eigen3
 
 rm -rf eigen-3.3.4.tar* eigen-eigen-*
 
