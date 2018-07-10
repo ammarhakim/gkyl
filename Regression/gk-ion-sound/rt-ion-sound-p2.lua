8e8178ec7a4b474024bf86b534f6c274a23e923f
Ammar <a.hakim777@gmail.com>
2018-07-10 18:34:27 -0400

    kperp = 0.0 is the correct value

--- Regression/gk-ion-sound/rt-ion-sound-p2.lua
+++ Regression/gk-ion-sound/rt-ion-sound-p2.lua
@@ -81,7 +81,7 @@ plasmaApp = Plasma.App {
    -- field solver
    field = Plasma.GkField {
       evolve = true, -- evolve field?
-      kperp2 = 1.0,
+      kperp2 = 0.0,
    },
 
    -- magnetic geometry 
