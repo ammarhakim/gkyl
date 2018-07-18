ca472882d7ff47c1d4ed157a7563a864464be7db
Liang Wang <1000547+liangwang0734@users.noreply.github.com>
2018-07-17 22:41:04 -0400

    multi-fluid: Add implicit 5m source update.
    Adapted from Gkeyll v1 without a few features, e.g., static field (needs to be
    implemeted), analytic updater (by Ng), and implicit B field.

--- Updater/FiveMomentSrcImpl.cpp
+++ Updater/FiveMomentSrcImpl.cpp
@@ -9,8 +9,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <Eigen/Eigen>
 
 // Makes indexing cleaner
+static const unsigned X = 0;
+static const unsigned Y = 1;
+static const unsigned Z = 2;
+
 static const unsigned RHO = 0;
 static const unsigned MX = 1;
 static const unsigned MY = 2;
@@ -23,8 +29,16 @@ static const unsigned EZ = 2;
 static const unsigned BX = 3;
 static const unsigned BY = 4;
 static const unsigned BZ = 5;
+static const unsigned PHIE = 6;
+static const unsigned PHIM = 7;
 
 static double sq(double x) { return x*x; }
+
+static const int COL_PIV_HOUSEHOLDER_QR = 0;
+static const int PARTIAL_PIV_LU = 1;
+
+#define fidx(n, c) (3 * (n) + (c))
+#define eidx(c) (3 * nFluids + (c))
 
 void
 gkylFiveMomentSrcRk3(FiveMomentSrcData_t *sd, FluidData_t *fd, double dt, double **ff, double *em)
@@ -118,4 +132,103 @@ gkylFiveMomentSrcRk3(FiveMomentSrcData_t *sd, FluidData_t *fd, double dt, double
       ff[n][ER] += keNew-keOld;
     }
   }  
+}
+
+void
+gkylFiveMomentSrcTimeCentered(FiveMomentSrcData_t *sd, FluidData_t *fd, double dt, double **ff, double *em)
+{
+  unsigned nFluids = sd->nFluids;
+  double dt1 = 0.5 * dt;
+  double dt2 = 0.5 * dt / sd->epsilon0;
+  
+  double staticEm[6] = {0.};  // TODO
+
+  Eigen::MatrixXd lhs = Eigen::MatrixXd::Constant(3*nFluids+3, 3*nFluids+3, 0.0);
+  Eigen::VectorXd rhs(3*nFluids+3);
+
+  //------------> fill elements corresponding to each fluid
+  for (unsigned n=0; n<nFluids; ++n)
+  {
+    double *f = ff[n];
+    double qbym = fd[n].charge/fd[n].mass;
+    double qbym2 = sq(qbym);
+
+    // eqn. for X-component of current
+    lhs(fidx(n,X), fidx(n,X)) = 1.0;
+    lhs(fidx(n,X), fidx(n,Y)) = -dt1*qbym*(em[BZ]+staticEm[BZ]);
+    lhs(fidx(n,X), fidx(n,Z)) = dt1*qbym*(em[BY]+staticEm[BY]);
+    lhs(fidx(n,X), eidx(X)) = -dt1*qbym2*f[RHO];
+
+    // eqn. for Y-component of current
+    lhs(fidx(n,Y), fidx(n,X)) = dt1*qbym*(em[BZ]+staticEm[BZ]);
+    lhs(fidx(n,Y), fidx(n,Y)) = 1.0;
+    lhs(fidx(n,Y), fidx(n,Z)) = -dt1*qbym*(em[BX]+staticEm[BX]);
+    lhs(fidx(n,Y), eidx(Y)) = -dt1*qbym2*f[RHO];
+
+    // eqn. for Z-component of current
+    lhs(fidx(n,Z), fidx(n,X)) = -dt1*qbym*(em[BY]+staticEm[BY]);
+    lhs(fidx(n,Z), fidx(n,Y)) = dt1*qbym*(em[BX]+staticEm[BX]);
+    lhs(fidx(n,Z), fidx(n,Z)) = 1.0;
+    lhs(fidx(n,Z), eidx(Z)) = -dt1*qbym2*f[RHO];
+
+    // fill corresponding RHS elements
+    rhs(fidx(n,X)) = qbym*f[MX];
+    rhs(fidx(n,Y)) = qbym*f[MY];
+    rhs(fidx(n,Z)) = qbym*f[MZ];
+
+    // add gravity source term for current
+    rhs(fidx(n, sd->gravityDir)) += qbym*f[RHO]*sd->gravity*dt1;
+
+    // set current contribution to electric field equation
+    lhs(eidx(X), fidx(n,X)) = dt2;
+    lhs(eidx(Y), fidx(n,Y)) = dt2;
+    lhs(eidx(Z), fidx(n,Z)) = dt2;
+  }
+
+  // fill in elements for electric field equations
+  Eigen::VectorXd sol;
+  lhs(eidx(EX), eidx(EX)) = 1.0;
+  lhs(eidx(EY), eidx(EY)) = 1.0;
+  lhs(eidx(EZ), eidx(EZ)) = 1.0;
+
+  rhs(eidx(EX)) = em[EX];
+  rhs(eidx(EY)) = em[EY];
+  rhs(eidx(EZ)) = em[EZ];
+
+  // invert to find solution
+  if (sd->linSolType == COL_PIV_HOUSEHOLDER_QR)
+    sol = lhs.colPivHouseholderQr().solve(rhs);
+  else if (sd->linSolType == PARTIAL_PIV_LU)
+    sol = lhs.partialPivLu().solve(rhs);
+  else
+    { /* can not happen */ }
+
+  //------------> update solution for fluids
+  double chargeDens = 0.0;
+  for (unsigned n=0; n<nFluids; ++n)
+  {
+    double *f = ff[n];
+    double qbym = fd[n].charge/fd[n].mass;
+
+    chargeDens += qbym*f[RHO];
+
+    double keOld = 0.5*(sq(f[MX]) + sq(f[MY]) + sq(f[MZ]))/f[RHO];
+    f[MX] = 2*sol(fidx(n,X))/qbym - f[MX];
+    f[MY] = 2*sol(fidx(n,Y))/qbym - f[MY];
+    f[MZ] = 2*sol(fidx(n,Z))/qbym - f[MZ];
+    if (sd->hasPressure)
+    {
+      double keNew = 0.5*(sq(f[MX]) + sq(f[MY]) + sq(f[MZ]))/f[RHO];
+      f[ER] += keNew-keOld;
+    }
+  }
+
+  //------------> update electric field
+  em[EX] = 2*sol(eidx(X)) - em[EX];
+  em[EY] = 2*sol(eidx(Y)) - em[EY];
+  em[EZ] = 2*sol(eidx(Z)) - em[EZ];
+
+  //------------> update correction potential
+  double crhoc = sd->chi_e*chargeDens/sd->epsilon0;
+  em[PHIE] += dt*crhoc;
 }
