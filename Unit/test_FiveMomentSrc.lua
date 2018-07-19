b4a54b1098695d4b16426c7ba00327369b65af39
Liang Wang <liang.wang.phys@gmail.com>
2018-07-19 10:16:15 -0400

    multi-fluid/5m-src: Clean up unit test.

--- Unit/test_FiveMomentSrc.lua
+++ Unit/test_FiveMomentSrc.lua
@@ -15,7 +15,7 @@ local Lin = require "Lib.Linalg"
 local assert_equal = Unit.assert_equal
 local stats = Unit.stats
 
-function test_1(scheme)
+function test_1(scheme, dt)
    local grid = Grid.RectCart {
       lower = {0.0},
       upper = {1.0},
@@ -39,29 +39,47 @@ function test_1(scheme)
 
    local localRange = elcFluid:localRange()
    local elcIdxr, ionIdxr, emIdxr = elcFluid:indexer(), ionFluid:indexer(), em:indexer()   
-   -- initialize fluids/fields
+  -- initialize fluids/fields
+ 
+   local gasGamma = 5. / 3.
+   local rhoe, vxe, vye, vze, pe = 1, 1, 2, 3, 1
+   local rhoi, vxi, vyi, vzi, pi = 12, 1, 2, 3, 10
+   local Ex, Ey, Ez = 4, 5, 6
+   local Bx, By, Bz = 7, 8, 9
+
+   local epsilon0 = 1
+   local q = 1
+   local me = 1
+   local omega_pe = math.sqrt(rhoe / epsilon0 * q^2 / me^2)
+   local B = math.sqrt(Bx^2 + By^2, Bz^2)
+   local Omega_Ce = B * q / me
+
+   print("1/omega_pe =", 1/omega_pe)
+   print("1/Omega_Ce =", 1/Omega_Ce)
 
    for i = localRange:lower(1), localRange:upper(1) do
       local elcItr, ionItr, emItr = elcFluid:get(elcIdxr(i)), ionFluid:get(ionIdxr(i)), em:get(emIdxr(i))
 
-      elcItr[1] = 1
-      elcItr[2] = 1
-      elcItr[3] = 2
-      elcItr[4] = 3
-      elcItr[5] = 8.5
+      ee = 0.5 * (vxe^2 + vye^2 + vze^2) * rhoe + pe / (gasGamma - 1)
+      elcItr[1] = rhoe
+      elcItr[2] = rhoe * vxe
+      elcItr[3] = rhoe * vye
+      elcItr[4] = rhoe * vze
+      elcItr[5] = ee
 
-      ionItr[1] = 12
-      ionItr[2] = 12
-      ionItr[3] = 24
-      ionItr[4] = 36
-      ionItr[5] = 99
+      ei = 0.5 * (vxi^2 + vyi^2 + vzi^2) * rhoi + pi / (gasGamma - 1)
+      ionItr[1] = rhoi
+      ionItr[2] = rhoi * vxi
+      ionItr[3] = rhoi * vyi
+      ionItr[4] = rhoi * vzi
+      ionItr[5] = ei
 
-      emItr[1] = 4
-      emItr[2] = 5
-      emItr[3] = 6
-      emItr[4] = 7
-      emItr[5] = 8
-      emItr[6] = 9
+      emItr[1] = Ex
+      emItr[2] = Ey
+      emItr[3] = Ez
+      emItr[4] = Bx
+      emItr[5] = By
+      emItr[6] = Bz
 
       emItr[7] = 3.33
       emItr[8] = 4.56
@@ -77,12 +95,11 @@ function test_1(scheme)
       mgnErrorSpeedFactor = 1,
       scheme = scheme,
    }
-   srcUpdater:advance(0.0, 1.0, {}, {elcFluid, ionFluid, em})
+   srcUpdater:advance(0.0, dt, {}, {elcFluid, ionFluid, em})
 
-   for i = localRange:lower(1), localRange:upper(1) do
+   for i = localRange:lower(1)+2, localRange:upper(1)-1 do
       local elcItr, ionItr, emItr = elcFluid:get(elcIdxr(i)), ionFluid:get(ionIdxr(i)), em:get(emIdxr(i))
  
-      print(string.format("  point %d", i))
       print(string.format("elc = {%g, %g, %g, %g, %g}", elcItr[1], elcItr[2], elcItr[3], elcItr[4], elcItr[5]))
       print(string.format("ion = {%g, %g, %g, %g, %g}", ionItr[1], ionItr[2], ionItr[3], ionItr[4], ionItr[5]))
       print(string.format("ele = {%g, %g, %g}", emItr[1], emItr[2], emItr[3]))
@@ -94,8 +111,10 @@ end
 
 -- run tests
 for _,scheme in ipairs({"time-centered", "ssp-rk3"}) do
-  print(string.format("5m source update test with scheme: %s", scheme))
-  test_1(scheme)
+  for _,dt in ipairs({1}) do
+     print(string.format("5m source update test, scheme = %s, dt = %g", scheme, dt))
+     test_1(scheme, dt)
+  end
 end
 
 if stats.fail > 0 then
