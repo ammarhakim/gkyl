a1aa87683a1108d08db681f67a3feb650c6a9689
Noah Mandell <nrmandell@gmail.com>
2018-07-24 17:36:41 -0400

    trying to calculate B0 automatically for polarization weight calculation

--- App/Species/GkSpecies.lua
+++ App/Species/GkSpecies.lua
@@ -109,7 +109,13 @@ function GkSpecies:createSolver(hasPhi, hasApar, funcField)
             end
          end
       end
-      self.B0 = funcField.B0
+      if self.cdim == 1 then 
+         self.B0 = funcField.bmagFunc(0.0, self.grid:mid(1))
+      elseif self.cdim == 2 then 
+         self.B0 = funcField.bmagFunc(0.0, self.grid:mid(1), self.grid:mid(1))
+      else
+         self.B0 = funcField.bmagFunc(0.0, self.grid:mid(1), self.grid:mid(1), self.grid:mid(2))
+      end
       self.bmag = assert(funcField.geo.bmag, "nil bmag")
    end
 
