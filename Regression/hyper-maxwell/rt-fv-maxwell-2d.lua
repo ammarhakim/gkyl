904702f84d6ec96e78de73faeaaac6fcb2fb3353
ammarhakim <a.hakim777@gmail.com>
2018-08-01 19:20:30 -0400

    FV Maxwell solver works via PlasmaOnCartGrid App. Added regression test to test in 2D. Seems to work just fine. Need more careful tests, though.
    Added flipSign method for p=0 case. This is needed to apply PEC walls
    for Maxwell. It is pretty amazing that the MaxwellField works with a
    different updater and scheme with such little mods ...

--- Regression/hyper-maxwell/rt-fv-maxwell-2d.lua
+++ Regression/hyper-maxwell/rt-fv-maxwell-2d.lua
@@ -2,22 +2,17 @@
 local Moments = require("App.PlasmaOnCartGrid").Moments
 local Constants = require "Lib.Constants"
 
-L = 1.0
-X = L -- [m]
-Y = L -- [m]
-NX = 128
-NY = 128
-kwave = 2
-lwave = 2
-freq = 2*math.pi/L*math.sqrt(kwave^2+lwave^2)*Constants.SPEED_OF_LIGHT
-tperiod = 2*math.pi/freq
+X = 80.0 -- [m]
+Y = 40.0 -- [m]
+NX = 80
+NY = 40
    
 -- create app
 maxwellApp = Moments.App {
    logToFile = true,
 
-   tEnd = tperiod, -- end time
-   nFrame = 2, -- number of output frame
+   tEnd = 150e-9, -- end time
+   nFrame = 1, -- number of output frame
    lower = {0.0, 0.0}, -- lower left corner
    upper = {X, Y}, -- upper right corner
    cells = {NX, NY}, -- number of cells
@@ -28,7 +23,7 @@ maxwellApp = Moments.App {
    decompCuts = {1, 1}, -- cuts in each direction
    useShared = false, -- if to use shared memory
 
-   periodicDirs = {1, 2}, -- periodic directions
+   periodicDirs = {}, -- periodic directions
 
    -- electrons
    field = Moments.Field {
@@ -38,18 +33,17 @@ maxwellApp = Moments.App {
       -- initial conditions
       init = function (t, xn)
 	 local x, y = xn[1], xn[2]
-	 local c = Constants.SPEED_OF_LIGHT
-	 local phi = 2*math.pi/L*(kwave*x + lwave*y)
-	 local E0 = 1.0
-	 local Ex, Ey = 0.0, 0.0
-	 local Ez = E0*math.cos(phi)
-	 local Bx = E0/c*math.cos(phi)*2*math.pi/L*lwave
-	 local By = -E0/c*math.cos(phi)*2*math.pi/L*kwave
-	 local Bz = 0.0
-	 return Ex, Ey, Ez, Bx, By, Bz
+	 local m, n = 8, 5
+	 local a = m*math.pi/X
+	 local b = n*math.pi/Y
+	 local Ez = math.sin(a*x)*math.sin(b*y)
+	 return 0.0, 0.0, Ez, 0.0, 0.0, 0.0
       end,
-      
+
       evolve = true, -- evolve species?
+
+      bcx = { Moments.Field.bcReflect, Moments.Field.bcReflect},
+      bcy = { Moments.Field.bcReflect, Moments.Field.bcReflect},
    },   
 }
 -- run application
