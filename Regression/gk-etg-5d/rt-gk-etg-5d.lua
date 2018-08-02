5da7e6536f75a2215332ffededc32a97c36ca188
Noah Mandell <nrmandell@gmail.com>
2018-08-02 15:24:03 -0400

    minor changes

--- Regression/gk-etg-5d/rt-gk-etg-5d.lua
+++ Regression/gk-etg-5d/rt-gk-etg-5d.lua
@@ -98,7 +98,7 @@ plasmaApp = Plasma.App {
              },
       fluctuationBCs = true, -- only apply BCs to fluctuations
       evolve = true, -- evolve species?
-      diagnosticMoments = {"GkM0", "GkM2"} 
+      diagnosticMoments = {"GkM0", "GkM1", "GkM2", perturbed = true},
    },
 
    -- adiabatic ions
