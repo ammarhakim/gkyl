f9e7cc917fd925d341f1107f2cad4a03b35e270e
Noah Mandell <nrmandell@gmail.com>
2018-08-09 16:52:04 -0400

    fixing possible divide-by-zero issue in positivity implementation

--- Eq/Positivity.h
+++ Eq/Positivity.h
@@ -1,6 +1,8 @@
 /* define C functions needed for positivity implementation */
 #include <cmath>
+#include <limits>
 #define extraType PATCHFIT
+#define EPSILON std::numeric_limits<double>::epsilon()
 
 double patchFit(double r, double x, double CFL);
 double limTheta(double r, double x, double CFL);
