338db16b2c4d41507a66d6b6f63bba6e10f8107f
Petr Cagas <pcagas@vt.edu>
2018-08-23 13:55:14 -0400

    Lagrange fix kernel generation scripts are ready for all the dim combinations

---
+++ Updater/lagrangeFixData/gkyl_ipow.cpp
@@ -0,0 +1,21 @@
+#include <lagrangeFixDecl.h> 
+
+double gkyl_ipow(double base, int exp) {
+  double result = 1;
+  int sign = 1;
+  if (exp < 0) {
+    sign = -1;
+    exp = -1*exp;
+  }
+  while (1) {
+    if (exp & 1)
+      result *= base;
+    exp >>= 1;
+    if (!exp)
+      break;
+    base *= base;
+  }
+  if (sign == -1)
+    result = 1/result;
+  return result;
+}
--- Updater/lagrangeFixData/lagrangeFixMax1x1v.cpp
+++ Updater/lagrangeFixData/lagrangeFixMax1x1v.cpp
@@ -1,11 +1,9 @@
 #include <math.h> 
 #include <lagrangeFixDecl.h> 
+#include <gkyl_ipow.h> 
 
-void lagrangeFixMax1x1v1p(const double *dm0, const double *dm1, const double*dm2, double *f, const double vc, const double L, const double Nv) {
-  double *dm0s = dm0 - 1;
-  double *dm1s = dm1 - 1;
-  double *dm2s = dm2 - 1;
-  f[0] = f[0] +  -(1.0*((84.85281374238573*dm0s[1]*L^2-1018.233764908629*dm2s[1])*Nv^4*vc^2+(67.8822509939086*dm1s[1]*L^2-67.8822509939086*dm1s[1]*L^2*Nv^4)*vc+(84.85281374238573*dm2s[1]*L^2-12.72792206135786*dm0s[1]*L^4)*Nv^4+(7.071067811865476*dm0s[1]*L^4-84.85281374238573*dm2s[1]*L^2)*Nv^2+5.656854249492382*dm0s[1]*L^4))/(4.0*L^5*Nv^4-4.0*L^5);
-  f[1] = f[1] +  -(1.0*((84.85281374238573*dm0s[2]*L^2-1018.233764908629*dm2s[2])*Nv^4*vc^2+(271.5290039756345*dm1s[2]*L^2*Nv^2-67.8822509939086*dm1s[2]*L^2*Nv^4)*vc+(84.85281374238573*dm2s[2]*L^2-12.72792206135786*dm0s[2]*L^4)*Nv^4+(35.35533905932738*dm0s[2]*L^4-84.85281374238573*dm2s[2]*L^2)*Nv^2-22.62741699796953*dm0s[2]*L^4))/(4.0*L^5*Nv^4-20.0*L^5*Nv^2+16.0*L^5);
-  f[2] = f[2] +  -(1.0*((30.0*dm0s[1]*L^2-360.0*dm2s[1])*Nv^4*vc-12.0*dm1s[1]*L^2*Nv^4+12.0*dm1s[1]*L^2))/(2.449489742783178*L^4*Nv^5-2.449489742783178*L^4*Nv);
+void lagrangeFixMax1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f) {
+  f[0] = f[0] +  -1.0*((84.85281374238573*dm0[0]*gkyl_ipow(L[0],2)-1018.233764908629*dm2[0])*gkyl_ipow(Nv[0],4)*gkyl_ipow(vc[0],2)+vc[0]*(67.8822509939086*dm1[0]*gkyl_ipow(L[0],2)-67.8822509939086*dm1[0]*gkyl_ipow(L[0],2)*gkyl_ipow(Nv[0],4))+(84.85281374238573*dm2[0]*gkyl_ipow(L[0],2)-12.72792206135786*dm0[0]*gkyl_ipow(L[0],4))*gkyl_ipow(Nv[0],4)+(7.071067811865476*dm0[0]*gkyl_ipow(L[0],4)-84.85281374238573*dm2[0]*gkyl_ipow(L[0],2))*gkyl_ipow(Nv[0],2)+5.656854249492382*dm0[0]*gkyl_ipow(L[0],4))*gkyl_ipow(4.0*gkyl_ipow(L[0],5)*gkyl_ipow(Nv[0],4)-4.0*gkyl_ipow(L[0],5),-1);
+  f[1] = f[1] +  -1.0*((84.85281374238573*dm0[1]*gkyl_ipow(L[0],2)-1018.233764908629*dm2[1])*gkyl_ipow(Nv[0],4)*gkyl_ipow(vc[0],2)+vc[0]*(271.5290039756345*dm1[1]*gkyl_ipow(L[0],2)*gkyl_ipow(Nv[0],2)-67.8822509939086*dm1[1]*gkyl_ipow(L[0],2)*gkyl_ipow(Nv[0],4))+(84.85281374238573*dm2[1]*gkyl_ipow(L[0],2)-12.72792206135786*dm0[1]*gkyl_ipow(L[0],4))*gkyl_ipow(Nv[0],4)+(35.35533905932738*dm0[1]*gkyl_ipow(L[0],4)-84.85281374238573*dm2[1]*gkyl_ipow(L[0],2))*gkyl_ipow(Nv[0],2)-22.62741699796953*dm0[1]*gkyl_ipow(L[0],4))*gkyl_ipow(4.0*gkyl_ipow(L[0],5)*gkyl_ipow(Nv[0],4)-20.0*gkyl_ipow(L[0],5)*gkyl_ipow(Nv[0],2)+16.0*gkyl_ipow(L[0],5),-1);
+  f[2] = f[2] +  -1.0*(vc[0]*(30.0*dm0[0]*gkyl_ipow(L[0],2)-360.0*dm2[0])*gkyl_ipow(Nv[0],4)-12.0*dm1[0]*gkyl_ipow(L[0],2)*gkyl_ipow(Nv[0],4)+12.0*dm1[0]*gkyl_ipow(L[0],2))*gkyl_ipow(2.449489742783178*gkyl_ipow(L[0],4)*gkyl_ipow(Nv[0],5)-2.449489742783178*Nv[0]*gkyl_ipow(L[0],4),-1);
 }
