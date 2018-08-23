9a0ebefac007884e5c4257b3dbde12ed3a89bfb8
Petr Cagas <pcagas@vt.edu>
2018-08-23 15:09:27 -0400

    bug fix

--- Updater/lagrangeFixData/lagrangeFixDecl.h
+++ Updater/lagrangeFixData/lagrangeFixDecl.h
@@ -7,7 +7,11 @@ double gkyl_ipow(double base, int exp);
 
 void lagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
+void lagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
 void lagrangeFixMax1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 } 
 #endif
