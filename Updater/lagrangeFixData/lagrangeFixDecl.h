9b1f4db0d09138f2ec22df66b5d54d05eec5948f
Petr Cagas <pcagas@vt.edu>
2018-08-24 10:53:03 -0400

    Lagrange fix modifications

--- Updater/lagrangeFixData/lagrangeFixDecl.h
+++ Updater/lagrangeFixData/lagrangeFixDecl.h
@@ -7,11 +7,19 @@ double gkyl_ipow(double base, int exp);
 
 void lagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
+void lagrangeFixSer1x1v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
 void lagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer1x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 void lagrangeFixMax1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
+void lagrangeFixMax1x1v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
 void lagrangeFixMax1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 } 
 #endif
