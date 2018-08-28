22308d25f816650052a1ccf9393258f4247f7e61
Petr Cagas <pcagas@vt.edu>
2018-08-28 08:50:00 -0400

    Lagrange fix Serendipity kernels added up to 2x3v p=3; testing is still required

--- Updater/lagrangeFixData/_lagrangeFixCdef.lua
+++ Updater/lagrangeFixData/_lagrangeFixCdef.lua
@@ -5,16 +5,60 @@ void lagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *L, doub
 
 void lagrangeFixSer1x1v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
+void lagrangeFixSer1x1v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
 void lagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 void lagrangeFixSer1x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer1x2v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer1x3v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer1x3v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer1x3v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x2v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x3v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x3v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixSer2x3v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 void lagrangeFixMax1x1v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 void lagrangeFixMax1x1v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
+void lagrangeFixMax1x1v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
 void lagrangeFixMax1x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 void lagrangeFixMax1x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x2v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x3v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x3v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax1x3v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x2v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x2v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x2v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x3v1p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x3v2p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
+
+void lagrangeFixMax2x3v3p(double *dm0, double *dm1, double *dm2, double *L, double *Nv, double *vc, double *f);
 
 ]]
