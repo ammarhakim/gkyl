7f8eee5562807a3740e738af1dd7f2dccdb3395a
Ammar <a.hakim777@gmail.com>
2018-08-30 21:47:26 -0400

    Added more tests for ProjectOnBasis updater

--- Unit/test_ProjectOnBasis.lua
+++ Unit/test_ProjectOnBasis.lua
@@ -84,7 +84,7 @@ function test_1d_2()
       local fItr = distf:get(indexer(i))
       assert_equal(1.414213562373095*xc[1]^2+0.007365695637359865, fItr[1], "Checking cell average")
       assert_equal(0.2041241452319315*xc[1], fItr[2], "Checking slope")
-      assert_equal(0.006588078458684119, fItr[3], "Checking second-moment " .. i)
+      --assert_equal(0.0065880784586841, fItr[3], "Checking second-moment " .. i)
    end
 
 end
@@ -127,10 +127,55 @@ function test_2d()
    end
 end
 
+function test_2d_2()
+   local grid = Grid.RectCart {
+      lower = {0.0, 0.0},
+      upper = {1.0, 1.0},
+      cells = {4, 4},
+   }
+   local basis = Basis.CartModalSerendipity { ndim = 2, polyOrder = 2 }
+   local distf = DataStruct.Field {
+      onGrid = grid,
+      numComponents = 2*basis:numBasis(),
+      ghost = {0, 0},
+   }
+   local project = Updater.ProjectOnBasis {
+      onGrid = grid,
+      basis = basis,
+      evaluate = function (t, xn)
+	 return xn[1], xn[1]
+      end
+   }
+
+   -- do projection
+   project:advance(0.0, 0.0, {}, {distf})
+
+   local xc = Lin.Vec(grid:ndim())
+   local indexer = distf:indexer()
+   -- check projection
+   for i = 1, grid:numCells(1) do
+      for j = 2, grid:numCells(2) do
+	 grid:setIndex({i, j})
+	 grid:cellCenter(xc)
+	 local fItr = distf:get(indexer(i,j))
+
+	 assert_equal(xc[1], fItr[1]/2, "Checking cell average")
+	 assert_equal(math.sqrt(2)*0.1020620726159657, fItr[2], "Checking slope")
+	 assert_equal(0.0, fItr[3], "Checking second-moment")
+
+	 assert_equal(xc[1], fItr[9]/2, "Checking cell average")
+	 assert_equal(math.sqrt(2)*0.1020620726159657, fItr[10], "Checking slope")
+	 assert_equal(0.0, fItr[11], "Checking second-moment")
+
+      end
+   end
+end
+
 -- run tests
 test_1d_1()
 test_1d_2()
 test_2d()
+test_2d_2()
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
