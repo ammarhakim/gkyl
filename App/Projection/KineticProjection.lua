4758080f77b744578876dc83cd61ba584c449c24
Petr Cagas <pcagas@vt.edu>
2018-09-06 15:52:56 -0400

    Lagrange fix hooked into the App and seems to be working

--- App/Projection/KineticProjection.lua
+++ App/Projection/KineticProjection.lua
@@ -6,10 +6,9 @@
 -- + 6 @ |||| # P ||| +
 --------------------------------------------------------------------------------
 
-local Proto = require "Lib.Proto"
 local ProjectionBase = require "App.Projection.ProjectionBase"
+local Proto = require "Lib.Proto"
 local Updater = require "Updater"
-local DataStruct = require "DataStruct"
 local xsys = require "xsys"
 --local Time = require "Lib.Time"
 
@@ -30,13 +29,16 @@ function KineticProjection:fullInit(species)
    self.confGrid = species.confGrid
 
    self.numVelDims = self.phaseGrid:ndim() - self.confGrid:ndim()
+
+   self.isInit = xsys.pickBool(self.tbl.isInit, true)
+   self.isBackground = xsys.pickBool(self.tbl.isBackground, false)
+   self.isSource = xsys.pickBool(self.tbl.isSource, false)
+
+   self.exactScaleM0 = xsys.pickBool(self.tbl.exactScaleM0, true)
+   self.exactLagFixM012 = xsys.pickBool(self.tbl.exactLagFixM012, false)
 end
 
-function KineticProjection:setName(nm)
-   self.name = nm
-end
-
-function KineticProjection:set(t, distf)
+function KineticProjection:run(t, distf)
    self.project:advance(t, 0.0, {}, {distf})
 end
 
@@ -47,8 +49,11 @@ local FunctionProjection = Proto(KineticProjection)
 function FunctionProjection:fullInit(species)
    FunctionProjection.super.fullInit(self, species)
 
-   local func = assert(self.tbl[1],
-		      "FunctionProjection: Must specify the function")
+   local func = self.tbl.func
+   if not func then
+      func = self.tbl[1]
+   end
+   assert(func, "FunctionProjection: Must specify the function")
    assert(type(func) == "function",
 	  "The input must be a table containing function")
    self.project = Updater.ProjectOnBasis {
@@ -76,11 +81,11 @@ function MaxwellianProjection:fullInit(species)
       self.density = function (t, zn) return tbl.density end
    end
    if type(self.drift) ~= "function" then
-      if type(tbl.drift) == "number" then 
+      if type(tbl.drift) == "number" then
 	 assert(self.numVelDims == 1,
 		string.format("KineticProjection: Number of 'drift' components (1) must correspond to the number of velocity dimensions (%d)", self.numVelDims))
 	 self.drift = function (t, zn) return {tbl.drift} end
-      else 
+      else
 	 assert(#tbl.drift == self.numVelDims,
 		string.format("KineticProjection: Number of 'drift' components (%d) must correspond to the number of velocity dimensions (%d)", #tbl.drift, self.numVelDims))
 	 self.drift = function (t, zn) return tbl.drift end
@@ -89,9 +94,6 @@ function MaxwellianProjection:fullInit(species)
    if type(self.temperature) ~= "function" then
       self.temperature = function (t, zn) return tbl.temperature end
    end
-
-   self.scaleM0 = xsys.pickBool(tbl.scaleM0, true)
-   self.lagFixM012 = xsys.pickBool(tbl.lagFixM012, false)
 
    local func = function (t, zn)
       return species:Maxwellian(zn, self.density(t, zn, species),
@@ -164,7 +166,7 @@ function MaxwellianProjection:lagrangeFix(distf)
 
    self.species.momDensityCalc:advance(0.0, 0.0, {distf}, {M1})
    func = function (t, zn)
-      return self.density(t, zn, self.species) * 
+      return self.density(t, zn, self.species) *
 	 self.drift(t, zn, self.species)[1]
    end
    project = Updater.ProjectOnBasis {
@@ -176,9 +178,9 @@ function MaxwellianProjection:lagrangeFix(distf)
    project:advance(0.0, 0.0, {}, {dM1})
    dM1:accumulate(-1.0, M1)
 
-   self.species.ptclEnergyCalc:advance(0.0, 0.0, {distf}, {M1})
+   self.species.ptclEnergyCalc:advance(0.0, 0.0, {distf}, {M2})
    func = function (t, zn)
-      return self.density(t, zn, self.species) * 
+      return self.density(t, zn, self.species) *
 	 ( self.drift(t, zn, self.species)[1] * self.drift(t, zn, self.species)[1] +
 	 self.temperature(t, zn, self.species) / self.species.mass )
    end
@@ -201,12 +203,12 @@ function MaxwellianProjection:lagrangeFix(distf)
    lagFix:advance(0.0, 0.0, {dM0, dM1, dM2}, {distf})
 end
 
-function MaxwellianProjection:set(t, distf)
+function MaxwellianProjection:run(t, distf)
    self.project:advance(t, 0.0, {}, {distf})
-   if self.scaleM0 then
+   if self.exactScaleM0 then
       self:scaleDensity(distf)
    end
-   if self.lagFixM012 then
+   if self.exactLagFixM012 then
       self:lagrangeFix(distf)
    end
 end
