9675fc5abbfe39e1589cbdc3f0d0adf66bf3d58a
Noah Mandell <nrmandell@gmail.com>
2018-09-11 13:03:20 -0400

    bug fix to indexing error in PositivityRescale

--- Updater/PositivityRescale.lua
+++ Updater/PositivityRescale.lua
@@ -31,20 +31,6 @@ function PositivityRescale:init(tbl)
    -- number of components to set
    self.numComponents = tbl.numComponents and tbl.numComponents or 1
    assert(self.numComponents == 1, "Updater.PositivityRescale only implemented for fields with numComponents = 1")
-
-   -- max allowable slope
-   local rMax = tbl.rMax and tbl.rMax or 3.0
-
-   -- set up nodes in Nd
-   local nodes = {-1.0/rMax, 1.0/rMax}
-   self.mu = {}
-   for i = 1, self.basis:numBasis() do
-      self.mu[i] = {}
-      for d = 1, self.basis:ndim() do
-        local ni = math.floor((i-1)/2^(d-1))%2
-        self.mu[i][#self.mu[i]+1] = nodes[ni+1]
-      end
-   end
 end   
 
 -- advance method
@@ -68,7 +54,7 @@ function PositivityRescale:_advance(tCurr, dt, inFld, outFld)
       fIn:fill(fInIndexer(idx), fInPtr)
       fOut:fill(fOutIndexer(idx), fOutPtr)
       
-      local f0 = 1.0/math.sqrt(2.0)^ndim*fInPtr[1] -- cell average
+      local f0 = 1.0/math.sqrt(2.0)^ndim*fInPtr[0] -- cell average
       
       local fmin = ffi.C.findMinNodalValue(fInPtr:data(), ndim)
     
@@ -76,8 +62,8 @@ function PositivityRescale:_advance(tCurr, dt, inFld, outFld)
       local theta = math.min(1, f0/(f0 - fmin + GKYL_EPSILON))
 
       -- modify moments. note no change to cell average
-      fOutPtr[1] = fInPtr[1]
-      for i = 2, numBasis do
+      fOutPtr[0] = fInPtr[0]
+      for i = 1, numBasis-1 do
          fOutPtr[i] = fInPtr[i]*theta
       end
    end
