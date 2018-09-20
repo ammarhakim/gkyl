2364cf7169a7e1317212bf3a23f4a2b6e84ef092
James Juno <7997308+JunoRavin@users.noreply.github.com>
2018-09-20 14:02:31 -0400

    Adding more regression tests. These set of tests are for just Maxwell's equations, as well as an additional collision operator test that has spatial gradients for a more sophisticated test of SelfPrimMoments and the collision operator.
    I am also adding to the commit log a note on p=3 regression tests. Most of them should always pass, but there seems to be some sort of small finite precision arithmetic error that only appears on a select few p=3 regression tests. When I run verbosely I get errors between the files of ~1e-19-1e-24, far below double precision, and yet other p=3 tests do not have this issue. It's probably not worth tracking down, but I do find it strange that for a few tests every run, no matter what, differs on that scale for some strange reason.

---
+++ Regression/vm-lbo/rt-conservation-collisions-1x1v-p3.lua
@@ -0,0 +1,126 @@
+-- Gkyl ------------------------------------------------------------------------
+local Plasma = require "App.PlasmaOnCartGrid"
+
+-- Maxwellian in 1x1v
+local function maxwellian1D(n, vx, ux, mass, temp)
+   local v2 = (vx - ux)^2
+   return n/math.sqrt(2*math.pi*temp/mass)*math.exp(-mass*v2/(2*temp))
+end
+
+-- normalization parameters
+epsilon0 = 1.0 -- permittivity of free space
+mu0 = 1.0 -- pemiability of free space
+lightSpeed = 1/math.sqrt(mu0*epsilon0) -- speed of light
+
+elcMass = 1.0 -- electron mass
+elcCharge = -1.0 -- electron charge
+ionMass = 1836.153 -- ion mass
+ionCharge = 1.0 -- ion charge
+
+nuElc = 0.001 --electron collision frequency
+nuIon = nuElc/math.sqrt(ionMass) --ion collision frequency
+
+Te_Ti = 1.0 -- ratio of electron to ion temperature
+
+n1 = 1.0
+Te1 = 1.0
+Ti1 = 1.0/Te_Ti
+
+-- derived parameters
+vtElc1 = math.sqrt(Te1/elcMass)
+vtIon1 = math.sqrt(Ti1/ionMass)
+-- plasma frequency and Debye length
+wpe1 = math.sqrt(elcCharge^2*n1/(epsilon0*elcMass))
+wpi1 = math.sqrt(ionCharge^2*n1/(epsilon0*ionMass))
+lambdaD1 = vtElc1/wpe1
+
+-- domain size and simulation time
+LX = 64.0*lambdaD1
+
+plasmaApp = Plasma.App {
+   logToFile = true,
+
+   tEnd = 200.0/wpe1, -- end time
+   nFrame = 1, -- number of output frames
+   lower = {0.0}, -- configuration space lower left
+   upper = {LX}, -- configuration space upper right
+   cells = {8}, -- configuration space cells
+   basis = "serendipity", -- one of "serendipity" or "maximal-order"
+   polyOrder = 3, -- polynomial order
+   timeStepper = "rk3", -- one of "rk2" or "rk3"
+   cflFrac = 0.9,
+   -- boundary conditions for configuration space
+   periodicDirs = {1}, -- periodic directions
+   -- decomposition for configuration space
+   decompCuts = {1}, -- cuts in each configuration direction
+   useShared = false, -- if to use shared memory
+
+   -- integrated moment flag, compute quantities 1000 times in simulation
+   calcIntQuantEvery = 0.0025,
+
+   -- electrons
+   elc = Plasma.VlasovSpecies {
+      charge = elcCharge, mass = elcMass,
+      -- velocity space grid
+      lower = {-5.0*vtElc1},
+      upper = {7.0*vtElc1},
+      cells = {12},
+      decompCuts = {1}, -- do not change, no parallelization in velocity space currently
+      -- initial conditions
+      init = function (t, xn)
+	 local x, v = xn[1], xn[2]
+	 local vd = vtElc1
+	 local xm = 0.25*LX
+	 local fv = (1+math.exp(-0.75*(x-xm)^2))*maxwellian1D(n1, v, vd, elcMass, Te1) 
+         if x>xm then
+            fv = (1+math.exp(-0.075*(x-xm)^2))*maxwellian1D(n1, v, vd, elcMass, Te1)
+         end
+	 return fv
+      end,
+      evolve = true, -- evolve species?
+      -- write out density, flow, total energy, and heat flux moments
+      diagnosticMoments = { "M0", "M1i", "M2", "M3i" },
+      diagnosticIntegratedMoments = { "intM0", "intM1i", "intM2Flow", "intM2Thermal", "intL2" },
+      coll = Plasma.VmLBOCollisions {
+            collFreq = nuElc,
+      },
+   },
+   -- protons
+   ion = Plasma.VlasovSpecies {
+      charge = ionCharge, mass = ionMass,
+      -- velocity space grid
+      lower = {-6.0*vtIon1+vtElc1},
+      upper = {6.0*vtIon1+vtElc1},
+      cells = {12},
+      decompCuts = {1}, -- do not change, no parallelization in velocity space currently
+      -- initial conditions
+      init = function (t, xn)
+	 local x, v = xn[1], xn[2]
+	 local vd = vtElc1
+	 local xm = 0.25*LX
+	 local fv = (1+math.exp(-0.75*(x-xm)^2))*maxwellian1D(n1, v, vd, ionMass, Ti1) 
+         if x>xm then
+            fv = (1+math.exp(-0.075*(x-xm)^2))*maxwellian1D(n1, v, vd, ionMass, Ti1)
+         end
+	 return fv
+      end,
+      evolve = true, -- evolve species?
+      -- write out density, flow, total energy, and heat flux moments
+      diagnosticMoments = { "M0", "M1i", "M2", "M3i" },
+      diagnosticIntegratedMoments = { "intM0", "intM1i", "intM2Flow", "intM2Thermal", "intL2" },
+      coll = Plasma.VmLBOCollisions {
+            collFreq = nuIon,
+      },
+   },
+
+   -- field solver
+   field = Plasma.MaxwellField {
+      epsilon0 = 1.0, mu0 = 1.0,
+      init = function (t, xn)
+	 return 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+      end,
+      evolve = true, -- evolve field?
+   },
+}
+-- run application
+plasmaApp:run()
