70b973199e1f20906c15982c139975e7e5816d5a
Petr Cagas <pcagas@vt.edu>
2018-09-24 15:45:37 -0400

    Added 2x2v Lagrange fix kernel for p=1. Based on the preliminary tests, it is working, but more testing is in order.

--- Updater/lagrangeFixData/GkLagrangeFixDecl.h
+++ Updater/lagrangeFixData/GkLagrangeFixDecl.h
@@ -5,7 +5,11 @@ extern "C" {
 
 double gkyl_ipow(double base, int exp);
 
+void GkLagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
+
 void GkLagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
+
+void GkLagrangeFixSer2x2v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
 
 } 
 #endif
--- Updater/lagrangeFixData/_GkLagrangeFixCdef.lua
+++ Updater/lagrangeFixData/_GkLagrangeFixCdef.lua
@@ -1,6 +1,10 @@
 local ffi  = require "ffi"
 ffi.cdef [[
 
+void GkLagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
+
 void GkLagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
+
+void GkLagrangeFixSer2x2v1p(double *dm0, double *dm1, double *dm2, double *B, double mass, double *L, double *Nv, double *vc, double *f);
 
 ]]
