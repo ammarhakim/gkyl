2b5e744b37e35e53fbf525c50cd4e5cb1d806079
Liang Wang <1000547+liangwang0734@users.noreply.github.com>
2018-10-05 10:04:55 -0400

    fv/5m: revert accidentally added print

--- App/Species/MomentSpecies.lua
+++ App/Species/MomentSpecies.lua
@@ -68,7 +68,6 @@ function MomentSpecies:appendBoundaryConditions(dir, edge, bcType)
       table.insert(self.boundaryConditions,
 		   self:makeBcUpdater(dir, edge, { bcCopyFunc }))
    else
-     print("appendBC", bcType)
       table.insert(self.boundaryConditions,
 		   self:makeBcUpdater(dir, edge, bcType ))
    end
