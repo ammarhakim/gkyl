ba7d4a726f15a7aa9b88feb9563615a7f91cb129
James Juno <7997308+JunoRavin@users.noreply.github.com>
2018-10-11 16:10:42 -0400

    Committing the tensor basis CartBinOp updater C functions. Petr and I want to do some tensor basis tests.

---
+++ Updater/binOpCalcData/CartFieldBinOpTensor1x1v.cpp
@@ -0,0 +1,322 @@
+#include <math.h> 
+#include <CartFieldBinOpModDecl.h> 
+ 
+using namespace Eigen; 
+ 
+void CartFieldBinOpMultiply1x1vTensor_P1(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       scalar/vector field in configuration space. 
+  // B:       scalar field in phase space. 
+  // Ncomp:   number of components of B (should =1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (should=1 here). 
+  // out:     output field A*B (same number of components as B). 
+ 
+  double tmp[4]; 
+  tmp[0] = 0.7071067811865475*A[1]*B[1]+0.7071067811865475*A[0]*B[0]; 
+  tmp[1] = 0.7071067811865475*A[0]*B[1]+0.7071067811865475*B[0]*A[1]; 
+  tmp[2] = 0.7071067811865475*A[1]*B[3]+0.7071067811865475*A[0]*B[2]; 
+  tmp[3] = 0.7071067811865475*A[0]*B[3]+0.7071067811865475*A[1]*B[2]; 
+ 
+  // This tmp allows for in-place multiplication. 
+  for (unsigned short int i=0; i<4; i++) 
+  { 
+    out[i] = tmp[i]; 
+  } 
+ 
+} 
+ 
+void CartFieldBinOpMultiply1x1vTensor_P2(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       scalar/vector field in configuration space. 
+  // B:       scalar field in phase space. 
+  // Ncomp:   number of components of B (should =1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (should=1 here). 
+  // out:     output field A*B (same number of components as B). 
+ 
+  double tmp[9]; 
+  tmp[0] = 0.7071067811865475*A[2]*B[4]+0.7071067811865475*A[1]*B[1]+0.7071067811865475*A[0]*B[0]; 
+  tmp[1] = 0.6324555320336759*A[1]*B[4]+0.6324555320336759*B[1]*A[2]+0.7071067811865475*A[0]*B[1]+0.7071067811865475*B[0]*A[1]; 
+  tmp[2] = 0.7071067811865475*A[2]*B[6]+0.7071067811865475*A[1]*B[3]+0.7071067811865475*A[0]*B[2]; 
+  tmp[3] = 0.632455532033676*A[1]*B[6]+0.6324555320336759*A[2]*B[3]+0.7071067811865475*A[0]*B[3]+0.7071067811865475*A[1]*B[2]; 
+  tmp[4] = 0.4517539514526256*A[2]*B[4]+0.7071067811865475*A[0]*B[4]+0.7071067811865475*B[0]*A[2]+0.6324555320336759*A[1]*B[1]; 
+  tmp[5] = 0.7071067811865475*A[2]*B[8]+0.7071067811865475*A[1]*B[7]+0.7071067811865475*A[0]*B[5]; 
+  tmp[6] = 0.4517539514526256*A[2]*B[6]+0.7071067811865475*A[0]*B[6]+0.632455532033676*A[1]*B[3]+0.7071067811865475*A[2]*B[2]; 
+  tmp[7] = 0.632455532033676*A[1]*B[8]+0.6324555320336759*A[2]*B[7]+0.7071067811865475*A[0]*B[7]+0.7071067811865475*A[1]*B[5]; 
+  tmp[8] = 0.4517539514526256*A[2]*B[8]+0.7071067811865475*A[0]*B[8]+0.632455532033676*A[1]*B[7]+0.7071067811865475*A[2]*B[5]; 
+ 
+  // This tmp allows for in-place multiplication. 
+  for (unsigned short int i=0; i<9; i++) 
+  { 
+    out[i] = tmp[i]; 
+  } 
+ 
+} 
+ 
+void CartFieldBinOpMultiply1x1vTensor_P3(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       scalar/vector field in configuration space. 
+  // B:       scalar field in phase space. 
+  // Ncomp:   number of components of B (should =1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (should=1 here). 
+  // out:     output field A*B (same number of components as B). 
+ 
+  double tmp[16]; 
+  tmp[0] = 0.7071067811865475*A[3]*B[8]+0.7071067811865475*A[2]*B[4]+0.7071067811865475*A[1]*B[1]+0.7071067811865475*A[0]*B[0]; 
+  tmp[1] = 0.6210590034081186*A[2]*B[8]+0.6210590034081186*A[3]*B[4]+0.6324555320336759*A[1]*B[4]+0.6324555320336759*B[1]*A[2]+0.7071067811865475*A[0]*B[1]+0.7071067811865475*B[0]*A[1]; 
+  tmp[2] = 0.7071067811865474*A[3]*B[11]+0.7071067811865475*A[2]*B[6]+0.7071067811865475*A[1]*B[3]+0.7071067811865475*A[0]*B[2]; 
+  tmp[3] = 0.6210590034081187*A[2]*B[11]+0.6210590034081187*A[3]*B[6]+0.632455532033676*A[1]*B[6]+0.6324555320336759*A[2]*B[3]+0.7071067811865475*A[0]*B[3]+0.7071067811865475*A[1]*B[2]; 
+  tmp[4] = 0.421637021355784*A[3]*B[8]+0.6210590034081186*A[1]*B[8]+0.4517539514526256*A[2]*B[4]+0.7071067811865475*A[0]*B[4]+0.6210590034081186*B[1]*A[3]+0.7071067811865475*B[0]*A[2]+0.6324555320336759*A[1]*B[1]; 
+  tmp[5] = 0.7071067811865474*A[3]*B[13]+0.7071067811865475*A[2]*B[10]+0.7071067811865475*A[1]*B[7]+0.7071067811865475*A[0]*B[5]; 
+  tmp[6] = 0.4216370213557839*A[3]*B[11]+0.6210590034081187*A[1]*B[11]+0.4517539514526256*A[2]*B[6]+0.7071067811865475*A[0]*B[6]+0.6210590034081187*A[3]*B[3]+0.632455532033676*A[1]*B[3]+0.7071067811865475*A[2]*B[2]; 
+  tmp[7] = 0.6210590034081186*A[2]*B[13]+0.6210590034081188*A[3]*B[10]+0.632455532033676*A[1]*B[10]+0.6324555320336759*A[2]*B[7]+0.7071067811865475*A[0]*B[7]+0.7071067811865475*A[1]*B[5]; 
+  tmp[8] = 0.421637021355784*A[2]*B[8]+0.7071067811865475*A[0]*B[8]+0.421637021355784*A[3]*B[4]+0.6210590034081186*A[1]*B[4]+0.7071067811865475*B[0]*A[3]+0.6210590034081186*B[1]*A[2]; 
+  tmp[9] = 0.7071067811865475*A[3]*B[15]+0.7071067811865474*A[2]*B[14]+0.7071067811865474*A[1]*B[12]+0.7071067811865475*A[0]*B[9]; 
+  tmp[10] = 0.421637021355784*A[3]*B[13]+0.6210590034081186*A[1]*B[13]+0.4517539514526256*A[2]*B[10]+0.7071067811865475*A[0]*B[10]+0.6210590034081187*A[3]*B[7]+0.632455532033676*A[1]*B[7]+0.7071067811865475*A[2]*B[5]; 
+  tmp[11] = 0.421637021355784*A[2]*B[11]+0.7071067811865475*A[0]*B[11]+0.4216370213557839*A[3]*B[6]+0.6210590034081187*A[1]*B[6]+0.6210590034081187*A[2]*B[3]+0.7071067811865474*B[2]*A[3]; 
+  tmp[12] = 0.6210590034081186*A[2]*B[15]+0.6210590034081186*A[3]*B[14]+0.6324555320336759*A[1]*B[14]+0.6324555320336759*A[2]*B[12]+0.7071067811865475*A[0]*B[12]+0.7071067811865474*A[1]*B[9]; 
+  tmp[13] = 0.421637021355784*A[2]*B[13]+0.7071067811865475*A[0]*B[13]+0.421637021355784*A[3]*B[10]+0.6210590034081186*A[1]*B[10]+0.6210590034081186*A[2]*B[7]+0.7071067811865474*A[3]*B[5]; 
+  tmp[14] = 0.421637021355784*A[3]*B[15]+0.6210590034081186*A[1]*B[15]+0.4517539514526256*A[2]*B[14]+0.7071067811865475*A[0]*B[14]+0.6210590034081186*A[3]*B[12]+0.6324555320336759*A[1]*B[12]+0.7071067811865474*A[2]*B[9]; 
+  tmp[15] = 0.421637021355784*A[2]*B[15]+0.7071067811865475*A[0]*B[15]+0.421637021355784*A[3]*B[14]+0.6210590034081186*A[1]*B[14]+0.6210590034081187*A[2]*B[12]+0.7071067811865475*A[3]*B[9]; 
+ 
+  // This tmp allows for in-place multiplication. 
+  for (unsigned short int i=0; i<16; i++) 
+  { 
+    out[i] = tmp[i]; 
+  } 
+ 
+} 
+ 
+void CartFieldBinOpDivide1x1vTensor_P1(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       configuration space denominator field (must be a scalar field). 
+  // B:       phase space numerator field (must be a scalar field). 
+  // Ncomp:   number of components of B (=1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (=1 here). 
+  // out:     output field (same number of components as B). 
+ 
+  // If a corner value is below zero, use cell average A.
+  bool avgA = false;
+  if (0.7071067811865475*A[0]-1.224744871391589*A[1] < 0) { 
+    avgA = true;
+  }
+  if (1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+ 
+  double As[2]; 
+  double Bs[4]; 
+  if (avgA) { 
+    As[0] = A[0]; 
+    As[1] = 0.0; 
+    Bs[0] = B[0]; 
+    Bs[1] = 0.0; 
+    Bs[2] = B[2]; 
+    Bs[3] = 0.0; 
+  } else { 
+    As[0] = A[0]; 
+    As[1] = A[1]; 
+    Bs[0] = B[0]; 
+    Bs[1] = B[1]; 
+    Bs[2] = B[2]; 
+    Bs[3] = B[3]; 
+  } 
+ 
+  // Declare Eigen Matrix with triple basis tensor dotted with B vector. 
+  Eigen::MatrixXd AEM = Eigen::MatrixXd::Zero(4,4); 
+  // Declare Eigen Vector with coefficients of B. 
+  Eigen::VectorXd BEV = Eigen::VectorXd::Zero(4);  
+  // Declare vector with solution to system of equations. 
+  Eigen::VectorXd u = Eigen::VectorXd::Zero(4);  
+ 
+  // Fill AEM matrix. 
+  AEM(0,0) = 0.7071067811865475*As[0]; 
+  AEM(0,1) = 0.7071067811865475*As[1]; 
+  AEM(0,2) = 0.7071067811865475*As[1]; 
+  AEM(0,3) = 0.7071067811865475*As[0]; 
+ 
+  // Fill BEV. 
+  BEV << Bs[0],Bs[1],Bs[2],Bs[3]; 
+ 
+  // Solve the system of equations. 
+  u = AEM.colPivHouseholderQr().solve(BEV); 
+ 
+  // Copy data from Eigen vector. 
+  Eigen::Map<VectorXd>(out,4,1) = u; 
+ 
+} 
+ 
+void CartFieldBinOpDivide1x1vTensor_P2(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       configuration space denominator field (must be a scalar field). 
+  // B:       phase space numerator field (must be a scalar field). 
+  // Ncomp:   number of components of B (=1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (=1 here). 
+  // out:     output field (same number of components as B). 
+ 
+  // If a corner value is below zero, use cell average A.
+  bool avgA = false;
+  if (1.58113883008419*A[2]-1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+  if (1.58113883008419*A[2]+1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+ 
+  double As[3]; 
+  double Bs[9]; 
+  if (avgA) { 
+    As[0] = A[0]; 
+    As[1] = 0.0; 
+    As[2] = 0.0; 
+    Bs[0] = B[0]; 
+    Bs[1] = 0.0; 
+    Bs[2] = B[2]; 
+    Bs[3] = 0.0; 
+    Bs[4] = 0.0; 
+    Bs[5] = B[5]; 
+    Bs[6] = 0.0; 
+    Bs[7] = 0.0; 
+    Bs[8] = 0.0; 
+  } else { 
+    As[0] = A[0]; 
+    As[1] = A[1]; 
+    As[2] = A[2]; 
+    Bs[0] = B[0]; 
+    Bs[1] = B[1]; 
+    Bs[2] = B[2]; 
+    Bs[3] = B[3]; 
+    Bs[4] = B[4]; 
+    Bs[5] = B[5]; 
+    Bs[6] = B[6]; 
+    Bs[7] = B[7]; 
+    Bs[8] = B[8]; 
+  } 
+ 
+  // Declare Eigen Matrix with triple basis tensor dotted with B vector. 
+  Eigen::MatrixXd AEM = Eigen::MatrixXd::Zero(9,9); 
+  // Declare Eigen Vector with coefficients of B. 
+  Eigen::VectorXd BEV = Eigen::VectorXd::Zero(9);  
+  // Declare vector with solution to system of equations. 
+  Eigen::VectorXd u = Eigen::VectorXd::Zero(9);  
+ 
+  // Fill AEM matrix. 
+  AEM(0,0) = 0.7071067811865475*As[0]; 
+  AEM(0,1) = 0.7071067811865475*As[1]; 
+  AEM(0,3) = 0.7071067811865475*As[1]; 
+  AEM(0,4) = 0.6324555320336759*As[2]+0.7071067811865475*As[0]; 
+  AEM(0,8) = 0.7071067811865475*As[0]; 
+  AEM(1,2) = 0.7071067811865475*As[1]; 
+  AEM(1,3) = 0.7071067811865475*As[2]; 
+  AEM(1,4) = 0.6324555320336759*As[1]; 
+  AEM(2,2) = 0.7071067811865475*As[2]; 
+ 
+  // Fill BEV. 
+  BEV << Bs[0],Bs[1],Bs[2],Bs[3],Bs[4],Bs[5],Bs[6],Bs[7],Bs[8]; 
+ 
+  // Solve the system of equations. 
+  u = AEM.colPivHouseholderQr().solve(BEV); 
+ 
+  // Copy data from Eigen vector. 
+  Eigen::Map<VectorXd>(out,9,1) = u; 
+ 
+} 
+ 
+void CartFieldBinOpDivide1x1vTensor_P3(const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       configuration space denominator field (must be a scalar field). 
+  // B:       phase space numerator field (must be a scalar field). 
+  // Ncomp:   number of components of B (=1 here). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (=1 here). 
+  // out:     output field (same number of components as B). 
+ 
+  // If a corner value is below zero, use cell average A.
+  bool avgA = false;
+  if ((-1.870828693386971*A[3])+1.58113883008419*A[2]-1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+  if (1.870828693386971*A[3]+1.58113883008419*A[2]+1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+ 
+  double As[4]; 
+  double Bs[16]; 
+  if (avgA) { 
+    As[0] = A[0]; 
+    As[1] = 0.0; 
+    As[2] = 0.0; 
+    As[3] = 0.0; 
+    Bs[0] = B[0]; 
+    Bs[1] = 0.0; 
+    Bs[2] = B[2]; 
+    Bs[3] = 0.0; 
+    Bs[4] = 0.0; 
+    Bs[5] = B[5]; 
+    Bs[6] = 0.0; 
+    Bs[7] = 0.0; 
+    Bs[8] = 0.0; 
+    Bs[9] = B[9]; 
+    Bs[10] = 0.0; 
+    Bs[11] = 0.0; 
+    Bs[12] = 0.0; 
+    Bs[13] = 0.0; 
+    Bs[14] = 0.0; 
+    Bs[15] = 0.0; 
+  } else { 
+    As[0] = A[0]; 
+    As[1] = A[1]; 
+    As[2] = A[2]; 
+    As[3] = A[3]; 
+    Bs[0] = B[0]; 
+    Bs[1] = B[1]; 
+    Bs[2] = B[2]; 
+    Bs[3] = B[3]; 
+    Bs[4] = B[4]; 
+    Bs[5] = B[5]; 
+    Bs[6] = B[6]; 
+    Bs[7] = B[7]; 
+    Bs[8] = B[8]; 
+    Bs[9] = B[9]; 
+    Bs[10] = B[10]; 
+    Bs[11] = B[11]; 
+    Bs[12] = B[12]; 
+    Bs[13] = B[13]; 
+    Bs[14] = B[14]; 
+    Bs[15] = B[15]; 
+  } 
+ 
+  // Declare Eigen Matrix with triple basis tensor dotted with B vector. 
+  Eigen::MatrixXd AEM = Eigen::MatrixXd::Zero(16,16); 
+  // Declare Eigen Vector with coefficients of B. 
+  Eigen::VectorXd BEV = Eigen::VectorXd::Zero(16);  
+  // Declare vector with solution to system of equations. 
+  Eigen::VectorXd u = Eigen::VectorXd::Zero(16);  
+ 
+  // Fill AEM matrix. 
+  AEM(0,0) = 0.7071067811865475*As[0]; 
+  AEM(0,1) = 0.7071067811865475*As[1]; 
+  AEM(0,4) = 0.7071067811865475*As[1]; 
+  AEM(0,5) = 0.6324555320336759*As[2]+0.7071067811865475*As[0]; 
+  AEM(0,10) = 0.7071067811865475*As[0]; 
+  AEM(0,11) = 0.7071067811865475*As[1]; 
+  AEM(0,14) = 0.7071067811865475*As[1]; 
+  AEM(0,15) = 0.6324555320336759*As[2]+0.7071067811865475*As[0]; 
+  AEM(1,0) = 0.7071067811865475*As[2]; 
+  AEM(1,1) = 0.6210590034081186*As[3]+0.6324555320336759*As[1]; 
+  AEM(1,10) = 0.7071067811865475*As[2]; 
+  AEM(1,11) = 0.6210590034081187*As[3]+0.632455532033676*As[1]; 
+  AEM(2,0) = 0.7071067811865475*As[3]; 
+  AEM(2,1) = 0.6210590034081186*As[2]; 
+  AEM(2,14) = 0.7071067811865474*As[3]; 
+  AEM(2,15) = 0.6210590034081187*As[2]; 
+ 
+  // Fill BEV. 
+  BEV << Bs[0],Bs[1],Bs[2],Bs[3],Bs[4],Bs[5],Bs[6],Bs[7],Bs[8],Bs[9],Bs[10],Bs[11],Bs[12],Bs[13],Bs[14],Bs[15]; 
+ 
+  // Solve the system of equations. 
+  u = AEM.colPivHouseholderQr().solve(BEV); 
+ 
+  // Copy data from Eigen vector. 
+  Eigen::Map<VectorXd>(out,16,1) = u; 
+ 
+} 
+ 
