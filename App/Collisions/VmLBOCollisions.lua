9238451a735132303903bf4ece734dc10ed7a3bb
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2018-10-27 23:44:38 -0400

    Place limits on u and vtSq in VmLBO. VmLBO gets turned off when limits are exceeded. Limit crossings are counted but currently the counter is turned off.

--- App/Collisions/VmLBOCollisions.lua
+++ App/Collisions/VmLBOCollisions.lua
@@ -13,6 +13,8 @@ local Time           = require "Lib.Time"
 local Updater        = require "Updater"
 local VmLBOconstNuEq = require "Eq.VmLBO"
 local xsys           = require "xsys"
+local Lin            = require "Lib.Linalg"
+local Mpi            = require "Comm.Mpi"
 
 -- VmLBOCollisions ---------------------------------------------------------------
 --
@@ -78,7 +80,22 @@ function VmLBOCollisions:setPhaseGrid(grid)
 end
 
 function VmLBOCollisions:createSolver()
-   self.vdim = self.phaseGrid:ndim() - self.confGrid:ndim()
+   self.vdim      = self.phaseGrid:ndim() - self.confGrid:ndim()
+
+   self.cNumBasis = self.confBasis:numBasis()
+
+   -- Maximum velocity of the velocity grid (and its square).
+   self.vMax   = Lin.Vec(self.vdim)
+   for vd = 1,self.vdim do
+      self.vMax[vd]   = self.phaseGrid:upper(self.confGrid:ndim()+vd)
+   end
+   self.vMaxSq = self.vMax[1] 
+   for vd = 1,self.vdim do
+      if (self.vMaxSq < self.vMax[vd]) then
+         self.vMaxSq = self.vMax[vd]
+      end
+   end
+   self.vMaxSq = self.vMaxSq^2
 
    -- intemediate storage for output of collisions
    self.collOut = DataStruct.Field {
@@ -90,13 +107,13 @@ function VmLBOCollisions:createSolver()
    -- Flow velocity in vdim directions.
    self.velocity = DataStruct.Field {
       onGrid        = self.confGrid,
-      numComponents = self.confBasis:numBasis()*self.vdim,
+      numComponents = self.cNumBasis*self.vdim,
       ghost         = {1, 1},
    }
    -- Thermal speed squared, vth=sqrt(T/m).
    self.vthSq = DataStruct.Field {
       onGrid        = self.confGrid,
-      numComponents = self.confBasis:numBasis(),
+      numComponents = self.cNumBasis,
       ghost         = {1, 1},
    }
 
@@ -110,7 +127,7 @@ function VmLBOCollisions:createSolver()
       -- Collisionality, nu.
       self.nuFld = DataStruct.Field {
          onGrid        = self.confGrid,
-         numComponents = self.confBasis:numBasis(),
+         numComponents = self.cNumBasis,
          ghost         = {1, 1},
       }
       -- Updater to compute spatially varying (Spitzer) nu.
@@ -126,6 +143,7 @@ function VmLBOCollisions:createSolver()
          phaseBasis       = self.phaseBasis,
          confBasis        = self.confBasis,
          useCellAverageNu = self.cellConstNu,
+         vUpper           = self.vMax,
       }
    else
       -- Lenard-Bernstein equation.
@@ -133,6 +151,7 @@ function VmLBOCollisions:createSolver()
          nu         = self.collFreq,
          phaseBasis = self.phaseBasis,
          confBasis  = self.confBasis,
+         vUpper     = self.vMax,
       }
    end
    self.collisionSlvr = Updater.HyperDisCont {
@@ -151,6 +170,15 @@ function VmLBOCollisions:createSolver()
       confBasis  = self.confBasis,
       operator   = "VmLBO",
    }
+
+   -- Number of cells in which number density was negative (somewhere).
+   self.primMomLimitCrossings = DataStruct.DynVector {
+      numComponents = 1,
+   }
+   self.primMomCrossLimitL = Lin.Vec(1)
+   self.primMomCrossLimitG = Lin.Vec(1)
+   -- Factor dividing zeroth-coefficient in configuration space cell average.
+   self.cellAvFac          = 1.0/math.sqrt(2.0^self.confGrid:ndim())
 end
 
 function VmLBOCollisions:forwardEuler(tCurr, dt, fIn, species, fOut)
@@ -164,6 +192,37 @@ function VmLBOCollisions:forwardEuler(tCurr, dt, fIn, species, fOut)
       self.primMomSelf:advance(0.0, 0.0, {selfMom[1], selfMom[2], selfMom[3],fIn},
                                          {self.velocity,self.vthSq})
       self.tmEvalMom = self.tmEvalMom + Time.clock() - tmEvalMomStart
+
+      -- NOTE: The following code is commented out because Vm users don't seem
+      -- to be as worried about limit crossings as Gk users, so counting them
+      -- is disabled for now. See the 'write' method as well.
+      ---- Determine whether primitive moments cross limits based on
+      ---- parallel flow speed and thermal speed squared.
+      --self.primMomCrossLimitL[1] = 0
+      --self.primMomCrossLimitG[1] = 0
+      --local confIndexer          = self.velocity:genIndexer()
+      --local uItr                 = self.velocity:get(1)
+      --local vthSqItr             = self.vthSq:get(1)
+      --for idx in self.velocity:localRangeIter() do
+      --   self.velocity:fill(confIndexer(idx), uItr)
+      --   self.vthSq:fill(confIndexer(idx), vthSqItr)
+      --   local primCrossingFound = false
+      --   for vd = 1,self.vdim do
+      --      if (math.abs(uItr[(vd-1)*self.cNumBasis+1]*self.cellAvFac)>self.vMax[vd]) then
+      --         uCrossingFound = true
+      --         break
+      --      end
+      --   end
+      --   local vthSq0 = vthSqItr[1]*self.cellAvFac
+
+      --   if (uCrossingFound or (vthSq0<0) or (vthSq0>self.vMaxSq)) then
+      --      self.primMomCrossLimitL[1] = self.primMomCrossLimitL[1]+1
+      --   end
+      --end
+      --Mpi.Allreduce(self.primMomCrossLimitL:data(), self.primMomCrossLimitG:data(), 1,
+      --              Mpi.DOUBLE, Mpi.SUM, self.confGrid:commSet().comm)
+      --self.primMomLimitCrossings:appendData(tCurr+dt, self.primMomCrossLimitG)
+
 
       if self.varNu then
          -- Compute the collisionality.
@@ -192,6 +251,8 @@ end
 function VmLBOCollisions:write(tm, frame)
    self.velocity:write(string.format("%s_%s_%d.bp", self.speciesName, "u", frame), tm, frame)
    self.vthSq:write(string.format("%s_%s_%d.bp", self.speciesName, "vthSq", frame), tm, frame)
+-- Since this doesn't seem to be as big a problem in Vm as in Gk, we comment this out for now.
+--   self.primMomLimitCrossings:write(string.format("%s_%s_%d.bp", self.speciesName, "primMomLimitCrossings", frame), tm, frame)
 end
 
 function VmLBOCollisions:totalTime()
