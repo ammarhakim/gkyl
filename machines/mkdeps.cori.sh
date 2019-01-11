176db46d254c2689e33c522a97ff2299bcf6f8c5
Noah Mandell <nrmandell@gmail.com>
2019-01-11 07:36:09 -0800

    removing user-specific paths from cori machine files

--- machines/mkdeps.cori.sh
+++ machines/mkdeps.cori.sh
@@ -4,11 +4,11 @@ CC=icc
 CXX=icpc
 MPICC=cc
 MPICXX=CC
-export GKYLSOFT='/global/common/software/m2116/nmandell/gkylsoft'
 # if we are in machines directory, go up a directory
 if [ `dirname "$0"` == "." ] 
   then
     cd ..
 fi
+export GKYLSOFT=$(readlink -f ../gkylsoft)
 cd install-deps
 ./mkdeps.sh --prefix=$GKYLSOFT CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --build-luajit=yes --build-adios=no --build-eigen=no --build-openmpi=no
