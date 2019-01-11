56c0fe4f12f1a04d906a2862d2d9c100ae550ea2
Noah Mandell <nrmandell@gmail.com>
2019-01-11 07:36:09 -0800

    removing user-specific paths from cori machine files

--- machines/configure.cori.sh
+++ machines/configure.cori.sh
@@ -12,17 +12,9 @@ MPICXX=CC
 
 # Build directory
 OUT=build
-# Install location
-GKYLSOFT='/global/common/software/m2116/nmandell/gkylsoft'
-PREFIX=$GKYLSOFT/gkyl
 
 # Compile flags (set optimization/debug flags here)
 CXXFLAGS='-O3,-std=c++14,-fast'
-
-# LuaJIT options
-LUAJIT_INC_DIR=$GKYLSOFT/luajit/include/luajit-2.1
-LUAJIT_LIB_DIR=$GKYLSOFT/luajit/lib
-LUAJIT_SHARE_DIR=$GKYLSOFT/luajit/share/luajit-2.1.0-beta3
 
 # MPI options
 ENABLE_MPI="--enable-mpi"
@@ -39,12 +31,21 @@ ADIOS_LINK_LIBS="z,mxml"
 # EIGEN options
 EIGEN_INC_DIR=$EIGEN3_DIR/include/eigen3
 
-# You probably do not need to modify the command itself
-cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR --adios-link-libs=$ADIOS_LINK_LIBS --eigen-inc-dir=$EIGEN_INC_DIR configure"
 # if we are in machines directory, go up a directory before executing cmd
 if [ `dirname "$0"` == "." ] 
   then
     cd ..
 fi
+# Install location
+export GKYLSOFT=$(readlink -f ../gkylsoft)
+PREFIX=$GKYLSOFT/gkyl
+
+# LuaJIT options
+LUAJIT_INC_DIR=$GKYLSOFT/luajit/include/luajit-2.1
+LUAJIT_LIB_DIR=$GKYLSOFT/luajit/lib
+LUAJIT_SHARE_DIR=$GKYLSOFT/luajit/share/luajit-2.1.0-beta3
+
+# You probably do not need to modify the command itself
+cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR --adios-link-libs=$ADIOS_LINK_LIBS --eigen-inc-dir=$EIGEN_INC_DIR configure"
 echo $cmd
 $cmd
