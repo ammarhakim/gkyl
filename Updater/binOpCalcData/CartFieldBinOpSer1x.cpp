32e18a2b01c622ed516dc097e4d3021835cdb4d4
James Juno <jjuno@umd.edu>
2019-01-21 12:58:06 -0500

    In the binOp reorganization, polyOrder 3 was deleted. Since I am using polyOrder 3 for a number of simulations I have regenerated the Serendipity basis, polyOrder 3 data for 1X1V through 2X2V. There also appeared to be a bug in the maxima script for the weak divide.
    When generating the weak divide for phase space data, the command to invert the matrix was: data->u_D = data->AEM_D.colPivHouseholderQr().solve(data->BEV_D);
    
    However, when generating the weak divide for configuration space data, the command in the maxima script was: data->u_S = data->AEM_S->colPivHouseholderQr().solve(data->BEV_S);
    
    The latter command appears to be incorrect because AEM_S is not a pointer. I have fixed this in the maxima script. To be honest, I am not sure how this error propagated into my pull of the code when the polyOrder 1 and 2 generated code was correct.

--- Updater/binOpCalcData/CartFieldBinOpSer1x.cpp
+++ Updater/binOpCalcData/CartFieldBinOpSer1x.cpp
@@ -58,6 +58,35 @@ void CartFieldBinOpMultiply1xSer_P2(binOpData_t* data, const double *A, const do
  
 } 
  
+void CartFieldBinOpMultiply1xSer_P3(binOpData_t* data, const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       scalar/vector field. 
+  // B:       scalar/vector field (must be vector if A is vector). 
+  // Ncomp:   number of components of B (could be 1D, 2D, 3D, vector). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else. 
+  // out:     output field A*B (same number of components as B). 
+ 
+  double tmp[4]; 
+ 
+  for (unsigned short int vd=0; vd<Ncomp; vd++) 
+  { 
+    unsigned short int b0 = 4*vd; 
+    unsigned short int a0 = b0*eqNcomp; 
+    // Component-wise (of the vectors) multiplication. 
+    tmp[0] = 0.7071067811865475*A[a0+3]*B[b0+3]+0.7071067811865475*A[a0+2]*B[b0+2]+0.7071067811865475*A[a0+1]*B[b0+1]+0.7071067811865475*A[a0]*B[b0]; 
+    tmp[1] = 0.6210590034081186*A[a0+2]*B[b0+3]+0.6210590034081186*A[a0+3]*B[b0+2]+0.6324555320336759*A[a0+1]*B[b0+2]+0.6324555320336759*A[a0+2]*B[b0+1]+0.7071067811865475*A[a0]*B[b0+1]+0.7071067811865475*A[a0+1]*B[b0]; 
+    tmp[2] = 0.421637021355784*A[a0+3]*B[b0+3]+0.6210590034081186*A[a0+1]*B[b0+3]+0.4517539514526256*A[a0+2]*B[b0+2]+0.7071067811865475*A[a0]*B[b0+2]+0.6210590034081186*A[a0+3]*B[b0+1]+0.6324555320336759*A[a0+1]*B[b0+1]+0.7071067811865475*A[a0+2]*B[b0]; 
+    tmp[3] = 0.421637021355784*A[a0+2]*B[b0+3]+0.7071067811865475*A[a0]*B[b0+3]+0.421637021355784*A[a0+3]*B[b0+2]+0.6210590034081186*A[a0+1]*B[b0+2]+0.6210590034081186*A[a0+2]*B[b0+1]+0.7071067811865475*A[a0+3]*B[b0]; 
+ 
+    // This tmp allows for in-place multiplication. 
+    for (unsigned short int i=0; i<4; i++) 
+    { 
+      out[b0+i] = tmp[i]; 
+    } 
+  } 
+ 
+} 
+ 
 void CartFieldBinOpDivide1xSer_P1(binOpData_t* data, const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
 { 
   // A:       denominator field (must be a scalar field). 
@@ -98,7 +127,7 @@ void CartFieldBinOpDivide1xSer_P1(binOpData_t* data, const double *A, const doub
     } 
   } 
  
-  // Fill AEM_S matrix. 
+  // Fill AEM matrix. 
   data->AEM_S(0,0) = 0.7071067811865475*As[0]; 
   data->AEM_S(0,1) = 0.7071067811865475*As[1]; 
   data->AEM_S(1,0) = 0.7071067811865475*As[1]; 
@@ -107,7 +136,7 @@ void CartFieldBinOpDivide1xSer_P1(binOpData_t* data, const double *A, const doub
   for(unsigned short int vd=0; vd<Ncomp; vd++) 
   { 
     b0 = 2*vd; 
-    // Fill BEV_S. 
+    // Fill BEV. 
     data->BEV_S << Bs[b0],Bs[b0+1]; 
  
     // Solve the system of equations. 
@@ -162,7 +191,7 @@ void CartFieldBinOpDivide1xSer_P2(binOpData_t* data, const double *A, const doub
     } 
   } 
  
-  // Fill AEM_S matrix. 
+  // Fill AEM matrix. 
   data->AEM_S(0,0) = 0.7071067811865475*As[0]; 
   data->AEM_S(0,1) = 0.7071067811865475*As[1]; 
   data->AEM_S(0,2) = 0.7071067811865475*As[2]; 
@@ -176,7 +205,7 @@ void CartFieldBinOpDivide1xSer_P2(binOpData_t* data, const double *A, const doub
   for(unsigned short int vd=0; vd<Ncomp; vd++) 
   { 
     b0 = 3*vd; 
-    // Fill BEV_S. 
+    // Fill BEV. 
     data->BEV_S << Bs[b0],Bs[b0+1],Bs[b0+2]; 
  
     // Solve the system of equations. 
@@ -184,6 +213,86 @@ void CartFieldBinOpDivide1xSer_P2(binOpData_t* data, const double *A, const doub
  
     // Copy data from Eigen vector. 
     Eigen::Map<VectorXd>(out+vd*3,3,1) = data->u_S; 
+  } 
+} 
+ 
+void CartFieldBinOpDivide1xSer_P3(binOpData_t* data, const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       denominator field (must be a scalar field). 
+  // B:       numerator field (can be scalar or vector). 
+  // Ncomp:   number of components of B (could be 1D, 2D, 3D, vector). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else. 
+  // out:     output field (same number of components as B). 
+ 
+  // If a corner value is below zero, use cell average A.
+  bool avgA = false;
+  if ((-1.870828693386971*A[3])+1.58113883008419*A[2]-1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+  if (1.870828693386971*A[3]+1.58113883008419*A[2]+1.224744871391589*A[1]+0.7071067811865475*A[0] < 0) { 
+    avgA = true;
+  }
+ 
+  unsigned short int b0; 
+  double As[4]; 
+  double Bs[4*Ncomp]; 
+  if (avgA) { 
+    As[0] = A[0]; 
+    As[1] = 0.0; 
+    As[2] = 0.0; 
+    As[3] = 0.0; 
+    for(unsigned short int vd=0; vd<Ncomp; vd++) 
+    { 
+      b0 = 4*vd; 
+      Bs[b0] = B[b0]; 
+      Bs[b0+1] = 0.0; 
+      Bs[b0+2] = 0.0; 
+      Bs[b0+3] = 0.0; 
+    } 
+  } else { 
+    As[0] = A[0]; 
+    As[1] = A[1]; 
+    As[2] = A[2]; 
+    As[3] = A[3]; 
+    for(unsigned short int vd=0; vd<Ncomp; vd++) 
+    { 
+      b0 = 4*vd; 
+      Bs[b0] = B[b0]; 
+      Bs[b0+1] = B[b0+1]; 
+      Bs[b0+2] = B[b0+2]; 
+      Bs[b0+3] = B[b0+3]; 
+    } 
+  } 
+ 
+  // Fill AEM matrix. 
+  data->AEM_S(0,0) = 0.7071067811865475*As[0]; 
+  data->AEM_S(0,1) = 0.7071067811865475*As[1]; 
+  data->AEM_S(0,2) = 0.7071067811865475*As[2]; 
+  data->AEM_S(0,3) = 0.7071067811865475*As[3]; 
+  data->AEM_S(1,0) = 0.7071067811865475*As[1]; 
+  data->AEM_S(1,1) = 0.6324555320336759*As[2]+0.7071067811865475*As[0]; 
+  data->AEM_S(1,2) = 0.6210590034081186*As[3]+0.6324555320336759*As[1]; 
+  data->AEM_S(1,3) = 0.6210590034081186*As[2]; 
+  data->AEM_S(2,0) = 0.7071067811865475*As[2]; 
+  data->AEM_S(2,1) = 0.6210590034081186*As[3]+0.6324555320336759*As[1]; 
+  data->AEM_S(2,2) = 0.4517539514526256*As[2]+0.7071067811865475*As[0]; 
+  data->AEM_S(2,3) = 0.421637021355784*As[3]+0.6210590034081186*As[1]; 
+  data->AEM_S(3,0) = 0.7071067811865475*As[3]; 
+  data->AEM_S(3,1) = 0.6210590034081186*As[2]; 
+  data->AEM_S(3,2) = 0.421637021355784*As[3]+0.6210590034081186*As[1]; 
+  data->AEM_S(3,3) = 0.421637021355784*As[2]+0.7071067811865475*As[0]; 
+ 
+  for(unsigned short int vd=0; vd<Ncomp; vd++) 
+  { 
+    b0 = 4*vd; 
+    // Fill BEV. 
+    data->BEV_S << Bs[b0],Bs[b0+1],Bs[b0+2],Bs[b0+3]; 
+ 
+    // Solve the system of equations. 
+    data->u_S = data->AEM_S.colPivHouseholderQr().solve(data->BEV_S); 
+ 
+    // Copy data from Eigen vector. 
+    Eigen::Map<VectorXd>(out+vd*4,4,1) = data->u_S; 
   } 
 } 
  
@@ -232,6 +341,32 @@ void CartFieldBinOpDotProduct1xSer_P2(binOpData_t* data, const double *A, const 
     out[0] += 0.7071067811865475*A[a0+2]*B[a0+2]+0.7071067811865475*A[a0+1]*B[a0+1]+0.7071067811865475*A[a0]*B[a0]; 
     out[1] += 0.6324555320336759*A[a0+1]*B[a0+2]+0.6324555320336759*B[a0+1]*A[a0+2]+0.7071067811865475*A[a0]*B[a0+1]+0.7071067811865475*B[a0]*A[a0+1]; 
     out[2] += 0.4517539514526256*A[a0+2]*B[a0+2]+0.7071067811865475*A[a0]*B[a0+2]+0.7071067811865475*B[a0]*A[a0+2]+0.6324555320336759*A[a0+1]*B[a0+1]; 
+  } 
+ 
+} 
+ 
+void CartFieldBinOpDotProduct1xSer_P3(binOpData_t* data, const double *A, const double *B, const short int Ncomp, const short int eqNcomp, double *out) 
+{ 
+  // A:       scalar/vector field. 
+  // B:       scalar/vector field (must be vector if A is vector). 
+  // Ncomp:   number of components of B (could be 1D, 2D, 3D, vector). 
+  // eqNcomp: =1 if A:numComponents=B:numComponents, =0 else (should be 1 here). 
+  // out:     output field A.B (out only has one component). 
+ 
+  // zero out. This is ok in this operator because there is no in-place dot-product. 
+  for (unsigned short int vd=0; vd<4; vd++) 
+  { 
+    out[vd] = 0.0; 
+  } 
+ 
+  for (unsigned short int vd=0; vd<Ncomp; vd++) 
+  { 
+    unsigned short int a0 = 4*vd; 
+    // Contribution to dot-product from weak multiplication of vd component. 
+    out[0] += 0.7071067811865475*A[a0+3]*B[a0+3]+0.7071067811865475*A[a0+2]*B[a0+2]+0.7071067811865475*A[a0+1]*B[a0+1]+0.7071067811865475*A[a0]*B[a0]; 
+    out[1] += 0.6210590034081186*A[a0+2]*B[a0+3]+0.6210590034081186*B[a0+2]*A[a0+3]+0.6324555320336759*A[a0+1]*B[a0+2]+0.6324555320336759*B[a0+1]*A[a0+2]+0.7071067811865475*A[a0]*B[a0+1]+0.7071067811865475*B[a0]*A[a0+1]; 
+    out[2] += 0.421637021355784*A[a0+3]*B[a0+3]+0.6210590034081186*A[a0+1]*B[a0+3]+0.6210590034081186*B[a0+1]*A[a0+3]+0.4517539514526256*A[a0+2]*B[a0+2]+0.7071067811865475*A[a0]*B[a0+2]+0.7071067811865475*B[a0]*A[a0+2]+0.6324555320336759*A[a0+1]*B[a0+1]; 
+    out[3] += 0.421637021355784*A[a0+2]*B[a0+3]+0.7071067811865475*A[a0]*B[a0+3]+0.421637021355784*B[a0+2]*A[a0+3]+0.7071067811865475*B[a0]*A[a0+3]+0.6210590034081186*A[a0+1]*B[a0+2]+0.6210590034081186*B[a0+1]*A[a0+2]; 
   } 
  
 } 
