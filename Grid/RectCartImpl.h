3931a3e54bf9bebcbd9bcbb0d384601cf06849b9
Noah Mandell <nrmandell@gmail.com>
2019-01-25 18:54:26 -0500

    forgot to add some files...

---
+++ Grid/RectCartImpl.h
@@ -0,0 +1,8 @@
+#ifndef RECT_CART_IMPL_H
+#define RECT_CART_IMPL_H
+
+extern "C" {
+   void cellCenter(int ndim, double* lower, int* currIdx, double* dx, double* xc);
+}
+
+#endif // RECT_CART_IMPL_H
