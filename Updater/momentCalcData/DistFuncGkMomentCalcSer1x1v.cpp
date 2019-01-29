ff7d307770a0ef0096afb59f97312864c77605b6
Noah Mandell <nrmandell@gmail.com>
2019-01-29 16:48:56 -0500

    generated kernels for ES GK and GK moments that include an extra basis element for vpar^2. need more testing, but preliminary results for etg test look ok.

--- Updater/momentCalcData/DistFuncGkMomentCalcSer1x1v.cpp
+++ Updater/momentCalcData/DistFuncGkMomentCalcSer1x1v.cpp
@@ -32,8 +32,8 @@ void GkMomentCalc1x1vSer_M1proj_P1(const double *w, const double *dxv, const dou
 { 
   const double volFact = dxv[1]/2; 
   const double wx1 = w[1], dv1 = dxv[1]; 
-  out[0] += 1.414213562373095*f[0]*volFact*wx1; 
-  out[1] += 1.414213562373095*f[1]*volFact*wx1; 
+  out[0] += volFact*(1.414213562373095*f[0]*wx1+0.408248290463863*f[2]*dv1); 
+  out[1] += volFact*(1.414213562373095*f[1]*wx1+0.408248290463863*f[3]*dv1); 
 } 
 void GkMomentCalc1x1vSer_M1proj_P2(const double *w, const double *dxv, const double m_, const double *Bmag, const double *f, double *out) 
 { 
@@ -48,7 +48,7 @@ void GkMomentCalc1x1vSer_M2_P1(const double *w, const double *dxv, const double 
   const double volFact = dxv[1]/2; 
   const double wx1 = w[1], dv1 = dxv[1]; 
   const double wx1_sq = wx1*wx1, dv1_sq = dv1*dv1; 
-  out[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.1178511301977579*f[0]*dv1_sq); 
+  out[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.105409255338946*f[4]*dv1_sq+0.1178511301977579*f[0]*dv1_sq); 
   out[1] += volFact*(1.414213562373095*f[1]*wx1_sq+0.8164965809277261*f[3]*dv1*wx1+0.1178511301977579*f[1]*dv1_sq); 
 } 
 void GkMomentCalc1x1vSer_M2_P2(const double *w, const double *dxv, const double m_, const double *Bmag, const double *f, double *out) 
@@ -65,7 +65,7 @@ void GkMomentCalc1x1vSer_M2par_P1(const double *w, const double *dxv, const doub
   const double volFact = dxv[1]/2; 
   const double wx1 = w[1], dv1 = dxv[1]; 
   const double wx1_sq = wx1*wx1, dv1_sq = dv1*dv1; 
-  out[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.1178511301977579*f[0]*dv1_sq); 
+  out[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.105409255338946*f[4]*dv1_sq+0.1178511301977579*f[0]*dv1_sq); 
   out[1] += volFact*(1.414213562373095*f[1]*wx1_sq+0.8164965809277261*f[3]*dv1*wx1+0.1178511301977579*f[1]*dv1_sq); 
 } 
 void GkMomentCalc1x1vSer_M2par_P2(const double *w, const double *dxv, const double m_, const double *Bmag, const double *f, double *out) 
@@ -83,7 +83,7 @@ void GkMomentCalc1x1vSer_M3par_P1(const double *w, const double *dxv, const doub
   const double wx1 = w[1], dv1 = dxv[1]; 
   const double wx1_sq = wx1*wx1, dv1_sq = dv1*dv1; 
   const double wx1_cu = wx1*wx1*wx1, dv1_cu = dv1*dv1*dv1; 
-  out[0] += volFact*(1.414213562373095*f[0]*wx1*wx1_sq+1.224744871391589*f[2]*dv1*wx1_sq+0.3535533905932737*f[0]*dv1_sq*wx1+0.06123724356957942*f[2]*dv1*dv1_sq); 
+  out[0] += volFact*(1.414213562373095*f[0]*wx1*wx1_sq+1.224744871391589*f[2]*dv1*wx1_sq+0.3162277660168379*f[4]*dv1_sq*wx1+0.3535533905932737*f[0]*dv1_sq*wx1+0.06123724356957942*f[2]*dv1*dv1_sq); 
   out[1] += volFact*(1.414213562373095*f[1]*wx1*wx1_sq+1.224744871391589*f[3]*dv1*wx1_sq+0.3535533905932737*f[1]*dv1_sq*wx1+0.06123724356957942*f[3]*dv1*dv1_sq); 
 } 
 void GkMomentCalc1x1vSer_M3par_P2(const double *w, const double *dxv, const double m_, const double *Bmag, const double *f, double *out) 
@@ -105,7 +105,7 @@ void GkMomentCalc1x1vSer_ThreeMoments_P1(const double *w, const double *dxv, con
   outM0[1] += 1.414213562373095*f[1]*volFact; 
   outM1[0] += 0.2357022603955158*volFact*(6.0*f[0]*wx1+1.732050807568877*f[2]*dv1); 
   outM1[1] += 0.2357022603955158*volFact*(6.0*f[1]*wx1+1.732050807568877*f[3]*dv1); 
-  outM2[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.1178511301977579*f[0]*dv1_sq); 
+  outM2[0] += volFact*(1.414213562373095*f[0]*wx1_sq+0.8164965809277261*f[2]*dv1*wx1+0.105409255338946*f[4]*dv1_sq+0.1178511301977579*f[0]*dv1_sq); 
   outM2[1] += volFact*(1.414213562373095*f[1]*wx1_sq+0.8164965809277261*f[3]*dv1*wx1+0.1178511301977579*f[1]*dv1_sq); 
 } 
 void GkMomentCalc1x1vSer_ThreeMoments_P2(const double *w, const double *dxv, const double m_, const double *Bmag, const double *f, double *outM0, double *outM1, double *outM2) 
