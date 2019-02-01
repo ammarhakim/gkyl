1f861c6fbf59f4c114b7b8062312249e6b6084ba
Petr Cagas <pcagas@vt.edu>
2019-02-01 16:39:31 -0500

    Fixed the bug in MaxwellianOnBasis, which was introduced when the guts of the updater were moved to C kernel. The issue was in messed up indexes.

--- Updater/MaxwellianOnBasisImpl.cpp
+++ Updater/MaxwellianOnBasisImpl.cpp
@@ -1,43 +1,67 @@
 #include <MaxwellianOnBasisImpl.h>
 
-void MaxwellianInnerLoop(double * n, double * u, double * vth2,
-			 double * fItr,
-			 double * weights, double * dz, double * zc,
-			 double * ordinates, double * basisAtOrdinates,
-			 int numBasis, int numConfOrds, int numVelOrds,
-			 int numConfDims, int numVelDims) {
-  double denom, maxwellian, v, v2;
-  int phaseIdx;
-  int numPhaseDims = numConfDims + numVelDims;
+class Indexer {
+private:
+  int nr, nc;
 
-  for (int k = 0; k < numBasis; ++k)
+public:
+  Indexer(int numRows, int numColumns) 
+    : nr(numRows), nc(numColumns) {}
+
+  int get(int row, int column) {
+    return row*nc + column;
+  }
+  
+};
+
+void MaxwellianInnerLoop(/* Number density, drift speed, and thermal velocity squared
+			    n[numConfOrds], u[numConfOrds, numVelDims], vth2[numConfOrds] */
+			 double * n, double * u, double * vth2,
+			 /* Pointer to output Maxwellian f[numPhaseBasis] */
+			 double * fItr,
+			 /* weights[numPhaseOrds], dz[numPhaseDims], zc[numPhaseDims] */
+			 double * weights, double * dz, double * zc,
+			 /* ordinates[numPhaseOrds, numPhaseDims] */
+			 double * ordinates, 
+			 /* basisAtOrdinates[numPhaseOrds, numPhaseBasis] */
+			 double * basisAtOrdinates,
+			 /* phaseToConfOrdMap[numPhaseOrds] */
+			 double * phaseToConfOrdMap,
+			 int numPhaseBasis,
+			 int numConfOrds, int numPhaseOrds,
+			 int numConfDims, int numPhaseDims) {
+
+  int numVelDims = numPhaseDims - numConfDims;
+  Indexer uIdx(numConfOrds, numVelDims);
+  Indexer oIdx(numPhaseOrds, numPhaseDims);
+  Indexer bIdx(numPhaseOrds, numPhaseBasis);
+
+  for (int k = 0; k < numPhaseBasis; ++k)
     fItr[k] = 0;
 
-  // Loop over configuration space ordinates
-  for (int confIdx = 0; confIdx < numConfOrds; ++confIdx) {
+  double maxwellNorm[numConfOrds];
+  for (int confOrdIdx = 0; confOrdIdx < numConfOrds; ++confOrdIdx) {
     // Prepare the Maxvellian normalization
-    denom = 1.0;
+    double denom = 1.0;
     for (int d = 0; d < numVelDims; ++d)
-      denom *= 2 * M_PI * vth2[confIdx];
-    denom = 1 / sqrt(denom);
-
-    // Loop over velocity space ordinates
-    for (int velIdx = 0; velIdx < numVelOrds; ++velIdx) {
-      maxwellian = n[confIdx] * denom;
-      phaseIdx = confIdx*numVelOrds + velIdx;
-
-      v2 = 0;
-      for (int d = 0; d < numVelDims; ++d) {
-	v = ordinates[phaseIdx*numPhaseDims + numConfDims + d];
-	// convert logical to physical coordinates
-	v = 0.5*dz[numConfDims + d]*v + zc[numConfDims + d];
-	v2 += (v - u[confIdx*numVelDims + d]) * (v - u[confIdx*numVelDims + d]);
-      }
-      maxwellian *= exp(-0.5 * v2 / vth2[confIdx]);
-
-      for (int k = 0; k < numBasis; ++k) 
-	fItr[k] +=
-	  weights[phaseIdx] * maxwellian * basisAtOrdinates[phaseIdx*numBasis + k];
+      denom *= 2*M_PI*vth2[confOrdIdx];
+    maxwellNorm[confOrdIdx] = n[confOrdIdx]/std::sqrt(denom);
+  }
+  
+  for (int phaseOrdIdx = 0; phaseOrdIdx < numPhaseOrds; ++phaseOrdIdx) {
+    int confOrdIdx = phaseToConfOrdMap[phaseOrdIdx]-1;
+    double maxwellian = maxwellNorm[confOrdIdx];
+    double v2 = 0;
+    for (int d = 0; d < numVelDims; ++d) {
+      double v = ordinates[oIdx.get(phaseOrdIdx, numConfDims+d)];
+      // convert logical to physical coordinates
+      v = 0.5*v*dz[numConfDims+d] + zc[numConfDims+d] - u[uIdx.get(confOrdIdx, d)];
+      v2 += v*v;
     }
+    maxwellian *= exp(-0.5*v2/vth2[confOrdIdx]);
+    
+    for (int k = 0; k < numPhaseBasis; ++k) 
+      fItr[k] +=
+	weights[phaseOrdIdx]*maxwellian*basisAtOrdinates[bIdx.get(phaseOrdIdx, k)];
   }
 }
