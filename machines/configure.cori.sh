75d4919be08d20e00cfd0aad886cbff95103e27a
Ammar <a.hakim777@gmail.com>
2019-02-14 14:22:29 -0800

    - Removed check for CXXFLAGS. No need to test for these. If a compiler does not have -O3 I think it is pointless building with it
    - No need to specify ADIOS directory for Cori. We need to use our own build for this. Now Cori builds should be out-of-the-box. Thanks for Mana for fiddling around with this stuff

--- machines/configure.cori.sh
+++ machines/configure.cori.sh
@@ -2,11 +2,12 @@
 
 # Edit the paths and options in the following command to suit your system
 module load cray-mpich
-module load adios
+module unload adios
 module load eigen3
-module load zlib
-CC=icc
-CXX=icpc
+module unload zlib
+module unload darshan
+CC=cc
+CXX=CC
 MPICC=cc
 MPICXX=CC
 
@@ -24,9 +25,9 @@ MPI_LINK_LIBS="mpich"
 
 # ADIOS options
 ENABLE_ADIOS="--enable-adios" 
-ADIOS_INC_DIR=$ADIOS_DIR/include
-ADIOS_LIB_DIR="$ADIOS_DIR/lib,$ZLIB_DIR,/usr/common/software/minixml/2.9/hsw//intel/lib"
-ADIOS_LINK_LIBS="z,mxml"
+ADIOS_INC_DIR=
+ADIOS_LIB_DIR=
+ADIOS_LINK_LIBS=
 
 # EIGEN options
 EIGEN_INC_DIR=$EIGEN3_DIR/include/eigen3
@@ -46,6 +47,6 @@ LUAJIT_LIB_DIR=$GKYLSOFT/luajit/lib
 LUAJIT_SHARE_DIR=$GKYLSOFT/luajit/share/luajit-2.1.0-beta3
 
 # You probably do not need to modify the command itself
-cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR --adios-link-libs=$ADIOS_LINK_LIBS --eigen-inc-dir=$EIGEN_INC_DIR configure"
+cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR  $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --eigen-inc-dir=$EIGEN_INC_DIR configure"
 echo $cmd
 $cmd
