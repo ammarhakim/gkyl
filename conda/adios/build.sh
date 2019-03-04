45747774e5ad52d3cde167c019d30da4ac6d7c27
Petr Cagas <pcagas@vt.edu>
2019-03-04 14:21:47 -0500

    rearragngin conda dependencies chain to avoid installing MPI

--- conda/adios/build.sh
+++ conda/adios/build.sh
@@ -1,6 +1,6 @@
-curl -L http://users.nccs.gov/~pnorbert/adios-1.13.0.tar.gz > adios-1.13.0.tar.gz
-gunzip adios-1.13.0.tar.gz
-tar -xvf adios-1.13.0.tar
-cd adios-1.13.0
-./configure --prefix=$PREFIX --disable-fortran CFLAGS="-fPIC" CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --enable-shared=no
+curl -L http://users.nccs.gov/~pnorbert/adios-1.13.1.tar.gz > adios-1.13.1.tar.gz
+gunzip adios-1.13.1.tar.gz
+tar -xvf adios-1.13.1.tar
+cd adios-1.13.1
+./configure --prefix=$PREFIX --disable-fortran --without-netcdf CFLAGS="-fPIC" CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --enable-shared=no
 make install
