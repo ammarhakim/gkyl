5e16c8e8ec49653239a795c704be0bd7b441b2fb
Ammar <a.hakim777@gmail.com>
2019-03-12 18:14:03 -0400

    Added an "examples" tool to the system. Now one can get in-build examples from installed Gkeyll package, including description of the example. One hopes there will be many good examples added to the system soon. A good example should be easy to run (though unlike a regression test it could take longer or need parallel resources to run) and should come with a brief description.
    The description should tell the user what the example does, how to
    make some typical plots, what resources are needed to run (specially
    if this is a parallel simulation) and other things. The description
    should be in markdown format. Equations can be included. Some users
    may want to pipe the description to pandoc and save the output as PDF
    or Word document and would hence would get nicely rendered equations.

---
+++ Tool/examples/es-two-stream.lua
@@ -0,0 +1,63 @@
+-- Gkyl ------------------------------------------------------------------------
+local Plasma = require("App.PlasmaOnCartGrid").VlasovMaxwell
+
+knumber = 0.5 -- wave-number
+elVTerm = 0.2 -- electron thermal velocity
+vDrift = 1.0 -- drift velocity
+perturbation = 1.0e-6 -- distribution function perturbation
+
+vlasovApp = Plasma.App {
+   logToFile = true,
+
+   tEnd = 50.0, -- end time
+   nFrame = 5, -- number of output frames
+   lower = {-math.pi/knumber}, -- configuration space lower left
+   upper = {math.pi/knumber}, -- configuration space upper right
+   cells = {64}, -- configuration space cells
+   basis = "serendipity", -- one of "serendipity" or "maximal-order"
+   polyOrder = 2, -- polynomial order
+   timeStepper = "rk3", -- one of "rk2" or "rk3"
+
+   -- decomposition for configuration space
+   decompCuts = {1}, -- cuts in each configuration direction
+   useShared = false, -- if to use shared memory
+
+   -- boundary conditions for configuration space
+   periodicDirs = {1}, -- periodic directions
+
+   -- electrons
+   elc = Plasma.Species {
+      charge = -1.0, mass = 1.0,
+      -- velocity space grid
+      lower = {-6.0},
+      upper = {6.0},
+      cells = {32},
+      decompCuts = {1},
+      -- initial conditions
+      init = function (t, xn)
+	 local x, v = xn[1], xn[2]
+	 local alpha = perturbation
+	 local k = knumber
+	 local vt = elVTerm
+	 
+	 local fv = 1/math.sqrt(8*math.pi*vt^2)*(math.exp(-(v-vDrift )^2/(2*vt^2))+math.exp(-(v+vDrift)^2/(2*vt^2)))
+	 return (1+alpha*math.cos(k*x))*fv
+      end,
+      evolve = true, -- evolve species?
+
+      diagnosticMoments = { "M0", "M1i", "M2" }
+   },
+
+   -- field solver
+   field = Plasma.Field {
+      epsilon0 = 1.0, mu0 = 1.0,
+      init = function (t, xn)
+	 local alpha = perturbation
+	 local k = knumber
+	 return -alpha*math.sin(k*xn[1])/k, 0.0, 0.0, 0.0, 0.0, 0.0
+      end,
+      evolve = true, -- evolve field?
+   },
+}
+-- run application
+vlasovApp:run()
