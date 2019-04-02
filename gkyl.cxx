beff7f858ca13865bbcfe0c0d915e4a681a0a63e
Petr Cagas <pcagas@vt.edu>
2019-04-02 12:46:17 -0400

    Rerolling changes to gkyl.cxx from e2448fa. It resulted in Gkeyll outputing to current working directory instead of the directory where is the input file located. This broke regression tests.

--- gkyl.cxx
+++ gkyl.cxx
@@ -155,19 +155,25 @@ createTopLevelDefs(int argc, char **argv, const std::string& inpFile,
   varDefs << "jit.opt.start('callunroll=20', 'loopunroll=60', 'maxmcode=40960', 'maxtrace=8000', 'maxrecord=16000', 'minstitch=3')"
           << std::endl;
 
-  std::string myInpFile(inpFile);
+  // This is change from commit e2448fa. Rerolling it back because of unforseen consequences
+  // std::string myInpFile(inpFile);
 
-  // figure out output prefix. This is complicated by the fact the
-  // input file (or tool file) may be specified via full path
-  auto const slashPos = myInpFile.find_last_of('/');
-  if (std::string::npos != slashPos)
-    myInpFile = myInpFile.substr(slashPos+1);
+  // // figure out output prefix. This is complicated by the fact the
+  // // input file (or tool file) may be specified via full path
+  // auto const slashPos = myInpFile.find_last_of('/');
+  // if (std::string::npos != slashPos)
+  //   myInpFile = myInpFile.substr(slashPos+1);
 
-  std::string snm(myInpFile);
-  auto const trunc = myInpFile.find_last_of(".", snm.size());
-  if (std::string::npos != trunc)
+  // std::string snm(myInpFile);
+  // auto const trunc = myInpFile.find_last_of(".", snm.size());
+  // if (std::string::npos != trunc)
+  std::string snm(inpFile);
+  unsigned trunc = inpFile.find_last_of(".", snm.size());
+  if (trunc > 0)
     snm.erase(trunc, snm.size());
   varDefs << "GKYL_OUT_PREFIX = '" << snm << "'" << std::endl;
+
+  std::cout << snm << std::endl;
 
   varDefs << "GKYL_COMMANDS = {}" << std::endl;
   // just append list of commands 
