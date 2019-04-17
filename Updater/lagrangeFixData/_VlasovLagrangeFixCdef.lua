d355a44f658ada9ee951697ab1511379b4c02da1
Petr Cagas <pcagas@vt.edu>
2019-04-17 12:56:47 -0400

    Generalized Lagrange multiplier fix kernels restructured and added for 1X3V and 2X3V.

--- Updater/lagrangeFixData/_VlasovLagrangeFixCdef.lua
+++ Updater/lagrangeFixData/_VlasovLagrangeFixCdef.lua
@@ -5,24 +5,60 @@ void VlasovLagrangeFixSer1x1v1p(double *dm0, double *dm1, double *dm2, double *l
 
 void VlasovLagrangeFixSer1x1v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
+void VlasovLagrangeFixSer1x1v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
 void VlasovLagrangeFixSer1x2v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixSer1x2v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer1x2v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer1x3v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer1x3v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer1x3v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixSer2x2v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixSer2x2v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
+void VlasovLagrangeFixSer2x2v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer2x3v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer2x3v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixSer2x3v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
 void VlasovLagrangeFixMax1x1v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixMax1x1v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax1x1v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixMax1x2v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixMax1x2v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
+void VlasovLagrangeFixMax1x2v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax1x3v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax1x3v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax1x3v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
 void VlasovLagrangeFixMax2x2v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 void VlasovLagrangeFixMax2x2v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax2x2v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax2x3v1p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax2x3v2p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
+
+void VlasovLagrangeFixMax2x3v3p(double *dm0, double *dm1, double *dm2, double *lo, double *L, double *Nv, double *vc, double *f);
 
 ]]
