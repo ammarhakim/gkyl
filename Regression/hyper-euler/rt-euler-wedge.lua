217b4b5393f7b093529d6b7b5456f85c7111ee12
Liang Wang <1000547+liangwang0734@users.noreply.github.com>
2019-04-18 12:08:15 -0400

    Regression/rt-euler-wedge: clean-up.

--- Regression/hyper-euler/rt-euler-wedge.lua
+++ Regression/hyper-euler/rt-euler-wedge.lua
@@ -11,14 +11,6 @@ csIn = math.sqrt(gasGamma*prIn/rhoIn)
 uIn = 8.0*csIn -- supersonic inflow
 erIn = 0.5*rhoIn*uIn^2 + prIn/(gasGamma-1)
 
--- resolution and time-stepping
-NX = 200
-NY = 100
-cfl = 0.9
-tStart = 0.0
-tEnd = 5*2.0/uIn
-nFrames = 10
-
 function wedge(x, y, x0, y0, angle)
    return (
       x>x0 and (y<y0+(x-x0)*math.tan(0.5*angle)) and (y>y0-(x-x0)*math.tan(0.5*angle))
@@ -29,12 +21,12 @@ end
 eulerApp = Moments.App {
    logToFile = true,
 
-   tEnd = tEnd, -- end time
-   nFrame = nFrames, -- number of output frame
+   tEnd = 5*2.0/uIn, -- end time
+   nFrame = 10, -- number of output frame
    lower = {-0.1, 0.0}, -- lower left corner
    upper = {1.0, 0.55}, -- upper right corner
-   cells = {NX, NY}, -- number of cells
-   cflFrac = cfl, -- CFL fraction
+   cells = {200, 100}, -- number of cells
+   cflFrac = 0.9, -- CFL fraction
    timeStepper = "fvDimSplit",
    
    -- decomposition stuff
@@ -47,26 +39,32 @@ eulerApp = Moments.App {
 
       equation = Euler { gasGamma = gasGamma },
       equationInv = Euler { gasGamma = gasGamma, flux="lax" },
+      forceInv = true,
+
       -- initial conditions
       init = function (t, xn)
          return 1e-6*rhoIn, 0.0, 0.0, 0.0, 1e-6*erIn
       end,
       evolve = true, -- evolve species?
 
+      -- outer boundary conditions
       bcx = { Euler.bcConst(rhoIn, rhoIn*uIn, 0.0, 0.0, erIn),
-              Moments.Species.bcCopy }, -- boundary conditions in X
-      bcy = { Moments.Species.bcWall, Moments.Species.bcCopy }, -- boundary conditions in X
+              Moments.Species.bcCopy },
+      bcy = { Moments.Species.bcWall, Moments.Species.bcCopy },
 
+      -- has interior (embedded) boundary conditions?
       hasSsBnd = true,
+      -- mask defining the embedded boundary
       inOutFunc = function (t, xn)
          local x, y = xn[1], xn[2]
          local xc, yc = 0.0, 0.0
          local rad = 0.5
          return  wedge(x,y, 0.0, 0.0, 30*math.pi/180) and -1.0 or 1.0
       end,
-
-      ssBc = { Moments.Species.bcWall }, -- embedded boundary conditions
+      -- boundary conditions to be applied on the embedded boundary
+      ssBc = { Moments.Species.bcWall },
    },   
 }
+
 -- run application
 eulerApp:run()
