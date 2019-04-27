b567ede90770dd22eefe3a18bfcd8596f35f0a8c
Ammar <a.hakim777@gmail.com>
2019-04-27 11:58:44 -0400

    Unit testing for MPI_Datatype infra. Seeems like sending a contigous block works but not non-contigous block. This potentially narrows down the issue and will help debug

--- Unit/test_Mpi.lua
+++ Unit/test_Mpi.lua
@@ -856,73 +856,25 @@ function test_15(comm, numComponents, ordering)
 
    -- X direction
    if rnk == 0 then
-      -- left edge
-      Mpi.Send(buff:data(), 1, dTypeX, 1, 42, comm)
-      -- right edge
-      local ix, iy = range:upper(1)-nlayer+1, range:lower(2)
-      Mpi.Send(buff:data()+cidx(ix,iy,1)-1, 1, dTypeX, 1, 43, comm)
+      -- send box on left side of domain
+      local loc = cidx(rangeX:lower(1), rangeX:lower(2), 0)
+      Mpi.Send(buff:data()+loc, 1, dTypeX, 1, 42, comm)
    else
-      -- left edge
-      Mpi.Recv(buff:data(), 1, dTypeX, 0, 42, comm, nil)
-      -- right edge
-      local ix, iy = range:upper(1)-nlayer+1, range:lower(2)
-      Mpi.Recv(buff:data()+cidx(ix,iy,1)-1, 1, dTypeX, 0, 43, comm, nil)
+      -- recv box on right side of domain
+      local loc = cidx(range:upper(1), rangeX:lower(2), 0)
+      Mpi.Recv(buff:data()+loc, 1, dTypeX, 0, 42, comm, nil)
 
-      for n = 1, nlayer do
-   	 local i = range:lower(1)+n-1
-   	 for j = range:lower(2), range:upper(2) do
-   	    for k = 1, numComponents do
-   	       assert_equal(i+20*j+0.5+1000*k, buff[cidx(i,j,k)],
-   			    string.format("Checking lower X-direction send/recv (%d,%d;%d [%d])",
+      for i = range:upper(1), range:upper(1) do
+	 for j = rangeX:lower(2), rangeX:upper(2) do
+	    for k = 1, numComponents do
+	       local isend, jsend = rangeX:lower(1), j
+	       assert_equal(isend+20*jsend+0.5+1000*k, buff[cidx(i,j,k)],
+			    string.format("Checking lower X-direction send/recv (%d,%d;%d [%d])",
 					  i,j,k,cidx(i,j,k)))
-   	    end
-   	 end
-      end
-
-      for n = 1, nlayer do
-      	 i = range:upper(1)-n+1
-      	 for j = range:lower(2), range:upper(2) do
-   	    for k = 1, numComponents do	    
-   	       assert_equal(i+20*j+0.5+1000*k, buff[cidx(i,j,k)],
-   			    string.format("Checking upper X-direction send/recv (%d,%d;%d [%d])",
-					  i,j,k,cidx(i,j,k)))
-   	    end
-      	 end
+	    end
+	 end
       end
    end
-
-   -- Y direction
-   if rnk == 0 then
-      -- bottom edge
-      Mpi.Send(buff:data(), 1, dTypeY, 1, 44, comm)
-      -- top edge
-      local ix, iy = range:lower(1), range:upper(2)-nlayer+1
-      Mpi.Send(buff:data()+cidx(ix,iy,1)-1, 1, dTypeY, 1, 45, comm)
-   else
-      -- bottom edge
-      Mpi.Recv(buff:data(), 1, dTypeY, 0, 44, comm, nil)
-      -- top edge
-      local ix, iy = range:lower(1), range:upper(2)-nlayer+1
-      Mpi.Recv(buff:data()+cidx(ix,iy,1)-1, 1, dTypeY, 0, 45, comm, nil)
-
-      for n = 1, nlayer do
-   	 local j = range:lower(2)+n-1
-   	 for i = range:lower(1), range:upper(1) do
-	    for k = 1, numComponents do
-	       assert_equal(i+20*j+0.5+1000*k, buff[cidx(i,j,k)], "Checking Y-direction send/recv")
-	    end
-   	 end
-      end
-
-      for n = 1, nlayer do
-   	 local j = range:upper(2)-n+1
-   	 for i = range:lower(1), range:upper(1) do
-	    for k = 1, numComponents do
-	       assert_equal(i+20*j+0.5+1000*k, buff[cidx(i,j,k)], "Checking Y-direction send/recv")
-	    end
-   	 end
-      end
-   end   
 end
 
 -- Run tests
@@ -938,16 +890,23 @@ test_8(Mpi.COMM_WORLD)
 test_9(Mpi.COMM_WORLD)
 test_10(Mpi.COMM_WORLD)
 test_11(Mpi.COMM_WORLD)
+
 test_12(Mpi.COMM_WORLD, 1, Range.rowMajor)
 test_12(Mpi.COMM_WORLD, 1, Range.colMajor)
 test_12(Mpi.COMM_WORLD, 2, Range.rowMajor)
 test_12(Mpi.COMM_WORLD, 2, Range.colMajor)
 test_12(Mpi.COMM_WORLD, 3, Range.rowMajor)
 test_12(Mpi.COMM_WORLD, 3, Range.colMajor)
+
 test_13(Mpi.COMM_WORLD, 1, 2, Range.rowMajor)
+
 test_14(Mpi.COMM_WORLD, 1, 2, Range.colMajor)
 test_14(Mpi.COMM_WORLD, 1, 2, Range.rowMajor)
---test_15(Mpi.COMM_WORLD, 2, Range.rowMajor)
+
+test_15(Mpi.COMM_WORLD, 1, Range.rowMajor)
+test_15(Mpi.COMM_WORLD, 2, Range.rowMajor)
+--test_15(Mpi.COMM_WORLD, 1, Range.colMajor)
+--test_15(Mpi.COMM_WORLD, 2, Range.colMajor)
 
 function allReduceOneInt(localv)
    local sendbuf, recvbuf = new("int[1]"), new("int[1]")
