30ea729ba54d5a0a10f191d0df82c47dbfe81632
Noah Mandell <nrmandell@gmail.com>
2019-05-14 22:52:32 -0400

    adding CC and CXX flags for openmpi build

--- machines/mkdeps.macosx.sh
+++ machines/mkdeps.macosx.sh
@@ -6,6 +6,6 @@ fi
 export GKYLSOFT='~/gkylsoft'
 cd install-deps
 # first build OpenMPI
-./mkdeps.sh --build-openmpi=yes
+./mkdeps.sh CC=clang CXX=clang++ --build-openmpi=yes
 # now build rest of packages
 ./mkdeps.sh CC=clang CXX=clang++ MPICC=$GKYLSOFT/openmpi-3.1.2/bin/mpicc MPICXX=$GKYLSOFT/openmpi-3.1.2/bin/mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes
