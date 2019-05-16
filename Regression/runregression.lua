524623d3423d1346f527e2d21490f9449fcde134
Ammar <a.hakim777@gmail.com>
2019-05-16 14:03:51 -0400

    - Added ability to specify meta-data in the AdiosCartField   object. Still need to hook into the CartField by adding a metaData   field to it. Tested and it seemes to work. Still need to modify   various App objects to annotate the output with polyOrder and   basisType fields.
    - Fixed bug in runregression code that was only computing the last
      relative errors. Now max of the error is computed. No tests are
      impacted

--- Regression/runregression.lua
+++ Regression/runregression.lua
@@ -321,10 +321,10 @@ local function compareFiles(f1, f2)
       -- compare CartField
       local d1, d2 = r1:getVar("CartGridField"):read(), r2:getVar("CartGridField"):read()
 
-      if d1:size() ~= d2:size() then return false end 
+      if d1:size() ~= d2:size() then return false end
       for i = 1, d1:size() do
 	 if check_equal_numeric(d1[i], d2[i]) == false then
-	    currMaxDiff = get_relative_numeric(d1[i], d2[i])
+	    currMaxDiff = math.max(currMaxDiff, get_relative_numeric(d1[i], d2[i]))
 	    cmpPass = false
 	 end
       end
@@ -334,7 +334,7 @@ local function compareFiles(f1, f2)
       if d1:size() ~= d2:size() then return false end
       for i = 1, d1:size() do
 	 if check_equal_numeric(d1[i], d2[i]) == false then
-	    currMaxDiff = get_relative_numeric(d1[i], d2[i])
+	    currMaxDiff = math.max(currMaxDiff, get_relative_numeric(d1[i], d2[i]))
 	    cmpPass = false
 	 end
       end
