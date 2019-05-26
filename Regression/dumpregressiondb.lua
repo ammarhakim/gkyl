21fba24dc32c5b9f3ddacdfc541d0945efeed5d2
Ammar <a.hakim777@gmail.com>
2019-05-26 10:42:11 -0400

    ci before mv

--- Regression/dumpregressiondb.lua
+++ Regression/dumpregressiondb.lua
@@ -1,13 +1,15 @@
 -- Gkyl ------------------------------------------------------------------------
 --
--- Debugging script to dump tables created by runregression code
+-- Query code to get information from DB created by runregression code
 --
 --    _______     ___
 -- + 6 @ |||| # P ||| +
 --------------------------------------------------------------------------------
 
+local argparse = require "Lib.argparse"
 local sql = require "sqlite3"
 
+-- load configuration file created by regression testing system
 local f = loadfile("runregression.config.lua")
 if not f then
    log("Regression tests not configured! Run config command first\n")
@@ -16,10 +18,16 @@ end
 configVals = f()
 
 -- open connection
-sqlConn = sql.open(string.format("%s/regressiondb", configVals['results_dir']))
+local sqlConn = sql.open(string.format("%s/regressiondb", configVals['results_dir']), "ro")
 
-sqlConn "select * from RegressionMeta"
+-- Create CLI parser to handle commands and options
+local parser = argparse()
+   :name("queryrdb")
+   :require_command(true)
+   :description [[
+Query database created by runregression system to extract information
+about various tests.
+]]
 
-print("\n\n")
-
---sqlConn "select * from RegressionData"
+-- parse command-line args (functionality encoded in command actions)
+local _ = parser:parse(GKYL_COMMANDS)
