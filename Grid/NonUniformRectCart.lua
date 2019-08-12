dc4ee3632d5e2480676534a8b096ca3c30509840
Liang Wang <1000547+liangwang0734@users.noreply.github.com>
2019-08-12 14:12:14 -0400

    Grid/NonUniformRectCart.lua: write method
    - Copy-and-paste from DataStruct/DynVector.lua
    - Only rank=0 node writes
    - Write to a bp file with coordinates stored in datasets like axis1, axis2, ...

--- Grid/NonUniformRectCart.lua
+++ Grid/NonUniformRectCart.lua
@@ -15,6 +15,29 @@ local Mpi = require "Comm.Mpi"
 local Proto = require "Lib.Proto"
 local Range = require "Lib.Range"
 local RectCart = require "Grid.RectCart"
+local Alloc = require "Lib.Alloc"
+local Adios = require "Io.Adios"
+local Mpi = require "Comm.Mpi"
+
+-- Code from Lua wiki to convert table to comma-seperated-values
+-- string.
+-- Used to escape "'s by toCSV
+local function escapeCSV (s)
+  if string.find(s, '[,"]') then
+    s = '"' .. string.gsub(s, '"', '""') .. '"'
+  end
+  return s
+end
+-- Convert from table to CSV string
+local function toCSV (tt)
+  local s = ""
+  -- ChM 23.02.2014: changed pairs to ipairs assumption is that
+  -- fromCSV and toCSV maintain data as ordered array
+  for _,p in ipairs(tt) do  
+    s = s .. "," .. escapeCSV(p)
+  end
+  return string.sub(s, 2)      -- remove first comma
+end
 
 -- NonUniformRectCartGrid ------------------------------------------------------
 --
@@ -71,6 +94,17 @@ function NonUniformRectCart:init(tbl)
 	    mapFunc, self._nodeCoords[d])
       end
    end
+
+   -- write only from rank-0: create sub-communicator and use that for
+   -- writing data (perhaps one needs a user-specified write-rank)
+   local ranks = Lin.IntVec(1); ranks[1] = 0
+   self._ioComm = Mpi.Split_comm(Mpi.COMM_WORLD, ranks)
+   self._ioBuff = {}
+
+   -- allocate space for IO buffer
+   for d = 1, ndim do
+     self._ioBuff[d] = Alloc.Double()
+   end
 end
 
 -- member functions
@@ -99,6 +133,43 @@ function NonUniformRectCart:cellVolume()
       v = v*self:dx(d)
    end
    return v
+end
+
+function NonUniformRectCart:write(fName)
+   local comm = self._ioComm
+   local rank = Mpi.Comm_rank(Mpi.COMM_WORLD)
+   if rank ~= 0 then  -- only run on rank 0 ...
+     return
+   end
+
+   -- setup ADIOS for IO
+   Adios.init_noxml(comm[0])
+
+   -- create group and set I/O method
+   local grpId = Adios.declare_group("NonUniformRectCart", "", Adios.flag_no)
+   Adios.select_method(grpId, "MPI", "", "")
+   
+   for d = 1, self._ndim do
+      -- ADIOS expects CSV string to specify data shape
+      local localDatSz = toCSV( {#self._nodeCoords[d]} )
+      -- define data to write
+      Adios.define_var(
+         grpId, string.format("axis%d", d), "", Adios.double, localDatSz, "", "")
+   end
+
+   local fullNm = fName
+   local fd = Adios.open("NonUniformRectCart", fullNm, "w", comm[0])
+   for d = 1, self._ndim do
+      local buff = self._ioBuff[d]
+      local coords = self._nodeCoords[d]
+      buff:expand(#coords)
+      for i = 1, #coords do
+         buff[i] = coords[i]
+      end
+      Adios.write(fd, string.format("axis%d", d), buff:data())
+   end
+   Adios.close(fd)
+   Adios.finalize(rank)
 end
 
 return NonUniformRectCart
