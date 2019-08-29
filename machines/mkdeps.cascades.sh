50ac0772a0705d26c259d30032a4346f16e288e8
Petr Cagas <pcagas@vt.edu>
2019-08-29 17:29:30 -0400

    adding mkdeps script for Cascades

---
+++ machines/mkdeps.cascades.sh
@@ -0,0 +1,11 @@
+module purge
+module load intel/17.0
+module load openmpi/3.1.2
+# if we are in machines directory, go up a directory
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
+export GKYLSOFT='~/gkylsoft'
+cd install-deps
+./mkdeps.sh CC=icc CXX=icpc MPICC=mpicc MPICXX=mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes --build-openmpi=no
