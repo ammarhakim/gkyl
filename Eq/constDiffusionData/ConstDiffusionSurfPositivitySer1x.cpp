cc58386ff0a101ab376ea94006210ca162bf97c0
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2019-09-01 17:16:24 -0400

    Clean up positivity option for ConstDiffusion equation. Add boundary surface term to ConstDiffusion so it can be used with non-periodic BCs. ConstDiffusion (w/o positivity) mysteriously works with the top-hat+periodic BCs, but I suspect it is not accurate. This becomes problematic with positivity=true, in which solution diverges. But if positivity=true and BCs are not periodic (this is the correct choice), top-hat relaxation proceeds as expected and control points remain positive.

---
+++ Eq/constDiffusionData/ConstDiffusionSurfPositivitySer1x.cpp
@@ -0,0 +1,55 @@
+#include <ConstDiffusionModDecl.h> 
+void ConstDiffusionSurfPositivity1xSer_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double *nu, const double *fl, const double *fr, double *outl, double *outr) 
+{ 
+  // w[1]:      Cell-center coordinates.
+  // dxv[1]:    Cell spacing.
+  // nu[1]:     diffusion coefficient (collisionality).
+  // fl/fr:     Distribution function in left/right cells.
+  // outl/outr: Incremented distribution function in left/right cells 
+  double rdxSq2nul = 2.0*nu[0]/(dxvl[0]*dxvl[0]); 
+  double rdxSq2nur = 2.0*nu[0]/(dxvr[0]*dxvr[0]); 
+
+  double incr1[2]; 
+  double incr2[2]; 
+
+  if ((-0.408248290463863*fr[1])+0.408248290463863*fl[1]+0.3535533905932737*fr[0]+0.3535533905932737*fl[0] >= 0.0) {
+    incr1[0] = 1.082531754730548*fr[1]+1.082531754730548*fl[1]-1.125*fr[0]+1.125*fl[0]; 
+    incr1[1] = (-1.875*fr[1])-1.875*fl[1]+1.948557158514986*fr[0]-1.948557158514986*fl[0]; 
+
+    incr2[1] = (-1.0*fr[1])+fl[1]+0.8660254037844386*fr[0]+0.8660254037844386*fl[0]; 
+
+    outr[0] += incr1[0]*rdxSq2nur; 
+    outr[1] += incr2[1]*rdxSq2nur+incr1[1]*rdxSq2nur; 
+
+    outl[0] += -1.0*incr1[0]*rdxSq2nul; 
+    outl[1] += incr1[1]*rdxSq2nul-1.0*incr2[1]*rdxSq2nul; 
+  } else {
+
+    double xBar = (0.3061862178478973*fr[1]+0.3061862178478973*fl[1]+0.5303300858899103*fr[0]-0.5303300858899103*fl[0])/(0.5*(2.449489742783178*fr[1]-2.449489742783178*fl[1])-0.25*(4.898979485566357*fr[1]-4.898979485566357*fl[1]-4.242640687119286*fr[0]-4.242640687119286*fl[0]));
+    double xBarSq = xBar*xBar;
+    double g1 = (3.0*xBar)/(1.0-1.0*xBarSq)-(1.0*xBar*xBarSq)/(1.0-1.0*xBarSq);
+
+    if (std::abs(g1) > 1.0e-15) {
+      double g1Sq = g1*g1;
+      incr1[0] = (-(0.25*fr[0]*g1Sq)/std::sinh(g1))-(0.25*fl[0]*g1Sq)/std::sinh(g1); 
+      incr1[1] = (0.4330127018922193*fr[0]*g1Sq)/std::sinh(g1)+(0.4330127018922193*fl[0]*g1Sq)/std::sinh(g1); 
+
+      incr2[1] = (0.8660254037844386*fr[0]*g1)/std::sinh(g1)+(0.8660254037844386*fl[0]*g1)/std::sinh(g1); 
+
+      outr[0] += incr1[0]*rdxSq2nur; 
+      outr[1] += incr2[1]*rdxSq2nur+incr1[1]*rdxSq2nur; 
+
+      outl[0] += -1.0*incr1[0]*rdxSq2nul; 
+      outl[1] += incr1[1]*rdxSq2nul-1.0*incr2[1]*rdxSq2nul; 
+    } else {
+
+      incr2[1] = (0.8660254037844386*fr[0]*g1)/std::sinh(g1)+(0.8660254037844386*fl[0]*g1)/std::sinh(g1)+fr[1]-1.0*fl[1]; 
+
+      outr[1] += incr2[1]*rdxSq2nur; 
+
+      outl[1] += -1.0*incr2[1]*rdxSq2nul; 
+    };
+
+  };
+
+} 
--- Eq/constDiffusionData/ConstDiffusionSurfSer1x.cpp
+++ Eq/constDiffusionData/ConstDiffusionSurfSer1x.cpp
@@ -1,41 +1,55 @@
 #include <ConstDiffusionModDecl.h> 
 void ConstDiffusionSurf1xSer_X_P1(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double *nu, const double *fl, const double *fr, double *outl, double *outr) 
 { 
-// w[NDIM]: Cell-center coordinates. dxv[NDIM]: Cell spacing. nu[NDIM]: diffusion coefficient (collisionality). fl/fr: Distribution function in left/right cells 
-// outl/outr: Incremented distribution function in left/right cells 
+  // w[1]:      Cell-center coordinates.
+  // dxv[1]:    Cell spacing.
+  // nu[1]:     diffusion coefficient (collisionality).
+  // fl/fr:     Distribution function in left/right cells.
+  // outl/outr: Incremented distribution function in left/right cells 
   double rdxSq2nul = 2.0*nu[0]/(dxvl[0]*dxvl[0]); 
   double rdxSq2nur = 2.0*nu[0]/(dxvr[0]*dxvr[0]); 
 
   double incr1[2]; 
   double incr2[2]; 
-  if ((-0.408248290463863*fr[1])+0.408248290463863*fl[1]+0.3535533905932737*fr[0]+0.3535533905932737*fl[0] >= 0.0) {
+
   incr1[0] = 1.082531754730548*fr[1]+1.082531754730548*fl[1]-1.125*fr[0]+1.125*fl[0]; 
   incr1[1] = (-1.875*fr[1])-1.875*fl[1]+1.948557158514986*fr[0]-1.948557158514986*fl[0]; 
 
   incr2[1] = (-1.0*fr[1])+fl[1]+0.8660254037844386*fr[0]+0.8660254037844386*fl[0]; 
-
-  } else {
-  double xBar = (0.3061862178478973*fr[1]+0.3061862178478973*fl[1]+0.5303300858899103*fr[0]-0.5303300858899103*fl[0])/(0.5*(2.449489742783178*fr[1]-2.449489742783178*fl[1])-0.25*(4.898979485566357*fr[1]-4.898979485566357*fl[1]-4.242640687119286*fr[0]-4.242640687119286*fl[0]));
-  double xBarSq = xBar*xBar;
-  double g1 = (3.0*xBar)/(1.0-1.0*xBarSq)-(1.0*xBar*xBarSq)/(1.0-1.0*xBarSq);
-  if (std::abs(g1) > 1.0e-15) {
-  incr1[0] = (-(0.25*fr[0]*g1*g1)/std::sinh(g1))-(0.25*fl[0]*g1*g1)/std::sinh(g1); 
-  incr1[1] = (0.4330127018922193*fr[0]*g1*g1)/std::sinh(g1)+(0.4330127018922193*fl[0]*g1*g1)/std::sinh(g1); 
-
-  incr2[1] = (0.8660254037844386*fr[0]*g1)/std::sinh(g1)+(0.8660254037844386*fl[0]*g1)/std::sinh(g1); 
-
-  } else {
-
-  incr2[1] = (0.8660254037844386*fr[0]*g1)/std::sinh(g1)+(0.8660254037844386*fl[0]*g1)/std::sinh(g1)+fr[1]-1.0*fl[1]; 
-
-  };
-
-  };
 
   outr[0] += incr1[0]*rdxSq2nur; 
   outr[1] += incr2[1]*rdxSq2nur+incr1[1]*rdxSq2nur; 
 
   outl[0] += -1.0*incr1[0]*rdxSq2nul; 
   outl[1] += incr1[1]*rdxSq2nul-1.0*incr2[1]*rdxSq2nul; 
+
+} 
+void ConstDiffusionSurf1xSer_X_P2(const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double *nu, const double *fl, const double *fr, double *outl, double *outr) 
+{ 
+  // w[1]:      Cell-center coordinates.
+  // dxv[1]:    Cell spacing.
+  // nu[1]:     diffusion coefficient (collisionality).
+  // fl/fr:     Distribution function in left/right cells.
+  // outl/outr: Incremented distribution function in left/right cells 
+  double rdxSq2nul = 2.0*nu[0]/(dxvl[0]*dxvl[0]); 
+  double rdxSq2nur = 2.0*nu[0]/(dxvr[0]*dxvr[0]); 
+
+  double incr1[3]; 
+  double incr2[3]; 
+
+  incr1[0] = (-1.341640786499874*fr[2])+1.341640786499874*fl[2]+2.381569860407206*fr[1]+2.381569860407206*fl[1]-1.875*fr[0]+1.875*fl[0]; 
+  incr1[1] = 2.32379000772445*fr[2]-2.32379000772445*fl[2]-4.125*fr[1]-4.125*fl[1]+3.247595264191645*fr[0]-3.247595264191645*fl[0]; 
+  incr1[2] = (-3.0*fr[2])+3.0*fl[2]+5.325352101035199*fr[1]+5.325352101035199*fl[1]-4.192627457812106*fr[0]+4.192627457812106*fl[0]; 
+
+  incr2[1] = 0.8472151069828725*fr[2]+0.8472151069828725*fl[2]-1.21875*fr[1]+1.21875*fl[1]+0.8660254037844386*fr[0]+0.8660254037844386*fl[0]; 
+  incr2[2] = (-3.28125*fr[2])-3.28125*fl[2]+4.720198453190289*fr[1]-4.720198453190289*fl[1]-3.354101966249685*fr[0]-3.354101966249685*fl[0]; 
+
+  outr[0] += incr1[0]*rdxSq2nur; 
+  outr[1] += incr2[1]*rdxSq2nur+incr1[1]*rdxSq2nur; 
+  outr[2] += incr2[2]*rdxSq2nur+incr1[2]*rdxSq2nur; 
+
+  outl[0] += -1.0*incr1[0]*rdxSq2nul; 
+  outl[1] += incr1[1]*rdxSq2nul-1.0*incr2[1]*rdxSq2nul; 
+  outl[2] += incr2[2]*rdxSq2nul-1.0*incr1[2]*rdxSq2nul; 
 
 } 
