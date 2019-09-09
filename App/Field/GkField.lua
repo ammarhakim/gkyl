3965f5adc2df4f0681dd2e0c66f0f311113f4020
Noah Mandell <nrmandell@gmail.com>
2019-09-09 16:49:02 -0400

    bug fix for when GkField is not evolving and is initialized with initPhiFunc... only first RK field was being initialized. this should only affect passive advection tests.

--- App/Field/GkField.lua
+++ App/Field/GkField.lua
@@ -112,6 +112,7 @@ function GkField:alloc(nRkDup)
    -- allocate fields needed in RK update
    -- nField is related to number of RK stages
    self.potentials = {}
+   self.nRkDup = nRkDup
    for i = 1, nRkDup do
       self.potentials[i] = {}
       self.potentials[i].phi = DataStruct.Field {
@@ -197,7 +198,9 @@ function GkField:initField(species)
          evaluate = self.initPhiFunc,
          projectOnGhosts = true
       }
-      project:advance(0.0, {}, {self.potentials[1].phi})
+      for i = 1, self.nRkDup do
+         project:advance(0.0, {}, {self.potentials[i].phi})
+      end
    else
       -- solve for initial phi
       self:advance(0.0, species, 1, 1)
