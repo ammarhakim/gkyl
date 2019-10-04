87ea7b0b241be7ac6009e9ffe35cd4f77a5dbb9f
Petr Cagas <pcagas@vt.edu>
2019-10-04 15:00:44 -0400

    Cleaned the FPO drag kernel. Hopefully, it's more readable now. It uses intermediate variables for the double recovery of the potential

--- Proto/Fpo/fpoKernelsDecl.h
+++ Proto/Fpo/fpoKernelsDecl.h
@@ -13,9 +13,9 @@ extern "C" {
   
   void fpoDiagKernelP2(const double *dv, const double *vc, const double *f, const double *h, double *out);
 
-  void fpoDragKernelP1(const double dt, const double *dv, const double *f, const double *fL, const double *fR, const double *fT, const double *fB, const double *h, const double *hL, const double *hR, const double *hT, const double *hB, const int isTopEdge, const int isBotEdge, const int isLeftEdge, const int isRightEdge, double *fOut);
+  void fpoDragKernelP1(const double dt, const double *dv, const double *fC, const double *fL, const double *fR, const double *fT, const double *fB, const double *hC, const double *hL, const double *hR, const double *hT, const double *hB, const int isTopEdge, const int isBotEdge, const int isLeftEdge, const int isRightEdge, double *fOut);
 
-  void fpoDragKernelP2(const double dt, const double *dv, const double *f, const double *fL, const double *fR, const double *fT, const double *fB, const double *h, const double *hL, const double *hR, const double *hT, const double *hB, const int isTopEdge, const int isBotEdge, const int isLeftEdge, const int isRightEdge, double *fOut);
+  void fpoDragKernelP2(const double dt, const double *dv, const double *fC, const double *fL, const double *fR, const double *fT, const double *fB, const double *hC, const double *hL, const double *hR, const double *hT, const double *hB, const int isTopEdge, const int isBotEdge, const int isLeftEdge, const int isRightEdge, double *fOut);
 
   void fpoDiffKernelP1(const double dt, const double *dv, const double *f, const double *fL, const double *fR, const double *fT, const double *fB, const double *g, const double *gL, const double *gR, const double *gT, const double *gB, const int isTopEdge, const int isBotEdge, const int isLeftEdge, const int isRightEdge, const int cross, double *fOut);
 
