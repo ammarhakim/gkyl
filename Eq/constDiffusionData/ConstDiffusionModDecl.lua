a067c14b1d8797fad13200c5937e444088652177
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2019-10-06 20:34:18 -0400

    Implement Dirichlet and Neumann BCs for constant diffusion terms using .cpp kernels. Also, the user is expected to indicate the desired Dirichlet/Neumann value in the input file, for example:
    bcx = {{Plasma.IncompEulerSpecies.bcDirichlet, 1.0}, {Plasma.IncompEulerSpecies.bcNeumann,2.0*math.pi-1.0}},
    
    The Dirichlet condition is implemented by constructing a maximal-order
    recovery polynomial across the skin-ghost cells and requiring that it
    matches the Dirichlet value at the boundary, and that the higher
    even/odd moments have odd/even symmetry.
    
    The Neumann condition is implemented by constructing a maximal-order
    recovery polynomial across the skin-ghost cells, and requiring that it
    has all the same derivatives (including the zeroth derivative) at the
    boundary as a polynomial constructed in the skin cell with one more
    degree of freedom (used to match the Neumann value there).
    
    This Neumann option is slightly inferior in the 1x, p=1 vanLeer 2005
    case compared to building a maximal-order polynomial across skin-ghost
    cells and requiring that it matches the Neumann derivative value at the
    boundary, and that it matches the value of the function in the ghost
    cell at the boundary. But it is not clear how to generalize this to
    higher polyOrder or dimensionality.

--- Eq/constDiffusionData/ConstDiffusionModDecl.lua
+++ Eq/constDiffusionData/ConstDiffusionModDecl.lua
@@ -17,13 +17,13 @@ local basisNmMap = { ["serendipity"] = "Ser", ["maximal-order"] = "Max" }
 
 local _M = {}
 
--- Select function to compute volume streaming terms.
+-- Select function to compute volume constDiffusion terms.
 function _M.selectVol(basisNm, CDIM, polyOrder)
    local funcNm = string.format("ConstDiffusionVol%dx%sP%d", CDIM, basisNmMap[basisNm], polyOrder)
    return ffi.C[funcNm]
 end
 
--- Select functions to compute surface streaming terms (output is a table of functions).
+-- Select functions to compute surface constDiffusion terms (output is a table of functions).
 function _M.selectSurf(basisNm, CDIM, polyOrder, applyPos)
    local posString = ""
    if applyPos then posString = "Positivity" end
@@ -42,7 +42,7 @@ function _M.selectSurf(basisNm, CDIM, polyOrder, applyPos)
    end
 end
 
--- Select functions to compute boundary surface streaming terms (output is a table of functions).
+-- Select functions to compute boundary surface constDiffusion terms (output is a table of functions).
 function _M.selectBoundarySurf(basisNm, CDIM, polyOrder, applyPos)
    if CDIM == 1 then
       local funcNmX = string.format("ConstDiffusionBoundarySurf%dx%s_X_P%d", CDIM, basisNmMap[basisNm], polyOrder)
@@ -56,6 +56,29 @@ function _M.selectBoundarySurf(basisNm, CDIM, polyOrder, applyPos)
       local funcNmY = string.format("ConstDiffusionBoundarySurf%dx%s_Y_P%d", CDIM, basisNmMap[basisNm], polyOrder)
       local funcNmZ = string.format("ConstDiffusionBoundarySurf%dx%s_Z_P%d", CDIM, basisNmMap[basisNm], polyOrder)
       return { ffi.C[funcNmX], ffi.C[funcNmY], ffi.C[funcNmZ] }
+   end
+end
+
+-- Select kernels that implement BCs specific to constDiffusion term (output is a table of functions).
+function _M.selectBCs(basisNm, CDIM, polyOrder, bcType)
+   if CDIM == 1 then
+      local funcNmXl = string.format("ConstDiffusionBC%dx%s%s_Xlower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmXu = string.format("ConstDiffusionBC%dx%s%s_Xupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      return { {ffi.C[funcNmXl],ffi.C[funcNmXu]} }                                                                     
+   elseif CDIM == 2 then                                                                                               
+      local funcNmXl = string.format("ConstDiffusionBC%dx%s%s_Xlower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmXu = string.format("ConstDiffusionBC%dx%s%s_Xupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmYl = string.format("ConstDiffusionBC%dx%s%s_Ylower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmYu = string.format("ConstDiffusionBC%dx%s%s_Yupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      return { {ffi.C[funcNmXl],ffi.C[funcNmXu]}, {ffi.C[funcNmYl],ffi.C[funcNmYu]} }                                  
+   elseif CDIM == 3 then                                                                                               
+      local funcNmXl = string.format("ConstDiffusionBC%dx%s%s_Xlower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmXu = string.format("ConstDiffusionBC%dx%s%s_Xupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmYl = string.format("ConstDiffusionBC%dx%s%s_Ylower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmYu = string.format("ConstDiffusionBC%dx%s%s_Yupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmZl = string.format("ConstDiffusionBC%dx%s%s_Zlower_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      local funcNmZu = string.format("ConstDiffusionBC%dx%s%s_Zupper_P%d", CDIM, basisNmMap[basisNm], bcType, polyOrder)
+      return { {ffi.C[funcNmXl],ffi.C[funcNmXu]}, {ffi.C[funcNmYl],ffi.C[funcNmYu]}, {ffi.C[funcNmZl],ffi.C[funcNmZu]} }
    end
 end
 
