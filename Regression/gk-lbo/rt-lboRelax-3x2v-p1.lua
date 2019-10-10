2eaae1f2d33c954105746e95e471a4e4e1ffd8ac
Manaure Francisquez <mana@dartmouth.edu>
2019-10-10 10:45:04 -0400

    Changing initPhiFunc to externalPhi in gkLBO reg tests. It puzzles me why more reg tests didn't fail last night since they all had initPhiFunc (e.g. rt-lboRelax-1x2v-p2.lua).

--- Regression/gk-lbo/rt-lboRelax-3x2v-p1.lua
+++ Regression/gk-lbo/rt-lboRelax-3x2v-p1.lua
@@ -99,7 +99,7 @@ plasmaApp = Plasma.App {
       -- Collisions.
       coll = Plasma.LBOCollisions {
          collideWith = {'square'},
-         frequencies = {nu, },
+         frequencies = {nu},
       },
    },
 
@@ -142,7 +142,7 @@ plasmaApp = Plasma.App {
       -- Collisions.
       coll = Plasma.LBOCollisions {
          collideWith = {'bump'},
-         frequencies = {nu, },
+         frequencies = {nu},
       },
    },
 
@@ -164,24 +164,24 @@ plasmaApp = Plasma.App {
    --    diagnosticMoments = { "GkM0", "GkM1", "GkM2" },
    -- },
 
-   -- field solver
+   -- Field solver.
    field = Plasma.Field {
-      evolve = true, -- evolve fields?
-      initPhiFunc = function (t, xn) return 0.0 end,
+      evolve      = true, -- Evolve fields?
+      externalPhi = function (t, xn) return 0.0 end,
    },
    
-   -- magnetic geometry 
+   -- Magnetic geometry.
    funcField = Plasma.Geometry {
-      -- background magnetic field
+      -- Background magnetic field
       bmag = function (t, xn)
          local x = xn[1]
          return B0
       end,
 
-      -- geometry is not time-dependent
+      -- Geometry is not time-dependent.
       evolve = false,
    },
 
 }
--- run application
+-- Run application.
 plasmaApp:run()
