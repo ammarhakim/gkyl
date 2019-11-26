0bb8986399e66f60773fdcc844a37e46eb8bde68
James L. Juno <jjuno@eddy.princeton.edu>
2019-11-26 18:36:47 -0500

    For some reason Eddy sometimes aggressively looks for ZeroMQ. Now there is the option in the configure stage to disable ZeroMQ. This is actually needed as simulations may fail to run if at runtime the build looks for the ZeroMQ shared libraries and they haven't been built

--- machines/configure.eddy.sh
+++ machines/configure.eddy.sh
@@ -33,10 +33,13 @@ ADIOS_INC_DIR=$HOME/gkylsoft/adios/include
 ADIOS_LIB_DIR=$HOME/gkylsoft/adios/lib
 
 # EIGEN options
-EIGEN_INC_DIR=$HOME/gkylsoft/eigen/include/eigen3
+EIGEN_INC_DIR=$HOME/gkylsoft/eigen3/include/eigen3
+
+# ZeroMQ options
+DISABLE_ZMQ="--disable-zmq"
 
 # You probably do not need to modify the command itself
-cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure"
+cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR $DISABLE_ZMQ configure"
 # if we are in machines directory, go up a directory before executing cmd
 if [ `dirname "$0"` == "." ] 
   then
