3a156f0a78210b51ae9f949bce4f4b77ea8a81f1
ammarhakim <a.hakim777@gmail.com>
2019-12-04 22:36:37 -0500

    Counting total and unit test LOCs

--- count-lines.sh
+++ count-lines.sh
@@ -1,1 +1,8 @@
-wc -l *.cxx *.h *.cpp App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua */*.cpp
+wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp
+
+# total hand-written loc 
+echo "Total LOC, including unit tests"
+wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp | grep total
+
+echo "Unit tests LOC"
+wc -l Unit/*.lua | grep total
