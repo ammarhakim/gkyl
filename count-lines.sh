f7f922fce90233635d3286918480a369dd5f08d6
ammarhakim <a.hakim777@gmail.com>
2019-12-05 22:12:22 -0500

    Line counts

--- count-lines.sh
+++ count-lines.sh
@@ -1,8 +1,8 @@
-wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp
+wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp */*.h
 
 # total hand-written loc 
 echo "Total LOC, including unit tests"
-wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp | grep total
+wc -l *.cxx *.h App/*.lua App/*/*.lua Basis/*.lua Comm/*.lua DataStruct/*.lua Eq/*.lua Grid/*.lua Io/*.lua Lib/*.lua Regression/*.lua Updater/*.lua Proto/*/*.lua Unit/*.lua */*.cpp */*.h | grep total
 
 echo "Unit tests LOC"
 wc -l Unit/*.lua | grep total
