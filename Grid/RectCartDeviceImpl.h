548e4d3df063589076eaeb8ed16171afba272c3e
Noah Mandell <nrmandell@gmail.com>
2019-12-06 12:44:00 -0500

    removing Grid/RectCartDeviceImpl.cu, and moving contents (cellCenter device func) to Grid/RectCartDeviceImpl.h

--- Grid/RectCartDeviceImpl.h
+++ Grid/RectCartDeviceImpl.h
@@ -8,7 +8,12 @@ extern "C"
         int32_t cells[6];
         double lower[6], upper[6];
         double vol, dx[6];
-        __device__ void cellCenter(int* idx, double* xc);
+        __device__ __inline__ void cellCenter(int* idx, double* xc)
+        {
+          for(unsigned int d=0; d<this->ndim; d++) {
+            xc[d] = this->lower[d] + (idx[d]-0.5)*this->dx[d];
+          }
+        }
     } RectCart_t;
 }
 
