568e768680e9743836ef2cf830bf1ddffa4de6d8
Liang Wang <liang.wang.phys@gmail.com>
2020-03-05 23:29:00 -0500

    Msphere: clean up a little

--- App/Special/Msphere.lua
+++ App/Special/Msphere.lua
@@ -3,6 +3,7 @@
 local Proto = require("Proto")
 local Moments = require("App.PlasmaOnCartGrid").Moments
 local Euler = require "Eq.Euler"
+local TenMoment = require "Eq.TenMoment"
 local Constants = require "Constants"
 local Logger = require "Logger"
 local common = require "App.Special.Msphere_common"
@@ -34,7 +35,6 @@ local function buildApp(tbl)
       tbl.massFractions[s] = mass[s] / totalMass
    end
 
-   -- domain and grid
    local R = tbl.planetRadius
    local xlo, xup, nx = tbl.xlo * R, tbl.xup * R, tbl.nx
    local ylo, yup, ny = tbl.ylo * R, tbl.yup * R, tbl.ny
@@ -45,7 +45,6 @@ local function buildApp(tbl)
 
    local coordinateMap = nil
 
-   -- nonuniform grid
    local useNonUniformGrid = default(tbl.useNonUniformGrid, false)
    if useNonUniformGrid then
       local wx, fwx, rx = tbl.wx, tbl.fwx, tbl.rx
@@ -62,15 +61,11 @@ local function buildApp(tbl)
       upper = {1, 1, 1}
    end
 
-   -- computational constants
-   -- i/o control
    local tEnd = tbl.tEnd
    local tFrame = tbl.tFrame
    local nFrame = math.floor(tEnd / tFrame)
 
-   -- derived parameters
    local valuesIn = common.calcValuesIn(tbl)
-
 
    local initMirdip = common.buildInitMirdip(tbl)
 
@@ -78,35 +73,38 @@ local function buildApp(tbl)
    local speciesNames = {}
    for s = 1, nSpecies do
       local moment = tbl.moments[s]
+
+      local model
       if moment == 5 then
-         species[s] = Moments.Species {
+         model = Euler
+      elseif moment == 10 then
+         model = TenMoment
+      end
+
+      species[s] = Moments.Species {
          charge = tbl.charge[s],
          mass = tbl.mass[s],
-         equation = Euler {gasGamma = tbl.gasGamma},
-         equationInv = Euler {gasGamma = tbl.gasGamma, numericalFlux = "lax"},
+         equation = model {gasGamma = tbl.gasGamma},
+         equationInv = model {gasGamma = tbl.gasGamma, numericalFlux = "lax"},
          forceInv = false,
          evolve = true,
 
-         -- initial conditions
          init = function(t, xn)
             local x, y, z = xn[1], xn[2], xn[3]
             return initMirdip.initFluid(x, y, z, s)
          end,
 
          bcx = {
-            Euler.bcConst(unpack(valuesIn[s])),
-            Euler.bcCopy
+            model.bcConst(unpack(valuesIn[s])),
+            model.bcCopy
          },
-         bcy = {Euler.bcCopy, Euler.bcCopy},
-         bcz = {Euler.bcCopy, Euler.bcCopy},
+         bcy = {model.bcCopy, model.bcCopy},
+         bcz = {model.bcCopy, model.bcCopy},
 
          hasSsBnd = true,
          inOutFunc = common.buildInOutFunc(tbl),
          ssBc = {Moments.Species.bcCopy}
       }
-      elseif moment == 10 then
-         assert(false) -- TODO
-      end
       speciesNames[s] = "fluid"..s
    end
 
@@ -133,7 +131,7 @@ local function buildApp(tbl)
          evolve = true,
          init = function(t, xn)
             local x, y, z = xn[1], xn[2], xn[3]
-            return initMirdip.initField(x, y, z, s)
+            return initMirdip.initField(x, y, z, nSpecies + 1)
          end,
          bcx = {
             Moments.Field.bcConst(unpack(valuesIn[nSpecies + 1])),
