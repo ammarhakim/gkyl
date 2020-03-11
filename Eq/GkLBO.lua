2afec22cdc090e28730f5c73b2aaf108a67d94c9
Noah Mandell <nrmandell@gmail.com>
2020-03-11 13:50:15 -0400

    fixing a merge mistake

--- Eq/GkLBO.lua
+++ Eq/GkLBO.lua
@@ -198,13 +198,9 @@ function GkLBO:volTerm(w, dx, idx, f_ptr, out_ptr)
       if ((math.abs(nuUParSum0)<(self._vParMax*self._inNuSum)) and
           (nuVtSqSum0>0) and (nuVtSqSum0<(self._vParMaxSq*self._inNuSum))) then
          cflRate = self._volUpdate(self._inMass, w:data(), dx:data(), self.positivityWeightByDir_ptr:data(), self._BmagInvPtr:data(), self._inNuSum, self._nuUSumPtr:data(), self._nuVtSqSumPtr:data(), f_ptr:data(), self.fRhsVol_ptr:data())
-         -- if using positivity, the volume term will be repeated in step 2, and we will use the cflFreq from there
+         -- if using positivity, the volume term will be repeated in step 2, and we will use the cflRate from there
          -- to avoid double counting, set cflFreq = 0 here
          if self._positivity then cflRate = 0.0 end
-         cflFreq = self._volUpdate(self._inMass, w:data(), dx:data(), self.cflRateByDir_ptr:data(), self._BmagInvPtr:data(), self._inNuSum, self._nuUSumPtr:data(), self._nuVtSqSumPtr:data(), f_ptr:data(), self.fRhsVol_ptr:data())
-         -- if using positivity, the volume term will be repeated in step 2, and we will use the cflFreq from there
-         -- to avoid double counting, set cflFreq = 0 here
-         if self._positivity then cflFreq = 0.0 end
       else
          cflRate = 0.0
          self.primMomCrossLimit = self.primMomCrossLimit+1
