eddd87a34b866f98cadd1dd02be705576b0139df
manauref <mfrancisquez@gmail.com>
2020-04-02 21:24:21 -0700

    Grid refinement/coarsening updater
    Through more testing found some grid combinations that were not done properly. Switch to finding the largest stencil needed (along each direction) by a brute force search. Refinement appears to work on every (2^a)*(3^b) grid combination between 2 and 54 cells in 1D an 2D. More testing is prudent.
    
    Lots of coarsening grid combinations appear to work too. Some do not, e.g. 18 -> 4 cells. The problem appears to be stencil selection. Will perhaps switch to having different stencil selection functions for refinement and coarsening.

--- Updater/CartFieldInterpolate.lua
+++ Updater/CartFieldInterpolate.lua
@@ -1,13 +1,18 @@
 -- Gkyl ------------------------------------------------------------------------
 --
--- Interpolation of a DG field, producing another DG field defined on a finer
--- grid.
+-- Interpolation of a DG field onto a finer or coarser grid.
+-- 
+-- Current limitations:
+--    1) Only does whole grids with same domain (no grid subsets).
+--    2) It may only work for (2^a)*(3^b) grids.
+--    3) Does not always work in both directions, e.g. 48 -> 9 breaks.
 --
--- 1) It's possible that in the future on wishes to pass a grid, or the nodes of
---    some arbitrary grid to interpolate to, instead of passing another field
---    to interpolate to.
--- 2) For now we'll restrict ourselves to the case in which the finer grid has
---    cell lengths that are smaller by an integer number.
+-- Notes:
+--    a] The code below makes references to a coarse and a fine grid. However,
+--       the updater works in either direction.
+--    b] It's possible that in the future on wishes to pass a grid, or the nodes of
+--       some arbitrary grid to interpolate to, instead of passing another field
+--       to interpolate to.
 --
 --    _______     ___
 -- + 6 @ |||| # P ||| +
@@ -22,6 +27,7 @@ local Lin               = require "Lib.Linalg"
 local ffi               = require "ffi"
 local lume              = require "Lib.lume" 
 local Range             = require "Lib.Range"
+local PrimeFactor       = require "Lib.PrimeFactor"
 
 -- Interpolate updater object.
 local CartFieldInterpolate = Proto(UpdaterBase)
@@ -30,16 +36,16 @@ function CartFieldInterpolate:init(tbl)
    CartFieldInterpolate.super.init(self, tbl) -- Setup base object.
 
    local toGrid    = assert(
-      tbl.onGrid, "Updater.CartFieldInterpolate: Must provide grid we'll interpolate to using 'targetGrid'.")
+      tbl.onGrid, "Updater.CartFieldInterpolate: Must provide grid to interpolate to using 'onGrid'.")
 
    local fromGrid  = assert(
-      tbl.fromGrid, "Updater.CartFieldInterpolate: Must provide grid we'll interpolate from using 'originGrid'.")
+      tbl.fromGrid, "Updater.CartFieldInterpolate: Must provide grid to interpolate from using 'fromGrid'.")
 
    local toBasis   = assert(
-      tbl.onBasis, "Updater.CartFieldInterpolate: Must provide basis we'll interpolate to using 'targetBasis'.")
+      tbl.onBasis, "Updater.CartFieldInterpolate: Must provide basis to interpolate to using 'onBasis'.")
 
    local fromBasis = assert(
-      tbl.fromBasis, "Updater.CartFieldInterpolate: Must provide basis we'll interpolate from using 'originBasis'.")
+      tbl.fromBasis, "Updater.CartFieldInterpolate: Must provide basis to interpolate from using 'fromBasis'.")
 
    -- Ensure dimensionality of two quantities is the same.
    assert(toBasis:ndim() == fromBasis:ndim(), "Updater.CartFieldInterpolate: the dimensionalities must be the same.")
@@ -57,14 +63,44 @@ function CartFieldInterpolate:init(tbl)
    for d = 1, self.dim do
       self.numCellsC[d] = fromGrid:numCells(d)
       self.numCellsF[d] = toGrid:numCells(d)
+
+      -- For now limit to (2^a)*(3^b) grids. Check:
+      local pf = PrimeFactor.all(self.numCellsC[d])
+      for i = 1, #pf do
+         assert((pf[i]==2) or (pf[i]==3), string.format("Updater.CartFieldInterpolate: Only (2^a)*(3^b) grids supported. Prime factor: %d", pf[i]))
+      end
+      local pf = PrimeFactor.all(self.numCellsF[d])
+      for i = 1, #pf do
+         assert((pf[i]==2) or (pf[i]==3), string.format("Updater.CartFieldInterpolate: Only (2^a)*(3^b) grids supported. Prime factor: %d", pf[i]))
+      end
    end
 
    self.beta = {}   -- Ratio of cell-lengths in each direction.
    for d = 1, self.dim do self.beta[d] = fromGrid:dx(d)/toGrid:dx(d) end
 
-   self.intStencilSize = {}   -- Largest interior (away from boundaries) stencil size, in each direction.
+   self.intStencilSize = {}   -- Interior (away from boundaries) stencil size, in each direction.
    for d = 1, self.dim do
-      self.intStencilSize[d] = math.floor(self.beta[d]) + math.ceil(2*(self.beta[d] - math.floor(self.beta[d])))
+      if (self.beta[d] > 1) then   -- Mesh refinement.
+--         self.intStencilSize[d] = math.ceil(self.beta[d]) + math.ceil(self.beta[d] - math.floor(self.beta[d]))
+--         self.intStencilSize[d] = math.floor(self.beta[d]) + math.ceil(2*(self.beta[d] - math.floor(self.beta[d])))
+--         self.intStencilSize[d] = math.floor(self.beta[d]) + math.ceil(self.beta[d] - math.floor(self.beta[d]))
+--         if (math.ceil(self.beta[d] - math.floor(self.beta[d])) == 1) then
+--            self.intStencilSize[d] = self.intStencilSize[d]+1
+--         end
+         -- Brute force search:
+         -- Start with the size of the boundary stencil:
+         local maxSizePossible = math.ceil(self.beta[d])+1
+         local maxSize = math.floor(self.beta[d])+math.ceil(self.beta[d]-math.floor(self.beta[d]))
+         for i = 2, self.numCellsC[d]-1 do
+            local decimalL = 1-((i-1)*(self.beta[d])-math.floor((i-1)*(self.beta[d])))
+            local decimalU = 1-(math.ceil(i*self.beta[d])-i*(self.beta[d]))
+            local currSize = math.floor(self.beta[d]-decimalL-decimalU)+math.ceil(decimalL)+math.ceil(decimalU) 
+            maxSize = math.max(maxSize, currSize)
+         end
+         self.intStencilSize[d] = maxSize
+      else                         -- Mesh coarsening.
+         self.intStencilSize[d] = 1+math.ceil(1*(1/self.beta[d]-math.floor(1/self.beta[d])))
+      end
    end
 
    -- This updater will loop through the coarse grid, and for each coarse grid it will pass
@@ -74,6 +110,7 @@ function CartFieldInterpolate:init(tbl)
    self.stencils    = {}
    self.stencils[1] = {}
    self.stencils[1].stencilSize = self.intStencilSize
+   print(" stencil size = ", self.stencils[1].stencilSize[1])
    -- For each cell in the stencil, the multidimensional index offset relative to th
    -- lower left corner index (of the stencil) will be given by the index of a range object.
    local rangeLimits = {{},{}}
@@ -89,8 +126,8 @@ function CartFieldInterpolate:init(tbl)
             sI = sI+1
             self.stencils[sI] = {}
             self.stencils[sI].stencilSize     = lume.clone(self.stencils[s].stencilSize)
-            self.stencils[sI].stencilSize[dI] = math.floor(self.beta[dI]) + math.ceil(self.beta[dI] - math.floor(self.beta[dI])) -- self.stencils[sI].stencilSize[dI]-math.ceil(self.beta[dI] - math.floor(self.beta[dI]))
---            self.stencils[sI].stencilSizeN = self.stencils[s].stencilSizeN*((self.intStencilSize[dI]-(math.ceil(self.beta[dI] - math.floor(self.beta[dI]))))/self.intStencilSize[dI])
+            self.stencils[sI].stencilSize[dI] = math.floor(self.beta[dI]) + math.ceil(self.beta[dI] - math.floor(self.beta[dI]))
+            print(" stencil size = ", self.stencils[sI].stencilSize[dI])
             rangeLimits = {{},{}}
             for d = 1, self.dim do
                rangeLimits[1][d] = 0
@@ -121,19 +158,29 @@ function CartFieldInterpolate:idx2stencil(idxIn, numCellsIn)
    -- within the table that holds relaxation/residue stencils.
    local stencilIdx = 1
    for d = 1, self.dim do
+      local remDecL = (idxIn[d]-1)*(self.beta[d])-math.floor((idxIn[d]-1)*(self.beta[d]))
+      local remDecU = math.ceil(idxIn[d]*self.beta[d])-idxIn[d]*(self.beta[d])
       if ((idxIn[d] == 1) or   -- First cell.
-          (((idxIn[d]-1)*self.beta[d]-math.floor((idxIn[d]-1)*self.beta[d])) == 0)) then  -- Interior cell with a left-boundary-like stencil.
+          (remDecL == 0) or   -- Interior cell with a left-boundary-like stencil.
+          ((remDecL <= 0.5) and (remDecU <= 0.5))) then -- or
+--          (((math.ceil(idxIn[d]*(self.beta[d]))-idxIn[d]*(self.beta[d]))+((idxIn[d]-1)*(self.beta[d])-math.floor((idxIn[d]-1)*(self.beta[d])))) <= 0.5) or
+--          ((math.ceil((idxIn[d]-1)*self.beta[d]-math.floor(idxIn[d]*self.beta[d]))) == 1)) then  -- Interior cell with a left-boundary-like stencil.
          stencilIdx = 2*stencilIdx + 3^(d-1) - 1
       elseif ((idxIn[d] == numCellsIn[d]) or   -- Last cell.
-              ((idxIn[d]*self.beta[d]-math.floor(idxIn[d]*self.beta[d])) == 0)) then  -- Interior cell with a right-boundary-like stencil.
+              (remDecU == 0)) then   -- Interior cell with a right-boundary-like stencil.
+--              ((idxIn[d]*self.beta[d]-math.floor(idxIn[d]*self.beta[d])) == 0)) then -- or
+--              ((math.ceil((idxIn[d]-1)*self.beta[d]-math.floor(idxIn[d]*self.beta[d]))) == 1)) then  -- Interior cell with a right-boundary-like stencil.
          stencilIdx = 2*stencilIdx + 3^(d-1)
       end
    end
    return stencilIdx
 end
 
-function CartFieldInterpolate:prolong(cFld,fFld)
-   -- Prolongation of a coarse-grid field (cFld) to a fine-grid field (fFld). 
+-- Advance method.
+function CartFieldInterpolate:_advance(tCurr, inFld, outFld)
+
+   local cFld        = inFld[1]
+   local fFld        = outFld[1]
 
    local cGrid       = cFld:grid() 
    local fGrid       = fFld:grid() 
@@ -162,9 +209,12 @@ function CartFieldInterpolate:prolong(cFld,fFld)
 
       cFld:fill(cFldIndexer(cIdx), cFldItr)   -- Coarse-grid field pointer.
 
+      print(" cIdx = ",cIdx[1])
+
       -- Loop over the fine-grid cells this coarse grid contributes to.
       for fIdxOff in self.stencils[self:idx2stencil(cIdx,self.numCellsC)].stencilRange:rowMajorIter() do
          for d=1,self.dim do self.fIdx[d] = self.fIdxLL[d]+fIdxOff[d] end
+         print("                 | fIdx = ",self.fIdx[1])
 
          fGrid:setIndex(self.fIdx)
          fGrid:getDx(self.dxF)
@@ -176,15 +226,6 @@ function CartFieldInterpolate:prolong(cFld,fFld)
       end
   
    end
-end
-
--- Advance method.
-function CartFieldInterpolate:_advance(tCurr, inFld, outFld)
-
-   local coarseField = inFld[1]
-   local fineField   = outFld[1]
-
-   self:prolong(coarseField, fineField)
 
 end
 
