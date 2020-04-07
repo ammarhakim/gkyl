22c291c970622d297126be3548b12f836ba09c8a
manauref <mfrancisquez@gmail.com>
2020-04-07 08:21:34 -0700

    Add 2D interpolation tests
    - Add 2D interpolation tests to unit CartFieldInterpolate test.
    - There are many possible interpolations, so one needs to manually test chunks
      of the combinatorial pool one at a time. Furthermore, the unit test may have to be
      run performing a single test at a time.

--- Unit/test_CartFieldInterpolate.lua
+++ Unit/test_CartFieldInterpolate.lua
@@ -88,9 +88,16 @@ local function interpDG(gridIn,basisIn,gridOut,basisOut)
    return interpUp
 end
 
--- Function to project and transfer between grids.
-local function sampleFunc(x, aIn, c0In, c1In)
+-- Function to project and transfer between 1D grids.
+local function sampleFunc1D(x, aIn, c0In, c1In)
    return (x[1]^2)/2.0-aIn*(x[1]^4)/12.0+c0In*x[1]+c1In
+end
+
+-- Function to project and transfer between 2D grids.
+local function sampleFunc2D(xIn, aIn, bIn, cIn, dIn)
+   local x = xIn[1]
+   local y = xIn[2]
+   return ((x^2)/2.0+cIn[1]*x+cIn[2])*((y^2)/2.0+dIn[1]*y+dIn[2])
 end
 
 local function test_1x(pOrder, basisName, writeOut)
@@ -139,7 +146,7 @@ local function test_1x(pOrder, basisName, writeOut)
                a  = 2.0
                c0 = (a/12.0-0.5)
                c1 = 0.0
-               return sampleFunc(xn,a,c0,c1) 
+               return sampleFunc1D(xn,a,c0,c1) 
             end)
          project:advance(0.0, {}, {fld[1]})
       
@@ -193,13 +200,145 @@ local function test_1x(pOrder, basisName, writeOut)
 
 end
 
-local polyOrderMax = 2
+local function test_2x(pOrder, basisName, writeOut, testN, off)
+   local lower = {0.0, 0.0}
+   local upper = {1.0, 1.0}
+
+   local maxPower = 2
+   local powBases = {2, 3}
+   -- List all the possible grids we will consider.
+   local possibleNs = {}
+   local nI = 0
+   for cI = 1, (maxPower+1)^(#powBases) do
+      local pow1 = math.floor(math.ceil(cI/(maxPower+1))-1)
+      local pow2 = math.floor(((cI-1+(maxPower+1)) % (maxPower+1)))
+      if not ((pow1==0) and (pow2==0)) then
+         nI = nI+1
+         possibleNs[nI] = math.floor(powBases[1]^pow1)*math.floor(powBases[2]^pow2)
+      end
+   end
+   -- Generate the permutations P(n,2) where n=#possibleNs.
+   local pairsN = {}
+   for i = 1,#possibleNs do for j = 1,#possibleNs do
+      pairsN[(i-1)*#possibleNs+j] = {possibleNs[i], possibleNs[j]}
+   end end
+   -- Generate the permutations of grid-pair combinations.
+   local gridPairs = {}
+   for i = 1,#pairsN do for j = 1,#pairsN do
+      gridPairs[(i-1)*#pairsN+j] = {pairsN[i], pairsN[j]}
+   end end
+
+   -- Below, testN gets reinterpreted to mean the number of grids to test,
+   -- and 'off' to mean the offset in the gridPairs table.
+   off   = off or 0
+   testN = testN or 500
+   if (#gridPairs)*2 < 1000 then
+      testN = #gridPairs
+      off   = 0
+   elseif ((off+1)*testN > #gridPairs) then
+      testN = #gridPairs - off*testN
+   end
+
+   print(" ")
+   print(string.format(" Number of interpolations possible: %d",(#gridPairs)*2))
+   print(string.format(" Number of possible grid combinations: %d",#gridPairs))
+   print(string.format(" Testing grid combinations %d to %d.",off*testN+1,(off+1)*testN))
+   print(" ")
+
+   for k = 1, testN do
+      N = gridPairs[off*testN+k]
+      for i = 1,2 do   -- Test interpolation in both directions.
+         if i==1 then
+            fromIdx = 1
+            toIdx   = 2
+         else
+            fromIdx = 2
+            toIdx   = 1
+         end
+         -- Grids and basis.
+         local grid  = {createGrid(lower, upper, N[fromIdx]), createGrid(lower, upper, N[toIdx])}
+         local basis = createBasis(grid[1]:ndim(), pOrder, basisName)
+         -- Fields.
+         local fld = {createField(grid[1],basis), createField(grid[2],basis)}
+
+         -- Project a sample function on the field to interpolate from.
+         local project = createProject(grid[1],basis)
+         project:setFunc(function (t, xn)
+               local a = 2.0
+               local b = 5.0
+               local c = {a/12.0 - 0.5, 0.0}
+               local d = {0.0, b/12.0 - 0.5}
+               return sampleFunc2D(xn,a,b,c,d)
+            end)
+         project:advance(0.0, {}, {fld[1]})
+
+         -- Create interpolation operator and perform interpolation.
+         local interpolation = interpDG(grid[1],basis,grid[2],basis)
+         interpolation:advance(0.0,{fld[1]},{fld[2]})
+
+         -- Compute the first 3 integrated moments of the fields.
+         local intF     = {DataStruct.DynVector {numComponents = 1,}, DataStruct.DynVector {numComponents = 1,}}
+         local intFcalc1 = intDGmom(grid[1],basis,"one")
+         local intFcalc2 = intDGmom(grid[2],basis,"one")
+         intFcalc1:advance(0.0,{fld[1]},{intF[1]})
+         intFcalc2:advance(0.0,{fld[2]},{intF[2]})
+         local jnk
+         local fMom = {}
+         jnk, fMom[1] = intF[1]:lastData()
+         jnk, fMom[2] = intF[2]:lastData()
+
+         local intxF     = {DataStruct.DynVector {numComponents = grid[1]:ndim(),}, DataStruct.DynVector {numComponents = grid[2]:ndim(),}}
+         local intxFcalc1 = intDGmom(grid[1],basis,"xi")
+         local intxFcalc2 = intDGmom(grid[2],basis,"xi")
+         intxFcalc1:advance(0.0,{fld[1]},{intxF[1]})
+         intxFcalc2:advance(0.0,{fld[2]},{intxF[2]})
+         local jnk
+         local xfMom = {}
+         jnk, xfMom[1] = intxF[1]:lastData()
+         jnk, xfMom[2] = intxF[2]:lastData()
+
+         local intxSqF     = {DataStruct.DynVector {numComponents = 1,}, DataStruct.DynVector {numComponents = 1,}}
+         local intxSqFcalc1 = intDGmom(grid[1],basis,"xSq")
+         local intxSqFcalc2 = intDGmom(grid[2],basis,"xSq")
+         intxSqFcalc1:advance(0.0,{fld[1]},{intxSqF[1]})
+         intxSqFcalc2:advance(0.0,{fld[2]},{intxSqF[2]})
+         local jnk
+         local xSqfMom = {}
+         jnk, xSqfMom[1] = intxSqF[1]:lastData()
+         jnk, xSqfMom[2] = intxSqF[2]:lastData()
+
+         -- Write data for plotting
+         if writeOut then
+            fld[1]:write(string.format("inFld_%sN%dx%d-N%dx%d_p%d.bp",basisName,N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2],pOrder),0.0)
+            fld[2]:write(string.format("outFld_%sN%dx%d-N%dx%d_p%d.bp",basisName,N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2],pOrder),0.0)
+         end
+
+         assert_equal(fMom[1][1], fMom[2][1], string.format("Checking 'one' moment in N%dx%d-N%dx%d",N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2]))
+         assert_equal(xfMom[1][1], xfMom[2][1], string.format("Checking 'x_1' moment in N%dx%d-N%dx%d",N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2]))
+         assert_equal(xfMom[1][2], xfMom[2][2], string.format("Checking 'x_2' moment in N%dx%d-N%dx%d",N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2]))
+         if (pOrder >= 2) then
+            assert_equal(xSqfMom[1][1], xSqfMom[2][1], string.format("Checking 'x^2' moment in N%dx%d-N%dx%d",N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2]))
+         else
+            assert_close(xSqfMom[1][1], xSqfMom[2][1], 0.02*xSqfMom[1][1], string.format("Checking 'x^2' moment in N%dx%d-N%dx%d",N[fromIdx][1],N[fromIdx][2],N[toIdx][1],N[toIdx][2]))
+         end
+      end
+   end
+
+   off = off+1
+end
+
+local polyOrder    = 1
 local basisType    = "Ser"
 local writeFiles   = false
 
-for polyOrder = 1, polyOrderMax do
-  test_1x( polyOrder, basisType, writeFiles)
-end
+-- These are for 2D and higher dim.
+local numInterpMax = 500   -- Maximum number of different interpolations to test (keep <501).
+local interpOffset = 0     -- Offset to decide which interpolations to test (start with 0).
+
+-- Due to the high number of possible interpolations, need to only perform one test at a time.
+test_1x( polyOrder, basisType, writeFiles)
+--test_2x( polyOrder, basisType, writeFiles, numInterpMax,  interpOffset )
+
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
