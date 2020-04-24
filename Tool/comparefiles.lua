6e7ea12c551cbf4e948033af1d9ffd7c00ffa065
ammarhakim <a.hakim777@gmail.com>
2020-04-24 11:11:46 -0400

    Updated comparefiles tool to also compare attributes and timeMesh in DynVectors

--- Tool/comparefiles.lua
+++ Tool/comparefiles.lua
@@ -52,19 +52,59 @@ end
 
 -- function to compare files
 local function compareFiles(f1, f2)
+   --verboseLog(string.format("Comparing %s %s ...\n", f1, f2))   
    if not lfs.attributes(f1) or not lfs.attributes(f2) then
-      verboseLogger(string.format(
+      verboseLog(string.format(
 		    " ... files %s and/or %s do not exist!\n", f1, f2))
       return false
    end
    
    local r1, r2 = AdiosReader.Reader(f1), AdiosReader.Reader(f2)
 
+   -- check attribute
+   local function checkVecAttr(attrNm)
+      print(string.format("Checking attr %s in %s %s ...", attrNm, f1, f2))
+      if not r1:hasAttr(attrNm) and not r2:hasAttr(attrNm) then
+	 return true -- if both files have attribute missing, consider as pass
+      end
+
+      -- If both don't have it
+      if not r1:hasAttr(attrNm) or not r2:hasAttr(attrNm) then
+	 verboseLog(string.format(
+		       " ... CartGridField attr %s not present in both files %s and %s!\n", attrNm, f1, f2))
+	 return false
+      end
+
+      print(string.format("... comparing %s", attrNm))
+      local r1_attrNm, r2_attrNm = r1:getAttr(attrNm):read(), r2:getAttr(attrNm):read()
+      if #r1_attrNm ~= #r2_attrNm then
+	 verboseLog(string.format(
+		       " ... CartGridField attr %s in files %s and %s not the same size!\n", attrNm, f1, f2))
+	 return false
+      end
+      for i = 1, #r1_attrNm do
+	 if r1_attrNm[i] ~= r2_attrNm[i] then
+	    verboseLog(string.format(
+			  " ... CartGridField attr %s not the same files %s and %s not the same!\n", attrNm, f1, f2))
+	    return false
+	 end
+      end
+      return true
+   end
+
    local cmpPass = true
    local currMaxDiff = 0.0
    
    if r1:hasVar("CartGridField") and r2:hasVar("CartGridField") then
-      -- compare CartField
+      
+      -- compare stable attributes (not all attributes can be compared)
+      if not checkVecAttr("numCells") then return false end
+      if not checkVecAttr("lowerBounds") then return false end
+      if not checkVecAttr("upperBounds") then return false end
+      if not checkVecAttr("basisType") then return false end
+      if not checkVecAttr("polyOrder") then return false end
+      
+      -- compare CartField data
       local d1, d2 = r1:getVar("CartGridField"):read(), r2:getVar("CartGridField"):read()
 
       if d1:size() ~= d2:size() then
@@ -80,26 +120,75 @@ local function compareFiles(f1, f2)
 	    cmpPass = false
 	 end
       end
-   elseif r1:hasVar("TimeMesh") and r2:hasVar("TimeMesh") then
-      -- Compare DynVector
-      local d1, d2 = r1:getVar("Data"):read(), r2:getVar("Data"):read()
-      if d1:size() ~= d2:size() then
-	 verboseLog(string.format(
-		       " ... DynVector in files %s and %s not the same size!\n", f1, f2))
-	 return false
+   -- Compare DynVector
+   elseif (r1:hasVar("TimeMesh0") or r1:hasVar("TimeMesh")) and (r2:hasVar("TimeMesh0") or r2:hasVar("TimeMesh")) then
+      local t1, t2
+      local d1, d2
+      
+      -- Read from file 1
+      -- If single dataset, read it into single array
+      if r1:hasVar("TimeMesh") then 
+         t1 = r1:getVar("TimeMesh"):read()
+         d1 = r1:getVar("Data"):read()
+      -- If multiple datasets, read and append into single array
+      elseif r1:hasVar("TimeMesh0") then
+         t1 = r1:getVar("TimeMesh0"):read()
+         d1 = r1:getVar("Data0"):read()
+         frNum = 1
+         while r1:hasVar("TimeMesh"..frNum) do
+            local t1N = r1:getVar("TimeMesh"..frNum):read()
+            local d1N = r1:getVar("Data"..frNum):read()
+            for i = 1, t1N:size() do
+               t1:push(t1N[i])
+            end
+            for i = 1, d1N:size() do
+               d1:push(d1N[i])
+            end
+            frNum = frNum + 1
+         end
       end
 
+      -- Read from file 2
+      -- If single dataset, read it into single array
+      if r2:hasVar("TimeMesh") then 
+         t2 = r2:getVar("TimeMesh"):read()
+         d2 = r2:getVar("Data"):read()
+      -- If multiple datasets, read and append into single array
+      elseif r2:hasVar("TimeMesh0") then
+         t2 = r2:getVar("TimeMesh0"):read()
+         d2 = r2:getVar("Data0"):read()
+         frNum = 1
+         while r2:hasVar("TimeMesh"..frNum) do
+            local t2N = r2:getVar("TimeMesh"..frNum):read()
+            local d2N = r2:getVar("Data"..frNum):read()
+            for i = 1, t2N:size() do
+               t2:push(t2N[i])
+            end
+            for i = 1, d2N:size() do
+               d2:push(d2N[i])
+            end
+            frNum = frNum + 1
+         end
+      end
+
+      -- Check equivalence
+      if d1:size() ~= d2:size() then
+         verboseLog(string.format(
+          	  " ... DynVector in files %s and %s not the same size!\n", f1, f2))
+         return false
+      end
+      
       local maxVal = math.max(maxValueInField(d1),maxValueInField(d2)) -- maximum value (for numeric comparison)
       for i = 1, d1:size() do
-	 if check_equal_numeric(d1[i], d2[i], maxVal) == false then
-	    currMaxDiff = math.max(currMaxDiff, get_relative_numeric(d1[i], d2[i], maxVal))
-	    cmpPass = false
-	 end
+         if check_equal_numeric(d1[i], d2[i], maxVal) == false then
+            currMaxDiff = math.max(currMaxDiff, get_relative_numeric(d1[i], d2[i], maxVal))
+            cmpPass = false
+         end
       end
    end
 
    if cmpPass == false then
-      verboseLogger(string.format(" ... relative error in file %s is %g ...\n", f2, currMaxDiff))
+      verboseLog(string.format(" ... relative error in file %s is %g ...\n", f2, currMaxDiff))
    end
 
    r1:close(); r2:close()
