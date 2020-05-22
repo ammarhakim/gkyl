115c30082c098930eb3c5170407b9bb7719f4014
ammarhakim <a.hakim777@gmail.com>
2020-05-21 23:13:43 -0400

    To demonstrate added templated set of classes (CDIM, VDIM, POLY, basisType) which calls into the appropriate lower-level kernels. Compile and linked into the kerneltimer tool but not called yet. Will do this tomorrow
    Header file autogenerated. At some point we need to unify the auto-generation into its own unified sub-system. It's a mess at present, but for now it is okay.

--- Eq/vlasovData/VlasovTmplModDecl.h
+++ Eq/vlasovData/VlasovTmplModDecl.h
@@ -1,3 +1,4 @@
+
 #ifndef VLASOV_TMPL_MOD_DECL_H
 #define VLASOV_TMPL_MOD_DECL_H
 
@@ -19,25 +20,455 @@ namespace Gkyl {
       /**
        * Surface streaming term
        */
-      void surfStreamTerm(unsigned dir, const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double *fl, const double *fr, double *outl, double *outr);
-      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr, const double *fl, const double *fr,
+        double *outl, double *outr);
+
+      /**
+       * Volume term (total surface + force)
+       */
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out);
+
+      /**
+       * Surface terms from EM forces
+       */
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr);
   };
 
   // Template specializations provide actual calls
+
+
   template <>
-  class VlasovModDecl<1,1,1,G_MAX_ORDER_C> {
+  class VlasovModDecl<1,1,1,G_SERENDIPITY_C> {
     public:
       double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
-        return VlasovVolStream1x1vMaxP1(w, dxv, f, out);
+        return VlasovVolStream1x1vSerP1(w, dxv, f, out);
       }
       
-      void surfStreamTerm(unsigned dir, const double *wl, const double *wr, const double *dxvl, const double *dxvr, const double *fl, const double *fr, double *outl, double *outr) {
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
         switch (dir) {
           case 0:
-              VlasovSurfStream1x1vMax_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              VlasovSurfStream1x1vSer_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
               break;
         }
       }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x1vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x1vSer_VX_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
   };
+
+
+  template <>
+  class VlasovModDecl<1,1,2,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream1x1vSerP2(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream1x1vSer_X_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x1vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x1vSer_VX_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<1,2,1,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream1x2vSerP1(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream1x2vSer_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x2vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x2vSer_VX_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag1x2vSer_VY_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<1,2,2,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream1x2vSerP2(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream1x2vSer_X_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x2vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x2vSer_VX_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag1x2vSer_VY_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<1,3,1,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream1x3vSerP1(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream1x3vSer_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x3vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x3vSer_VX_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag1x3vSer_VY_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 2:
+              VlasovSurfElcMag1x3vSer_VZ_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<1,3,2,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream1x3vSerP2(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream1x3vSer_X_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol1x3vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag1x3vSer_VX_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag1x3vSer_VY_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 2:
+              VlasovSurfElcMag1x3vSer_VZ_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<2,2,1,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream2x2vSerP1(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream2x2vSer_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfStream2x2vSer_Y_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol2x2vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag2x2vSer_VX_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag2x2vSer_VY_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<2,2,2,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream2x2vSerP2(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream2x2vSer_X_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfStream2x2vSer_Y_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol2x2vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag2x2vSer_VX_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag2x2vSer_VY_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<2,3,1,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream2x3vSerP1(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream2x3vSer_X_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfStream2x3vSer_Y_P1(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol2x3vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag2x3vSer_VX_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag2x3vSer_VY_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 2:
+              VlasovSurfElcMag2x3vSer_VZ_P1(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
+
+  template <>
+  class VlasovModDecl<2,3,2,G_SERENDIPITY_C> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream2x3vSerP2(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream2x3vSer_X_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfStream2x3vSer_Y_P2(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol2x3vSerP1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag2x3vSer_VX_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 1:
+              VlasovSurfElcMag2x3vSer_VY_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+          case 2:
+              VlasovSurfElcMag2x3vSer_VZ_P2(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+        }        
+      }
+  };
+
 }
 #endif // VLASOV_TMPL_MOD_DECL_H
+
---
+++ cas-scripts/vlasov-calc/vlasov-tmpl-header.lua
@@ -0,0 +1,133 @@
+local xsys = require "xsys"
+
+vlasovHeaderTemplateString = [[
+
+#ifndef VLASOV_TMPL_MOD_DECL_H
+#define VLASOV_TMPL_MOD_DECL_H
+
+#include <VlasovModDecl.h>
+#include <GkBasisTypes.h>
+
+namespace Gkyl {
+
+  // Provides a templated wrapper around the low level C-style kernels
+  // so they can be called more systematically from C++ code
+  template <unsigned CDIM, unsigned VDIM, unsigned POLYORDER, unsigned BASISTYPE>
+  class VlasovModDecl {
+    public:
+      /**
+       * Volume streaming term
+       */
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out);
+
+      /**
+       * Surface streaming term
+       */
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr, const double *fl, const double *fr,
+        double *outl, double *outr);
+
+      /**
+       * Volume term (total surface + force)
+       */
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out);
+
+      /**
+       * Surface terms from EM forces
+       */
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr);
+  };
+
+  // Template specializations provide actual calls
+
+|for ci = 1, CDIM do
+| for vi = ci, VDIM do
+|  for pi = 1, PMAX do
+|    for bni = 1, #basisNm do
+
+  template <>
+  class VlasovModDecl<${ci},${vi},${pi},${basisNm[bni]}> {
+    public:
+      double volumeStreamTerm(const double *w, const double *dxv, const double *f, double *out) {
+        return VlasovVolStream${ci}x${vi}v${basisShortNm[bni]}P${pi}(w, dxv, f, out);
+      }
+      
+      void surfStreamTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double *fl, const double *fr,
+        double *outl, double *outr) {
+        
+        switch (dir) {
+          case 0:
+              VlasovSurfStream${ci}x${vi}v${basisShortNm[bni]}_X_P${pi}(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+|if ci>1 then
+          case 1:
+              VlasovSurfStream${ci}x${vi}v${basisShortNm[bni]}_Y_P${pi}(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+|end
+|if ci>2 then
+          case 2:
+              VlasovSurfStream${ci}x${vi}v${basisShortNm[bni]}_Z_P${pi}(wl, wr, dxvl, dxvr, fl, fr, outl, outr);
+              break;
+|end
+        }
+      }
+
+      double volumeTerm(const double *w, const double *dxv, const double *E, const double *f, double *out) {
+        return VlasovVol${ci}x${vi}v${basisShortNm[bni]}P1(w, dxv, E, f, out);
+      }
+
+      void surfElcMagTerm(unsigned dir, const double *wl, const double *wr,
+        const double *dxvl, const double *dxvr,
+        const double amax, const double *E, const
+        double *fl, const double *fr,
+        double *outl, double *outr) {
+
+        switch (dir) {
+          case 0:
+              VlasovSurfElcMag${ci}x${vi}v${basisShortNm[bni]}_VX_P${pi}(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+|if vi > 1 then
+          case 1:
+              VlasovSurfElcMag${ci}x${vi}v${basisShortNm[bni]}_VY_P${pi}(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+|end
+|if vi > 2 then
+          case 2:
+              VlasovSurfElcMag${ci}x${vi}v${basisShortNm[bni]}_VZ_P${pi}(wl, wr, dxvl, dxvr, amax, E, fl, fr, outl, outr);
+              break;
+|end
+        }        
+      }
+  };
+
+|      end
+|    end
+|  end
+|end
+}
+#endif // VLASOV_TMPL_MOD_DECL_H
+
+]]
+
+-- instantiate template
+local vlasovHeaderTemplate = xsys.template ( vlasovHeaderTemplateString )
+-- environment to pass to template 
+local vlasovEnv = {
+   CDIM = 2, VDIM = 3, PMAX = 2,
+   basisNm = {'G_SERENDIPITY_C'},
+   basisShortNm = {'Ser'},
+}
+
+--   basisNm = {'G_MAX_ORDER_C', 'G_SERENDIPITY_C'},
+--   basisShortNm = {'Max', 'Ser'},
+
+
+-- write out string
+io.write( vlasovHeaderTemplate (vlasovEnv) )
+			   
