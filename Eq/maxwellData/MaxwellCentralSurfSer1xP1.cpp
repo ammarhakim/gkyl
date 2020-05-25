64340813a1e8a05300c73c4c3cb6ec662d6a9ac7
James Juno <junoravin@gmail.com>
2020-05-24 23:33:21 -0400

    Adding the separated Maxwell kernels, along with the GPU decoratives

---
+++ Eq/maxwellData/MaxwellCentralSurfSer1xP1.cpp
@@ -0,0 +1,121 @@
+#include <MaxwellModDecl.h> 
+__host__ __device__ double MaxwellCentralSurf1xSer_X_P1(const MaxwellEq_t *meq, const double *wl, const double *wr, const double *dxl, const double *dxr, const double tau, const double *ql, const double *qr, double *outl, double *outr) 
+{ 
+  const double c = meq->c, chi = meq->chi, gamma = meq->gamma; 
+  const double c2 = c*c; 
+  const double c2chi = c2*chi, c2gamma = c2*gamma; 
+ 
+  const double dxl1 = 2.0/dxl[0]; 
+  const double dxr1 = 2.0/dxr[0]; 
+  const double *exl = &ql[0]; 
+  const double *eyl = &ql[2]; 
+  const double *ezl = &ql[4]; 
+  const double *bxl = &ql[6]; 
+  const double *byl = &ql[8]; 
+  const double *bzl = &ql[10]; 
+  const double *phl = &ql[12]; 
+  const double *psl = &ql[14]; 
+ 
+  double *outExl = &outl[0]; 
+  double *outEyl = &outl[2]; 
+  double *outEzl = &outl[4]; 
+  double *outBxl = &outl[6]; 
+  double *outByl = &outl[8]; 
+  double *outBzl = &outl[10]; 
+  double *outPhl = &outl[12]; 
+  double *outPsl = &outl[14]; 
+ 
+  const double *exr = &qr[0]; 
+  const double *eyr = &qr[2]; 
+  const double *ezr = &qr[4]; 
+  const double *bxr = &qr[6]; 
+  const double *byr = &qr[8]; 
+  const double *bzr = &qr[10]; 
+  const double *phr = &qr[12]; 
+  const double *psr = &qr[14]; 
+ 
+  double *outExr = &outr[0]; 
+  double *outEyr = &outr[2]; 
+  double *outEzr = &outr[4]; 
+  double *outBxr = &outr[6]; 
+  double *outByr = &outr[8]; 
+  double *outBzr = &outr[10]; 
+  double *outPhr = &outr[12]; 
+  double *outPsr = &outr[14]; 
+ 
+  double incr[2]; 
+ 
+  incr[0] = ((-0.4330127018922193*phr[1])+0.4330127018922193*phl[1]+0.25*(phr[0]+phl[0]))*c2chi; 
+  incr[1] = (0.75*phr[1]-0.75*phl[1]-0.4330127018922193*(phr[0]+phl[0]))*c2chi; 
+
+  outExr[0] += incr[0]*dxr1; 
+  outExr[1] += incr[1]*dxr1; 
+
+  outExl[0] += -1.0*incr[0]*dxl1; 
+  outExl[1] += incr[1]*dxl1; 
+
+  incr[0] = ((-0.4330127018922193*bzr[1])+0.4330127018922193*bzl[1]+0.25*(bzr[0]+bzl[0]))*c2; 
+  incr[1] = (0.75*bzr[1]-0.75*bzl[1]-0.4330127018922193*(bzr[0]+bzl[0]))*c2; 
+
+  outEyr[0] += incr[0]*dxr1; 
+  outEyr[1] += incr[1]*dxr1; 
+
+  outEyl[0] += -1.0*incr[0]*dxl1; 
+  outEyl[1] += incr[1]*dxl1; 
+
+  incr[0] = (0.4330127018922193*byr[1]-0.4330127018922193*byl[1]-0.25*(byr[0]+byl[0]))*c2; 
+  incr[1] = ((-0.75*byr[1])+0.75*byl[1]+0.4330127018922193*(byr[0]+byl[0]))*c2; 
+
+  outEzr[0] += incr[0]*dxr1; 
+  outEzr[1] += incr[1]*dxr1; 
+
+  outEzl[0] += -1.0*incr[0]*dxl1; 
+  outEzl[1] += incr[1]*dxl1; 
+
+  incr[0] = ((-0.4330127018922193*psr[1])+0.4330127018922193*psl[1]+0.25*(psr[0]+psl[0]))*gamma; 
+  incr[1] = (0.75*psr[1]-0.75*psl[1]-0.4330127018922193*(psr[0]+psl[0]))*gamma; 
+
+  outBxr[0] += incr[0]*dxr1; 
+  outBxr[1] += incr[1]*dxr1; 
+
+  outBxl[0] += -1.0*incr[0]*dxl1; 
+  outBxl[1] += incr[1]*dxl1; 
+
+  incr[0] = 0.4330127018922193*ezr[1]-0.4330127018922193*ezl[1]-0.25*(ezr[0]+ezl[0]); 
+  incr[1] = (-0.75*ezr[1])+0.75*ezl[1]+0.4330127018922193*(ezr[0]+ezl[0]); 
+
+  outByr[0] += incr[0]*dxr1; 
+  outByr[1] += incr[1]*dxr1; 
+
+  outByl[0] += -1.0*incr[0]*dxl1; 
+  outByl[1] += incr[1]*dxl1; 
+
+  incr[0] = (-0.4330127018922193*eyr[1])+0.4330127018922193*eyl[1]+0.25*(eyr[0]+eyl[0]); 
+  incr[1] = 0.75*eyr[1]-0.75*eyl[1]-0.4330127018922193*(eyr[0]+eyl[0]); 
+
+  outBzr[0] += incr[0]*dxr1; 
+  outBzr[1] += incr[1]*dxr1; 
+
+  outBzl[0] += -1.0*incr[0]*dxl1; 
+  outBzl[1] += incr[1]*dxl1; 
+
+  incr[0] = ((-0.4330127018922193*exr[1])+0.4330127018922193*exl[1]+0.25*(exr[0]+exl[0]))*chi; 
+  incr[1] = (0.75*exr[1]-0.75*exl[1]-0.4330127018922193*(exr[0]+exl[0]))*chi; 
+
+  outPhr[0] += incr[0]*dxr1; 
+  outPhr[1] += incr[1]*dxr1; 
+
+  outPhl[0] += -1.0*incr[0]*dxl1; 
+  outPhl[1] += incr[1]*dxl1; 
+
+  incr[0] = ((-0.4330127018922193*bxr[1])+0.4330127018922193*bxl[1]+0.25*(bxr[0]+bxl[0]))*c2gamma; 
+  incr[1] = (0.75*bxr[1]-0.75*bxl[1]-0.4330127018922193*(bxr[0]+bxl[0]))*c2gamma; 
+
+  outPsr[0] += incr[0]*dxr1; 
+  outPsr[1] += incr[1]*dxr1; 
+
+  outPsl[0] += -1.0*incr[0]*dxl1; 
+  outPsl[1] += incr[1]*dxl1; 
+
+  return c; 
+} 
---
+++ Eq/maxwellData/MaxwellVolSer2xP1.cpp
@@ -0,0 +1,64 @@
+#include <MaxwellModDecl.h> 
+__host__ __device__ double MaxwellVol2xSerP1(const MaxwellEq_t * const meq, const double *w, const double *dx, const double *q, double *out) 
+{ 
+  const double c2 = meq->c*meq->c, chi = meq->chi, gamma = meq->gamma; 
+  const double c2chi = c2*chi, c2gamma = c2*gamma; 
+ 
+  const double *ex = &q[0]; 
+  const double *ey = &q[4]; 
+  const double *ez = &q[8]; 
+  const double *bx = &q[12]; 
+  const double *by = &q[16]; 
+  const double *bz = &q[20]; 
+  const double *ph = &q[24]; 
+  const double *ps = &q[28]; 
+ 
+  double *outEx = &out[0]; 
+  double *outEy = &out[4]; 
+  double *outEz = &out[8]; 
+  double *outBx = &out[12]; 
+  double *outBy = &out[16]; 
+  double *outBz = &out[20]; 
+  double *outPh = &out[24]; 
+  double *outPs = &out[28]; 
+ 
+  double dx0 = 2.0/dx[0]; 
+  double dx1 = 2.0/dx[1]; 
+
+  outEx[1] += 1.732050807568877*ph[0]*c2chi*dx0; 
+  outEx[2] += -1.732050807568877*bz[0]*c2*dx1; 
+  outEx[3] += 1.732050807568877*ph[2]*c2chi*dx0-1.732050807568877*bz[1]*c2*dx1; 
+
+  outEy[1] += 1.732050807568877*bz[0]*c2*dx0; 
+  outEy[2] += 1.732050807568877*ph[0]*c2chi*dx1; 
+  outEy[3] += 1.732050807568877*ph[1]*c2chi*dx1+1.732050807568877*bz[2]*c2*dx0; 
+
+  outEz[1] += -1.732050807568877*by[0]*c2*dx0; 
+  outEz[2] += 1.732050807568877*bx[0]*c2*dx1; 
+  outEz[3] += 1.732050807568877*bx[1]*c2*dx1-1.732050807568877*by[2]*c2*dx0; 
+
+  outBx[1] += 1.732050807568877*ps[0]*dx0*gamma; 
+  outBx[2] += 1.732050807568877*ez[0]*dx1; 
+  outBx[3] += 1.732050807568877*ps[2]*dx0*gamma+1.732050807568877*ez[1]*dx1; 
+
+  outBy[1] += -1.732050807568877*ez[0]*dx0; 
+  outBy[2] += 1.732050807568877*ps[0]*dx1*gamma; 
+  outBy[3] += 1.732050807568877*ps[1]*dx1*gamma-1.732050807568877*ez[2]*dx0; 
+
+  outBz[1] += 1.732050807568877*ey[0]*dx0; 
+  outBz[2] += -1.732050807568877*ex[0]*dx1; 
+  outBz[3] += 1.732050807568877*ey[2]*dx0-1.732050807568877*ex[1]*dx1; 
+
+  outPh[1] += 1.732050807568877*ex[0]*chi*dx0; 
+  outPh[2] += 1.732050807568877*ey[0]*chi*dx1; 
+  outPh[3] += 1.732050807568877*ey[1]*chi*dx1+1.732050807568877*ex[2]*chi*dx0; 
+
+  outPs[1] += 1.732050807568877*bx[0]*c2gamma*dx0; 
+  outPs[2] += 1.732050807568877*by[0]*c2gamma*dx1; 
+  outPs[3] += 1.732050807568877*by[1]*c2gamma*dx1+1.732050807568877*bx[2]*c2gamma*dx0; 
+
+  double cflFreq = 0.0; 
+  cflFreq += meq->c/dx[0]; 
+  cflFreq += meq->c/dx[1]; 
+  return cflFreq; 
+} 
