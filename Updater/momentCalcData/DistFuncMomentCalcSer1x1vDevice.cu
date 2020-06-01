a8873b7a0435a6117673ef3b9eafc3aecce16650
manauref <mfrancisquez@gmail.com>
2020-05-31 22:36:15 -0400

    Generalizing device moment kernel so it works with higher polyOrder. Update Gentran code.

--- Updater/momentCalcData/DistFuncMomentCalcSer1x1vDevice.cu
+++ Updater/momentCalcData/DistFuncMomentCalcSer1x1vDevice.cu
@@ -18,7 +18,7 @@ __global__ void d_calcMom1x1vSer_M0_P1(GkylCartField_t *fIn, GkylCartField_t *ou
   // Then one must perform a reduction across a block for each conf-space basis coefficient.
 
   // Index of the first phase-space memory address to access.
-  unsigned int linearIdx = blockIdx.x*blockDim.x + threadIdx.x;
+  unsigned int linearIdx       = blockIdx.x*blockDim.x + threadIdx.x;
   GkylRange_t *localPhaseRange = fIn->localRange;
   Gkyl::GenIndexer localPhaseIdxr(localPhaseRange);
   Gkyl::GenIndexer fIdxr = fIn->genIndexer();
@@ -29,11 +29,11 @@ __global__ void d_calcMom1x1vSer_M0_P1(GkylCartField_t *fIn, GkylCartField_t *ou
 
   double localSum[2];
   for (unsigned int k = 0; k < 2; k++) {
-    localSum[k]=0.0;
+    localSum[k] = 0.0;
   }
 
   // Pointers to quantities expected by the moment kernel.
-  const double *distF = fIn->getDataPtrAt(phaseLinIdx);
+  const double *distF  = fIn->getDataPtrAt(phaseLinIdx);
   GkylRectCart_t *grid = fIn->grid;
   double cellxc[2];
   grid->cellCenter(phaseIdx,cellxc);
@@ -50,13 +50,17 @@ __global__ void d_calcMom1x1vSer_M0_P1(GkylCartField_t *fIn, GkylCartField_t *ou
   Gkyl::GenIndexer localConfIdxr(localConfRange);
   Gkyl::GenIndexer outIdxr = out->genIndexer();
   int confIdx[1];
-  confIdx[0]=phaseIdx[0];
+  for (unsigned int k = 0; k < 1; k++) {
+    confIdx[k] = phaseIdx[k];
+  }
   int confLinIdx = outIdxr.index(phaseIdx);
-  double *mom = out->getDataPtrAt(confLinIdx);
+  double *mom    = out->getDataPtrAt(confLinIdx);
+  unsigned int numComponents = out->numComponents;
 
   if (threadIdx.x==0) {
-    mom[confLinIdx]=localSumPtr[0];
-    mom[confLinIdx+1]=localSumPtr[1];
+    for (unsigned int k = 0; k < numComponents; k++) {
+      mom[confLinIdx+k] = localSumPtr[k];
+    }
   }
 
 }
