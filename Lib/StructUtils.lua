7bcf54f5e4b7e2826e45381d9decdafc12c6464b
ammarhakim <a.hakim777@gmail.com>
2020-06-07 14:45:56 -0400

    Added a meta-object that allows wrapping a C-struct (like the ones we are using to communicate with the device) and then clone it on the device. Added tests for host-side and will next test on device

---
+++ Lib/StructUtils.lua
@@ -0,0 +1,62 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Utility functions to make dealing with C-structs easier
+--
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+local ffi = require "ffi"
+if GKYL_HAVE_CUDA then
+   cuda = require "Cuda.RunTime"
+end
+
+-- Struct ----------------------------------------------------------------------
+--
+-- A wrapper around a C-struct to make some functionality cleaner
+--------------------------------------------------------------------------------
+
+local function Struct(elct)
+   local struct_mf = {
+      size = function(self)
+	 return ffi.sizeof(elct)
+      end,      
+      clone = function(self)
+	 -- clone does a shallow copy: i.e. pointers in original and
+	 -- cloned struct point to the same address
+	 local v = ffi.new(self)
+	 ffi.copy(v, self, ffi.sizeof(elct))
+	 return v
+      end,
+      cloneOnDevice = GKYL_HAVE_CUDA and
+	 function (self)
+	    local cuObj, err = cuda.Malloc(ffi.sizeof(elct))
+	    assert(cuda.Success == err, "StructUtils.Struct: unable to allocate device memory!")
+	    cuda.Memcpy(cuObj, self, ffi.sizeof(elct), cuda.MemcpyHostToDevice)
+	    return cuObj
+	 end or
+	 function (self)
+	    -- when not on device, return host clone
+	    return self.clone()
+	 end
+   }
+   local struct_mt = {
+      __new = function(ct, init)
+	 -- 'init' is a table of values values to initialize
+	 -- struct. Values must be in same order as entries in
+	 -- C-struct
+	 if init then
+	    return ffi.new(ct, init)
+	 end
+	 return ffi.new(ct)
+      end,
+      __index = function (self, k)
+	 return struct_mf[k]
+      end,
+   }
+   return ffi.metatype(elct, struct_mt)
+end
+
+return {
+   Struct = Struct,
+}
---
+++ Unit/test_StructUtils.lua
@@ -0,0 +1,100 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+-- Test for range objects
+--    _______     ___
+-- + 6 @ |||| # P ||| +
+--------------------------------------------------------------------------------
+
+local ffi  = require "ffi"
+local Unit = require "Unit"
+local StructUtils = require "Lib.StructUtils"
+
+local assert_equal = Unit.assert_equal
+local stats = Unit.stats
+
+ffi.cdef [[
+  typedef struct {
+    double x, y, z;
+  } Particle_t;
+ 
+  /* To illustrate behaviour of pointers */
+  typedef struct {
+    Particle_t *p1, *p2;
+  } TwoParticles_t;
+]]
+
+-- constructor for structs
+local Particle = StructUtils.Struct("Particle_t")
+local TwoParticles = StructUtils.Struct("TwoParticles_t")
+
+function test_1()
+   local p1 = Particle {1.5, 2.5, 3.5 }
+   assert_equal(1.5, p1.x, "Checking struct x")
+   assert_equal(2.5, p1.y, "Checking struct y")
+   assert_equal(3.5, p1.z, "Checking struct z")
+
+   local p2 = p1:clone()
+   assert_equal(1.5, p2.x, "Checking struct clone x")
+   assert_equal(2.5, p2.y, "Checking struct clone y")
+   assert_equal(3.5, p2.z, "Checking struct clone z")
+
+   p2.x = 10.5
+   assert_equal(10.5, p2.x, "Checking struct clone x")
+
+   assert_equal(1.5, p1.x, "Checking struct x")
+end
+
+function test_2()
+   local pa = Particle {1.5, 2.5, 3.5 }
+   local pb = Particle {10.5, 20.5, 30.5 }
+
+   local twoP = TwoParticles { pa, pb }
+
+   assert_equal(1.5, twoP.p1.x, "Checking pointers")
+   assert_equal(2.5, twoP.p1.y, "Checking pointers")
+   assert_equal(3.5, twoP.p1.z, "Checking pointers")
+
+   assert_equal(10.5, twoP.p2.x, "Checking pointers")
+   assert_equal(20.5, twoP.p2.y, "Checking pointers")
+   assert_equal(30.5, twoP.p2.z, "Checking pointers")
+
+   local cloneTwoP = twoP:clone()
+   assert_equal(1.5, cloneTwoP.p1.x, "Checking pointers")
+   assert_equal(2.5, cloneTwoP.p1.y, "Checking pointers")
+   assert_equal(3.5, cloneTwoP.p1.z, "Checking pointers")
+
+   assert_equal(10.5, cloneTwoP.p2.x, "Checking pointers")
+   assert_equal(20.5, cloneTwoP.p2.y, "Checking pointers")
+   assert_equal(30.5, cloneTwoP.p2.z, "Checking pointers")
+
+   -- now change pa and pb
+   pa.x, pa.y, pa.z = 100.5, 200.5, 300.5
+   pb.x, pb.y, pb.z = 1000.5, 2000.5, 3000.5
+
+   -- change is reflected in both twoP as well as cloneTwoP
+   assert_equal(100.5, twoP.p1.x, "Checking pointers")
+   assert_equal(200.5, twoP.p1.y, "Checking pointers")
+   assert_equal(300.5, twoP.p1.z, "Checking pointers")
+
+   assert_equal(1000.5, twoP.p2.x, "Checking pointers")
+   assert_equal(2000.5, twoP.p2.y, "Checking pointers")
+   assert_equal(3000.5, twoP.p2.z, "Checking pointers")
+
+   assert_equal(100.5, cloneTwoP.p1.x, "Checking pointers")
+   assert_equal(200.5, cloneTwoP.p1.y, "Checking pointers")
+   assert_equal(300.5, cloneTwoP.p1.z, "Checking pointers")
+
+   assert_equal(1000.5, cloneTwoP.p2.x, "Checking pointers")
+   assert_equal(2000.5, cloneTwoP.p2.y, "Checking pointers")
+   assert_equal(3000.5, cloneTwoP.p2.z, "Checking pointers")
+end
+
+test_1()
+test_2()
+
+if stats.fail > 0 then
+   print(string.format("\nPASSED %d tests", stats.pass))
+   print(string.format("**** FAILED %d tests", stats.fail))
+else
+   print(string.format("PASSED ALL %d tests!", stats.pass))
+end
