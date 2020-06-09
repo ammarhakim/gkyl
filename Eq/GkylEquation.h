44d3d30d2e78f2c7b4725cba951cbae899821196
ammarhakim <a.hakim777@gmail.com>
2020-06-09 11:58:29 -0400

    Adding base structure for equations and hooking in the various functions needed. Now to add specific equation objects for Vlasov.

--- Eq/GkylEquation.h
+++ Eq/GkylEquation.h
@@ -3,6 +3,58 @@
 
 #include <GkylCudaConfig.h>
 
+extern "C"  {
+    
+    // Function pointer types for volume and surface terms
+    typedef double (*volTermFunc_t)(void *self, 
+      double *xc, double *dx, int *idx, double *qIn, double *qRhsOut);
+    
+    typedef double (*surfTermFunc_t)(void *self, int dir, double *cflL, double *cflR,
+      double *xcL, double *xcR, double *dxL, double *dxR,
+      double maxsOld, int* idxL, int *idxR,
+      double *qInL, double *qInR, double *qRhsOutL, double *qRhsOutR);
+
+    typedef double (*boundarySurfTermFunc_t)(void *self, int dir, double *cflL, double *cflR,
+      double *xcL, double *xcR, double *dxL, double *dxR,
+      double maxsOld, int* idxL, int *idxR,
+      double *qInL, double *qInR, double *qRhsOutL, double *qRhsOutR);
+
+    typedef struct {
+        // Pointer to specific equation object
+        void *equation;
+        // pointer to equation volTerm function
+        volTermFunc_t equationVolTerm;
+        // pointer to equation surfTerm function
+        surfTermFunc_t equationSurfTerm;
+         // pointer to equation boundarySurfTerm function
+        boundarySurfTermFunc_t equationBoundarySurfTerm;
+        
+        __host__ __device__ double volTerm(double *xc, double *dx, int *idx, double *qIn, double *qRhsOut) {
+          return equationVolTerm(equation, xc, dx, idx, qIn, qRhsOut);
+        }
+
+        __host__ __device__ double surfTerm(int dir, double *cflL, double *cflR,
+          double *xcL, double *xcR, double *dxL, double *dxR,
+          double maxsOld, int* idxL, int *idxR,
+          double *qInL, double *qInR, double *qRhsOutL, double *qRhsOutR) {
+          return equationSurfTerm(equation, dir, cflL, cflR,
+            xcL, xcR, dxL, dxR,
+            maxsOld, idxL, idxR,
+            qInL, qInR, qRhsOutL, qRhsOutR);
+        }
+
+        __host__ __device__ double boundarySurfTerm(int dir, double *cflL, double *cflR,
+          double *xcL, double *xcR, double *dxL, double *dxR,
+          double maxsOld, int* idxL, int *idxR,
+          double *qInL, double *qInR, double *qRhsOutL, double *qRhsOutR) {
+          return equationBoundarySurfTerm(equation, dir, cflL, cflR,
+            xcL, xcR, dxL, dxR,
+            maxsOld, idxL, idxR,
+            qInL, qInR, qRhsOutL, qRhsOutR);
+        }
+    } GkylEquation_t;
+}
+
 class Equation {
  public:
   Equation() {};
