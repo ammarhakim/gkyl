6eb681bd1459d08ede797457a1c3605ec13400cc
Liang Wang <liang.wang.phys@gmail.com>
2020-06-08 23:39:17 -0400

    GkylEuler: use cuda math funcs

--- Eq/GkylEuler.cpp
+++ Eq/GkylEuler.cpp
@@ -38,12 +38,12 @@ namespace Gkyl {
 
     double pr = pressure(ql);
     double u = ql[d[1]]/ql[0];
-    double cs = std::sqrt(_gasGamma*pr/ql[0]);
+    double cs = sqrt(_gasGamma*pr/ql[0]);
     double sl = u+cs;
 
     pr = pressure(qr);
     u = qr[d[1]]/qr[0];
-    cs = std::sqrt(_gasGamma*pr/qr[0]);
+    cs = sqrt(_gasGamma*pr/qr[0]);
     double sr = u+cs   ;
 
     s[1] = 0.5*(sl+sr);
@@ -55,44 +55,44 @@ namespace Gkyl {
       double *apdq) {
     const int *d = dirShuffle[dir];
 
-   // Arrange fluctuations to sum to jump in physical flux.
-   auto fl = _fl;
-   auto fr = _fr;
+    // Arrange fluctuations to sum to jump in physical flux.
+    auto fl = _fl;
+    auto fr = _fr;
 
-   // Left fluxes.
-   auto pr = pressure(ql);
-   auto u = ql[d[1]]/ql[0];
-   fl[0] = ql[d[1]]; // rho*u
-   fl[d[1]] = ql[d[1]]*u + pr; // rho*u*u + p
-   fl[d[2]] = ql[d[2]]*u; // rho*v*u
-   fl[d[3]] = ql[d[3]]*u; // rho*w*u
-   fl[4] = (ql[4]+pr)*u; // (E+p)*u
-   auto absMaxsl = abs(u)+sqrt(_gasGamma*pr/ql[1]);
+    // Left fluxes.
+    auto pr = pressure(ql);
+    auto u = ql[d[1]]/ql[0];
+    fl[0] = ql[d[1]]; // rho*u
+    fl[d[1]] = ql[d[1]]*u + pr; // rho*u*u + p
+    fl[d[2]] = ql[d[2]]*u; // rho*v*u
+    fl[d[3]] = ql[d[3]]*u; // rho*w*u
+    fl[4] = (ql[4]+pr)*u; // (E+p)*u
+    auto absMaxsl = abs(u)+sqrt(_gasGamma*pr/ql[1]);
 
-   // Right fluxes.
-   pr = pressure(qr);
-   u = qr[d[1]]/qr[0];
-   fr[0] = qr[d[1]]; // rho*u
-   fr[d[1]] = qr[d[1]]*u + pr; // rho*u*u + p
-   fr[d[2]] = qr[d[2]]*u; // rho*v*u
-   fr[d[3]] = qr[d[3]]*u; // rho*w*u
-   fr[4] = (qr[4]+pr)*u; // (E+p)*u
-   auto absMaxsr =abs(u)+sqrt(_gasGamma*pr/qr[1]);
+    // Right fluxes.
+    pr = pressure(qr);
+    u = qr[d[1]]/qr[0];
+    fr[0] = qr[d[1]]; // rho*u
+    fr[d[1]] = qr[d[1]]*u + pr; // rho*u*u + p
+    fr[d[2]] = qr[d[2]]*u; // rho*v*u
+    fr[d[3]] = qr[d[3]]*u; // rho*w*u
+    fr[4] = (qr[4]+pr)*u; // (E+p)*u
+    auto absMaxsr =abs(u)+sqrt(_gasGamma*pr/qr[1]);
 
-   auto absMaxs = fmaxf(absMaxsl, absMaxsr);
+    auto absMaxs = fmaxf(absMaxsl, absMaxsr);
 
-   // Left going fluctuations.
-   amdq[0] = 0.5*(fr[0]-fl[0] - absMaxs*(qr[0]-ql[0]));
-   amdq[d[1]] = 0.5*(fr[d[1]]-fl[d[1]] - absMaxs*(qr[d[1]]-ql[d[1]]));
-   amdq[d[2]] = 0.5*(fr[d[2]]-fl[d[2]] - absMaxs*(qr[d[2]]-ql[d[2]]));
-   amdq[d[3]] = 0.5*(fr[d[3]]-fl[d[3]] - absMaxs*(qr[d[3]]-ql[d[3]]));
-   amdq[4] = 0.5*(fr[4]-fl[4] - absMaxs*(qr[4]-ql[4]));
+    // Left going fluctuations.
+    amdq[0] = 0.5*(fr[0]-fl[0] - absMaxs*(qr[0]-ql[0]));
+    amdq[d[1]] = 0.5*(fr[d[1]]-fl[d[1]] - absMaxs*(qr[d[1]]-ql[d[1]]));
+    amdq[d[2]] = 0.5*(fr[d[2]]-fl[d[2]] - absMaxs*(qr[d[2]]-ql[d[2]]));
+    amdq[d[3]] = 0.5*(fr[d[3]]-fl[d[3]] - absMaxs*(qr[d[3]]-ql[d[3]]));
+    amdq[4] = 0.5*(fr[4]-fl[4] - absMaxs*(qr[4]-ql[4]));
 
-   // Right going fluctuations.
-   apdq[0]  = 0.5*(fr[0]-fl[0] + absMaxs*(qr[0]-ql[0]));
-   apdq[d[1]] = 0.5*(fr[d[1]]-fl[d[1]] + absMaxs*(qr[d[1]]-ql[d[1]]));
-   apdq[d[2]] = 0.5*(fr[d[2]]-fl[d[2]] + absMaxs*(qr[d[2]]-ql[d[2]]));
-   apdq[d[3]] = 0.5*(fr[d[3]]-fl[d[3]] + absMaxs*(qr[d[3]]-ql[d[3]]));
-   apdq[4] = 0.5*(fr[4]-fl[4] + absMaxs*(qr[4]-ql[4]));
+    // Right going fluctuations.
+    apdq[0]  = 0.5*(fr[0]-fl[0] + absMaxs*(qr[0]-ql[0]));
+    apdq[d[1]] = 0.5*(fr[d[1]]-fl[d[1]] + absMaxs*(qr[d[1]]-ql[d[1]]));
+    apdq[d[2]] = 0.5*(fr[d[2]]-fl[d[2]] + absMaxs*(qr[d[2]]-ql[d[2]]));
+    apdq[d[3]] = 0.5*(fr[d[3]]-fl[d[3]] + absMaxs*(qr[d[3]]-ql[d[3]]));
+    apdq[4] = 0.5*(fr[4]-fl[4] + absMaxs*(qr[4]-ql[4]));
   }
 }
--- Eq/GkylEuler.h
+++ Eq/GkylEuler.h
@@ -11,7 +11,6 @@
 #include <memory>
 #include <string>
 #include <vector>
-#include <cmath>
 
 namespace Gkyl {
 
