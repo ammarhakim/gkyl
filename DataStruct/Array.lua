0bf76ae3e4701e6f93382a4e8c0e3895a5506680
ammarhakim <a.hakim777@gmail.com>
2020-06-16 23:55:26 -0400

    Array c-struct in near-final form. Added tests including on how to hook into another C-struct without memory leaks or pointers disappearing. CUDA clone and then hooking into the CartField

--- DataStruct/Array.lua
+++ DataStruct/Array.lua
@@ -18,60 +18,94 @@ local Alloc = require "Lib.Alloc"
 
 _M = {}
 
--- Array object is a data containter for storing multi-dimensional
--- data. The interface to this object is delibrately kept low-level
--- and is accessible via the C-struct definition of the array
--- directly. This is done so as to make communication with C-code
--- (including CUDA) easy and via a simple interface.
---
--- Arrays are reference counted. However, for this to work one must be
--- disciplined. If you need to store a reference in another object or
--- C-struct, please obtain it with the aquire() method. When you are
--- done with the pointer release it using the release()
--- method. Release can be automated by proper use of __gc method in
--- the struct metamethods. If you do not do this memory can leak and,
--- very seriously, can disappear under you.
---
--- In general, no ordering is imposed on the data. Ordering needs to
--- be provided elsewhere. For example, there is no mechanism to loop
--- over the array in row/col major order. If one needs to do this one
--- can use the Range object to impose an order and do looping.
+_M.__help = [=[
 
--- Array structure. The "d" pointer points to the shape information
--- and also the data stored in the array. d[0] ... d[r-1] store the
--- shape of the array and the rest of the elements of 'd' store the
--- actual stored data. For accessing this data the pointer (d+r) needs
--- to be cast to the proper C-pointer.
+Array object is a data containter for storing multi-dimensional
+data. The interface to this object is delibrately kept low-level and
+is accessible via the C-struct definition of the array directly. This
+is done so as to make communication with C-code (including CUDA) easy
+and via a simple interface.
+
+Arrays are reference counted. However, for this to work one must be
+disciplined. If you need to store a pointer to the array in another
+object or C-struct, please obtain it with the aquire() method. When
+you are done with the pointer release it using the release()
+method. Release can be automated by proper use of __gc method in the
+struct metamethods. If you do not do this memory can leak and, very
+seriously, can disappear under you.
+
+In general, no ordering is imposed on the data. Ordering needs to be
+provided elsewhere. For example, there is no mechanism to loop over
+the array in row/col major order. If one needs to do this one can use
+the Range object to impose an order and do looping.
+
+Array structure. The "d" pointer points data stored in the array. This
+is stored as void* and so needs to be cast to the proper C-pointer
+before use. To do this, if 'arr' is the array and it stores doubles
+then do:
+
+local d = ffi.cast("double*", arr.d)
+
+At this point, d[0] ... d[n-1] give you access to the values stored in
+the array.
+
+Recall that the size and shape are stored as long int. This is needed
+as int are too small to store information for very large array we may
+need in many simulations. Hence, you need to use tonumber() Lua method
+to convert the size and shape to something Lua can use (for example in
+loops).
+
+ ]=]
 
 ffi.cdef [[
   typedef struct {
-    long r, n, c, s, *d; // rank, size, use-count, sizeof element, data
+    int r, c, sz; // rank, use-count, element-size
+    long n, *s; // total number of elements and shape
+    void *d; // pointer to data
   } GkylArray_t;
 ]]
 
+local longSz = sizeof("long")
+
 -- Various types for arrays of basic C-types
-_M.float = { typeof('float'), typeof('float*') }
-_M.double = { typeof('double'), typeof('double*') }
-_M.int = { typeof('int'), typeof('int*') }
-_M.long = { typeof('long'), typeof('long*') }
-_M.char = { typeof('char'), typeof('char*') }
+_M.float = typeof('float')
+_M.double = typeof('double')
+_M.int = typeof('int')
+_M.long = typeof('long')
+_M.char = typeof('char')
 
 -- Array ctype
 local ArrayCt = typeof("GkylArray_t")
 
--- compute size in bytes to allocate
-local calcSize = function (r, N, sz)
-   return r*sizeof("long") + N*sz
-end
-
 local array_fn = {
+   rank = function (self)
+      return self.r
+   end,
+   shape = function (self)
+      local s = {}
+      for i = 1, self.r do
+	 s[i] = tonumber(self.s[i-1])
+      end
+      return s
+   end,
+   size = function (self)
+      return tonumber(self.n)
+   end,
+   elemSize = function (self)
+      return self.sz
+   end,
    clone = function (self)
       local a = new(self)
-      local totalSz = calcSize(self.r, self.n, self.s)
-      a.d = Alloc.malloc(totalSz)
-      a.r, a.n, a.s = self.r, self.n, self.s
+      a.r, a.n, a.sz = self.r, self.n, self.sz
+      
+      a.d = Alloc.calloc(self.n, self.sz)
+      ffi.copy(a.d, self.d, self.n*self.sz)
+
+      a.s = Alloc.malloc(self.r*longSz)
+      ffi.copy(a.s, self.s, self.r*longSz)
+
       a.c = 1 -- newly created
-      ffi.copy(a.d, self.d, totalSz)
+
       return a
    end,
    aquire = function (self)
@@ -81,8 +115,8 @@ local array_fn = {
    release = function (self)
       self.c = self.c-1
       if self.c == 0 then
-	 Alloc.free(self.d);
-	 self.d = 0; self.p = 0
+	 Alloc.free(self.s)
+	 Alloc.free(self.d)
       end
    end,
 }
@@ -94,12 +128,14 @@ local array_mt = {
       -- compute total number of elements
       a.n = 1
       for i = 1, #shape do a.n = a.n*shape[i] end
-      a.c, a.s = 1, sizeof(atype)
+      a.c, a.sz = 1, sizeof(atype)
+
       -- allocate memory
-      local totalSz = calcSize(a.r, a.n, a.s)
-      a.d = Alloc.malloc(totalSz); ffi.fill(a.d, totalSz)
+      a.d = Alloc.calloc(a.n, a.sz)
+
       -- set shape of array
-      for i = 1, #shape do a.d[i-1] = shape[i] end
+      a.s = Alloc.malloc(a.r*longSz)
+      for i = 1, #shape do a.s[i-1] = shape[i] end
       return a
    end,
    __gc = function(self)
@@ -111,16 +147,7 @@ local ArrayCtor = metatype(ArrayCt, array_mt)
 
 -- Construct array of given shape and type
 _M.Array = function (shape, atype)
-   if type(atype) == "table" then
-      return ArrayCtor(shape, atype[1])
-   else
-      return ArrayCtor(shape, atype)
-   end
-end
-
--- Get data-pointer to data in array, case to proper C-type
-_M.dataPtr = function (arr, atype)
-   return ffi.cast(atype[2], arr.d+arr.r)
+   return ArrayCtor(shape, atype)
 end
 
 return _M
--- Unit/test_Array.lua
+++ Unit/test_Array.lua
@@ -15,28 +15,96 @@ local stats = Unit.stats
 function test_1()
    local arr = Array.Array( {10, 20}, Array.double)
 
-   assert_equal(2, arr.r, "Testing array rank")
-   assert_equal(10, arr.d[0], "Testing array shape")
-   assert_equal(20, arr.d[1], "Testing array shape")
+   assert_equal(2, arr:rank(), "Testing array rank")
+   local shape = arr:shape()
+   assert_equal(10, shape[1], "Testing array shape")
+   assert_equal(20, shape[2], "Testing array shape")
    assert_equal(1, arr.c, "Use count")
-   assert_equal(200, arr.n, "Total n")
-   assert_equal(ffi.sizeof("double"), arr.s, "Size of element")
-   local arrData = Array.dataPtr(arr, Array.double)
-   assert_equal(true, arrData == arr.d+arr.r)
+   assert_equal(200, arr:size(), "Total n")
+   assert_equal(ffi.sizeof("double"), arr:elemSize(), "Size of element")
 
    local brr = arr:clone()
-   assert_equal(2, brr.r, "Testing array rank")
-   assert_equal(10, brr.d[0], "Testing array shape")
-   assert_equal(20, brr.d[1], "Testing array shape")
+   assert_equal(2, brr:rank(), "Testing brray rank")
+   shape = brr:shape()
+   assert_equal(10, shape[1], "Testing brray shape")
+   assert_equal(20, shape[2], "Testing brray shape")
    assert_equal(1, brr.c, "Use count")
-   assert_equal(200, brr.n, "Total n")
-   assert_equal(ffi.sizeof("double"), brr.s, "Size of element")
-   local brrData = Array.dataPtr(brr, Array.double)
-   assert_equal(true, brrData == brr.d+brr.r)
+   assert_equal(200, brr:size(), "Total n")
+   assert_equal(ffi.sizeof("double"), brr:elemSize(), "Size of element")
+end
+
+function test_2()
+   local arr = Array.Array( {10}, Array.double)
+   -- fetch pointer to data
+   local arr_d = ffi.cast("double *", arr.d)
+
+   -- set values
+   for i = 0, tonumber(arr.s[0])-1 do
+      arr_d[i] = i+0.5
+   end
+
+   local brr = arr:clone()
+   local brr_d = ffi.cast("double *", brr.d)
+   -- check values
+   for i = 0, tonumber(brr.s[0])-1 do
+      assert_equal(i+0.5, brr_d[i], "Checking clone")
+   end   
+end
+
+function test_3()
+   local a = Array.Array( {10, 15, 20}, Array.float)
+   
+   -- Aquire pointer and store in a C-struct
+   local ds = ffi.new(ffi.typeof("struct { GkylArray_t *d; } "), a:aquire())
+   assert_equal(2, a.c, "Checking use-count")
+
+   -- Original 'a' can go away at this point
+   a = nil
+   collectgarbage("collect") -- force 'a' to dissapear
+
+   assert_equal(1, ds.d.c, "Checking use-count")
+   assert_equal(3, ds.d:rank(), "Rank")
+
+   -- Release pointer to prevent memory leak
+   ds.d:release()
+end
+
+function test_4()
+   local a = Array.Array( {10, 15, 20}, Array.float)
+   
+   -- Aquire pointer and store in a C-struct: in this test we attach a
+   -- destructor to the created object in which we release() the
+   -- aquire()-ed pointer
+   local ds = ffi.gc(
+      ffi.new(ffi.typeof("struct { GkylArray_t *d; } "), a:aquire()),
+      function (p) p.d:release() end
+   )
+   assert_equal(2, a.c, "Checking use-count")
+
+   -- Original 'a' can go away at this point
+   a = nil
+   collectgarbage("collect") -- force 'a' to dissapear
+
+   assert_equal(1, ds.d.c, "Checking use-count")
+   assert_equal(3, ds.d:rank(), "Rank")
+end
+
+function test_5()
+   local a = Array.Array( {10},
+      ffi.typeof("struct { double x, y, z;}")
+   )
+   assert_equal(3*ffi.sizeof("double"), a:elemSize(), "Checking size of custom type")
+
+   local dptr = ffi.cast(ffi.typeof("struct { double x, y, z;} *"), a.d)
+   dptr[0].x, dptr[0].y, dptr[0].z = 1.0, 2.0, 3.0
 end
 
 -- Run tests
 test_1()
+test_2()
+test_3()
+test_4()
+test_5()
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
