8131c0e9e57acb122877f1917f933e681028405c
Liang Wang <liang.wang.phys@gmail.com>
2020-06-22 21:52:46 -0400

    cuda/WavePropagation: remove some intermediate vars; formatting

--- Updater/WavePropagationDevice.cu
+++ Updater/WavePropagationDevice.cu
@@ -2,10 +2,11 @@
 #include <GkylWavePropagation.h>
 
 __device__ inline  static void calcFirstOrderGud(
-  const double dtdx, double *ql, double *qr, const double *amdq,
-  const double *apdq, const int meqn)
+    const double dtdx, double *ql, double *qr, const double *amdq,
+    const double *apdq, const int meqn)
 {
-  for (int i = 0; i < meqn; i++) {
+  for (int i = 0; i < meqn; i++)
+  {
     /* qr[i] -= dtdx * apdq[i]; */
     /* ql[i] -= dtdx * amdq[i]; */
     // XXX
@@ -15,33 +16,39 @@ __device__ inline  static void calcFirstOrderGud(
 }
 
 __device__ static inline double calcCfla(
-  const double cfla, const double dtdx, const double *speeds, const int mwave)
+    const double cfla, const double dtdx, const double *speeds, const int mwave)
 {
   double c = cfla;
-  for (int i = 0; i < mwave; i ++) {
+  for (int i = 0; i < mwave; i ++)
+  {
     c = max(c, dtdx * abs(speeds[i]));
   }
   return c;
 }
 
 __device__ static inline double waveDotProd(
-    const double *waves, const double *waves1, const int mw, const int meqn) {
+    const double *waves, const double *waves1, const int mw, const int meqn)
+{
   double result = 0.;
-  for (int i = 0; i < meqn; i++) {
+  for (int i = 0; i < meqn; i++)
+  {
     result += waves[meqn*mw+i] * waves1[meqn*mw+i];
   }
   return result;
 }
 
-__device__ static inline double limiter_minMod(const double r) {
+__device__ static inline double limiter_minMod(const double r)
+{
    return max(0., min(1., r));
 }
 
 __device__ static void limitWaves(
     const double *waves, const double *speeds, double *limitedWaves,
-    const int mwave, const int meqn) {
+    const int mwave, const int meqn)
+{
   int jump = meqn * mwave;
-  for (int mw = 0; mw < mwave; mw++ ){
+  for (int mw = 0; mw < mwave; mw++ )
+  {
     const double wnorm2 = waveDotProd(waves, waves, mw, meqn);
     if (wnorm2 > 0) {
       double wlimitr = 1.;
@@ -57,66 +64,66 @@ __device__ static void limitWaves(
 }
 
 __device__ static inline void secondOrderFluxOneWave(
-  const double dtdx, const double speed, const double *wave, double *fs,
-  const int meqn) {
+    const double dtdx, const double speed, const double *wave, double *fs,
+    const int meqn) {
   double sfact = 0.5 * abs(speed) * (1 - abs(speed) * dtdx);
-  for (int i = 0; i < meqn; i++) {
+  for (int i = 0; i < meqn; i++)
+  {
     fs[i] += sfact * wave[i];
   }
 }
 
 __device__ static void secondOrderFlux(
-  const double dtdx, const double *speeds, const double *waves, double *fs,
-  const int meqn, const int mwave) {
-    for (int mw = 0; mw < mwave; mw++) {
+    const double dtdx, const double *speeds, const double *waves, double *fs,
+    const int meqn, const int mwave)
+{
+    for (int mw = 0; mw < mwave; mw++)
+    {
       secondOrderFluxOneWave(dtdx, speeds[mw], waves+mw*meqn, fs, meqn);
     }
 }
 
 __device__ static inline void secondOrderUpdate(
     const double dtdx, const double *fs, const double *fs1, double *q,
-    const int meqn) {
-  for (int i = 0; i < meqn; i++) {
+    const int meqn)
+{
+  for (int i = 0; i < meqn; i++)
+  {
     // q[i] -= dtdx * (fs1[i] - fs[i]);
     atomicAdd(q+i, -dtdx * (fs1[i] - fs[i]));
   }
 }
 
 __device__ static inline void copyComponents(
-    const double *ptrFrom, double *ptrTo, const int nComponents) {
-  for (int i = 0; i < nComponents; i++) {
+    const double *ptrFrom, double *ptrTo, const int nComponents)
+{
+  for (int i = 0; i < nComponents; i++)
+  {
     ptrTo[i] = ptrFrom[i];
   }
 }
 
 __global__ void cuda_WavePropagation(
-  GkylWavePropagation_t *hyper, GkylCartField_t *qIn, GkylCartField_t *qOut)
+    GkylWavePropagation_t *hyper, GkylCartField_t *qIn, GkylCartField_t *qOut)
 {
   GkylRange_t *localRange = qIn->localRange;
   Gkyl::GenIndexer fIdxr = qIn->genIndexer();
-  GkylRectCart_t *grid = qIn->grid;
-
-  const int ndim = localRange->ndim;
   Gkyl::GenIndexer localIdxr(localRange);
-
-  const int *updateDirs = hyper->updateDirs;
-  const int numUpdateDirs = hyper->numUpdateDirs;
   GkylEquationFv_t *eq = hyper->equation;
   GkylCartField_t *dtByCell = hyper->dtByCell;
-
   const int meqn = eq->numEquations;
   const int mwave = eq->numWaves;
 
-  int linearIdx = threadIdx.x + blockIdx.x*blockDim.x;
+  const int linearIdx = threadIdx.x + blockIdx.x*blockDim.x;
 
   // assign buffer space for different usages
   extern __shared__ double dummy[];
   int base = 0;
 
-  double *amdq = dummy + base + (meqn) * (threadIdx.x);
+  double *amdq = dummy + base + (meqn)*(threadIdx.x);
   base += (meqn) * (blockDim.x);
 
-  double *apdq = dummy + base + (meqn) * (threadIdx.x);
+  double *apdq = dummy + base + (meqn)*(threadIdx.x);
   base += (meqn) * (blockDim.x);
 
   // numThreads == numRealCells
@@ -124,32 +131,31 @@ __global__ void cuda_WavePropagation(
   // real-real faces, thus the jump is blockDim.x+3;
   // also, the 0th thread will work on one more Rp on its left, thus the address
   // for its own waves/speeds is threadIdx.x+1 (not threadIdx.x)
-  double *waves = dummy + base + (meqn * mwave) * (threadIdx.x+1);
-  base += (meqn * mwave) * (blockDim.x + 3);
+  double *waves = dummy + base + (meqn*mwave)*(threadIdx.x+1);
+  base += (meqn*mwave) * (blockDim.x+3);
 
-  double *speeds = dummy + base + (mwave) * (threadIdx.x+1);
-  base += (mwave) * (blockDim.x + 3);
+  double *speeds = dummy + base + (mwave)*(threadIdx.x+1);
+  base += (mwave) * (blockDim.x+3);
 
   // limitedWaves and second-order fluxs are defiend on ghost-real and
   // real-real faces, thus the jump is blockDim.x+1
-  double *limitedWaves = dummy + base + (meqn * mwave) * (threadIdx.x);
-  base += (meqn * mwave) * (blockDim.x + 1);
+  double *limitedWaves = dummy + base + (meqn*mwave) * (threadIdx.x);
+  base += (meqn*mwave) * (blockDim.x+1);
 
   base = 0; // reusing the shared memory since amdq/apdq are not longer needed
 
-  double *flux = dummy + base + (meqn) * (threadIdx.x);
-  base += (meqn) * (blockDim.x + 1);
+  double *flux = dummy + base + (meqn)*(threadIdx.x);
+  base += (meqn) * (blockDim.x+1);
 
   int idxC[3];
   int idxL[3];
   int idxR[3];
 
-  // get i,j,k... index idxC
+  // get i,j,k... index idxC; real cells have idxC=1,2,...,shape
   localIdxr.invIndex(linearIdx, idxC);
-  // if ndim>1, linearIdxC!=linearIdx since linearIdxC jumps due to ghost cells
+  // linearIdxC knows ghots cells, the 1st real cell has linearIdxC=2
+  // if ndim>1, linearIdxC jumps (i.e., not consecutive) due to ghost cells
   const int linearIdxC = fIdxr.index(idxC);
-
-  const double *dx = grid->dx;
 
   double cfl = hyper->_cfl;
   double cflm = hyper->_cflm;
@@ -160,25 +166,27 @@ __global__ void cuda_WavePropagation(
 
   // ghost cells are not copied, but this is OK because the waves and speeds are
   // computed using qIn anyway
-  if(linearIdx < localRange->volume()) {
+  if(linearIdx < localRange->volume())
+  {
     copyComponents(qInC, qOutC, meqn);
   }
 
-  for(int i=0; i<numUpdateDirs; i++) {
-    int dir = updateDirs[i] - 1;
-    const double dtdx = hyper->dt / dx[dir];
+  for(int i=0; i<hyper->numUpdateDirs; i++)
+  {
+    const int dir = hyper->updateDirs[i] - 1;
+    const double dtdx = hyper->dt / qIn->grid->dx[dir];
 
-    for(int d=0; d<ndim; d++) {
+#pragma unroll
+    for(int d=0; d<3; d++)
+    {
       idxL[d] = idxC[d];
       idxR[d] = idxC[d];
     }
-    idxL[dir] = idxC[dir] - 1;
-    idxR[dir] = idxC[dir];
+    idxL[dir] = idxC[dir]-1;
 
-    bool isLoDevice = idxC[dir] == 1;
-    bool isHiDevice = idxC[dir] == localRange->shape(dir);
-    bool isLoBlock = threadIdx.x==0;
-    bool isHiBlock = threadIdx.x==blockDim.x-1;
+    const bool isLoBlockOrDevice = threadIdx.x==0 || idxC[dir]==1;
+    const bool isHiDevice = idxC[dir]==localRange->shape(dir);
+    const bool isHiBlockOrDevice = threadIdx.x==blockDim.x-1 || isHiDevice;
 
     const int linearIdxL = fIdxr.index(idxL);
     const int linearIdxR = fIdxr.index(idxR);
@@ -188,16 +196,18 @@ __global__ void cuda_WavePropagation(
     double *qOutL = qOut->getDataPtrAt(linearIdxL);
     double *qOutR = qOut->getDataPtrAt(linearIdxR);
 
-    if(linearIdx < localRange->volume()) {
+    if(linearIdx < localRange->volume())
+    {
       eq->rp(dir, qInL, qInR, waves, speeds);
       eq->qFluctuations(dir, qInL, qInR, waves, speeds, amdq, apdq);
       calcFirstOrderGud(dtdx, qOutL, qOutR, amdq, apdq, meqn);
       cfla = calcCfla(cfla, dtdx, speeds, mwave);
-      copyComponents(waves, limitedWaves, meqn * mwave);
+      copyComponents(waves, limitedWaves, meqn*mwave);
 
       // can we avoid branching?
       // solve one additional Riemann problem on the lower side
-      if (isLoBlock || isLoDevice) {
+      if (isLoBlockOrDevice)
+      {
         int inc = -1;
         idxL[dir] += inc;
         idxR[dir] += inc;
@@ -215,8 +225,10 @@ __global__ void cuda_WavePropagation(
 
       // solve two additional Riemann problems on the higher side and update the
       // last real cell
-      if (isHiBlock || isHiDevice) {
-        for (int inc = 1; inc < 3; inc++) {
+      if (isHiBlockOrDevice)
+      {
+        for (int inc = 1; inc < 3; inc++)
+        {
           idxL[dir] += inc;
           idxR[dir] += inc;
           const int linearIdxL = fIdxr.index(idxL);
@@ -227,11 +239,13 @@ __global__ void cuda_WavePropagation(
           eq->rp(dir, qInL, qInR, waves+inc*meqn*mwave, speeds+inc*mwave);
           cfla = calcCfla(cfla, dtdx, speeds+inc*mwave, mwave);
 
-          if (inc==1) {
+          if (inc==1)
+          {
             copyComponents(
                 waves+inc*meqn*mwave, limitedWaves+inc*meqn*mwave, meqn*mwave);
 
-            if (isHiDevice) {
+            if (isHiDevice)
+            {
               double *qOutL = qOut->getDataPtrAt(linearIdxL);
               double *qOutR = qOut->getDataPtrAt(linearIdxR);
               eq->qFluctuations(
@@ -248,10 +262,12 @@ __global__ void cuda_WavePropagation(
     }
 
     __syncthreads();
-    if(linearIdx < localRange->volume()) {
+    if(linearIdx < localRange->volume())
+    {
       limitWaves(waves, speeds, limitedWaves, mwave, meqn);
 
-      if (isHiBlock || isHiDevice) {
+      if (isHiBlockOrDevice)
+      {
         limitWaves(
             waves+meqn*mwave, speeds+mwave, limitedWaves+meqn*mwave, mwave,
             meqn);
@@ -259,14 +275,17 @@ __global__ void cuda_WavePropagation(
     }
 
     __syncthreads();
-    if(linearIdx < localRange->volume()) {
-      for (int c = 0; c < meqn; c++) {
+    if(linearIdx < localRange->volume())
+    {
+      for (int c=0; c<meqn; c++)
+      {
         flux[c] = 0;
       }
       secondOrderFlux(dtdx, speeds, limitedWaves, flux, meqn, mwave);
 
-      if (isHiBlock || isHiDevice) {
-        for (int c = 0; c < meqn; c++) {
+      if (isHiBlockOrDevice) {
+        for (int c=0; c<meqn; c++)
+        {
           (flux+meqn)[c] = 0;
         }
         secondOrderFlux(
@@ -276,7 +295,8 @@ __global__ void cuda_WavePropagation(
     }
 
     __syncthreads();
-    if(linearIdx < localRange->volume()) {
+    if(linearIdx < localRange->volume())
+    {
       secondOrderUpdate(dtdx, flux, flux+meqn, qOutC, meqn);
     }
   }
@@ -285,13 +305,13 @@ __global__ void cuda_WavePropagation(
 }
 
 void wavePropagationAdvanceOnDevice(
-  const int meqn, const int mwave, const int numBlocks, const int numThreads,
-  GkylWavePropagation_t *hyper, GkylCartField_t *qIn, GkylCartField_t *qOut)
+    const int meqn, const int mwave, const int numBlocks, const int numThreads,
+    GkylWavePropagation_t *hyper, GkylCartField_t *qIn, GkylCartField_t *qOut)
 {
   int sharedMemSize = 0;
-  // amdq andapdq
-  sharedMemSize += (numThreads) * (meqn+meqn);
   // numThreads == numRealCellsPerBlock
+  // amdq and apdq
+  sharedMemSize += (numThreads) * (meqn+meqn);
   // waves & speeds are needed on all real-real, real-ghost, and ghost-ghost
   // cell faces
   sharedMemSize += (numThreads+3) * (mwave*meqn+mwave);
@@ -306,10 +326,12 @@ void wavePropagationAdvanceOnDevice(
       hyper, qIn, qOut);
 }
 
-__global__ void setDtOnDevice(GkylWavePropagation_t *hyper, double dt) {
+__global__ void setDtOnDevice(GkylWavePropagation_t *hyper, double dt)
+{
   hyper->dt = dt;
 }
 
-void setDt(GkylWavePropagation_t *hyper, double dt) {
+void setDt(GkylWavePropagation_t *hyper, double dt)
+{
   setDtOnDevice<<<1, 1>>>(hyper, dt);
 }
