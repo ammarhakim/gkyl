952d3fa813c17cae0b323631eea52d677cee7fb7
Noah Mandell <nrmandell@gmail.com>
2021-04-22 16:33:32 -0400

    updating engaging machine files. also adding --enable-cuda (default true) and --disable-cuda flags to the waf configure command. --disable-cuda makes it so that cuda can be disabled even if nvcc can be found.

--- machines/configure.engaging.sh
+++ machines/configure.engaging.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # Edit the paths and options in the following command to suit your system
-module load gcc/8.3.0
+module load gcc/9.3.0
+module load openmpi/4.0.5
 
 # Build directory
 OUT=build
-# Install location
-PREFIX=$HOME/gkylsoft/gkyl
+# if we are in machines directory, go up a directory before 
+# executing commands in this script
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
+
+# Install location... by default, puts gkylsoft directory
+# on same level as gkyl directory (where we are now, presumably)
+export GKYLSOFT=$(readlink -f ../gkylsoft)
+PREFIX=$GKYLSOFT/gkyl
 
 # Compile flags (set optimization/debug flags here)
 CC=gcc
@@ -14,32 +24,30 @@ CXX=g++
 CXXFLAGS='-O3,-std=c++17'
 
 # LuaJIT options
-LUAJIT_INC_DIR=$HOME/gkylsoft/luajit/include/luajit-2.1/
-LUAJIT_LIB_DIR=$HOME/gkylsoft/luajit/lib/
-LUAJIT_SHARE_DIR=$HOME/gkylsoft/luajit/share/luajit-2.1.0-beta3/
+LUAJIT_INC_DIR=$GKYLSOFT/luajit/include/luajit-2.1/
+LUAJIT_LIB_DIR=$GKYLSOFT/luajit/lib/
+LUAJIT_SHARE_DIR=$GKYLSOFT/luajit/share/luajit-2.1.0-beta3/
 
 ## MPI options
-MPICC=$HOME/gkylsoft/openmpi/bin/mpicc
-MPICXX=$HOME/gkylsoft/openmpi/bin/mpicxx
-ENABLE_MPI="--enable-mpi"
-MPI_INC_DIR=$HOME/gkylsoft/openmpi/include/
-MPI_LIB_DIR=$HOME/gkylsoft/openmpi/lib/
+MPICC=mpicc
+MPICXX=mpicxx
+ENABLE_MPI= #"--enable-mpi"
+#MPI_INC_DIR=$GKYLSOFT/openmpi/include/
+#MPI_LIB_DIR=$GKYLSOFT/openmpi/lib/
 MPI_LINK_LIBS="mpi"
 
 # ADIOS options
 ENABLE_ADIOS="--enable-adios" # set to blank to disable ADIOS
-ADIOS_INC_DIR=$HOME/gkylsoft/adios/include/
-ADIOS_LIB_DIR=$HOME/gkylsoft/adios/lib/
+ADIOS_INC_DIR=$GKYLSOFT/adios/include/
+ADIOS_LIB_DIR=$GKYLSOFT/adios/lib/
 
 # EIGEN options
-EIGEN_INC_DIR=$HOME/gkylsoft/eigen3/include/eigen3/
+EIGEN_INC_DIR=$GKYLSOFT/eigen3/include/eigen3/
+
+# CUDA options
+ENABLE_CUDA="--disable-cuda" # disable CUDA, even if nvcc is found
 
 # You probably do not need to modify the command itself
-cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR configure"
-# if we are in machines directory, go up a directory before executing cmd
-if [ `dirname "$0"` == "." ] 
-  then
-    cd ..
-fi
+cmd="./waf CC=$CC CXX=$CXX MPICC=$MPICC MPICXX=$MPICXX --out=$OUT --prefix=$PREFIX --cxxflags=$CXXFLAGS --luajit-inc-dir=$LUAJIT_INC_DIR --luajit-lib-dir=$LUAJIT_LIB_DIR --luajit-share-dir=$LUAJIT_SHARE_DIR $ENABLE_MPI --mpi-inc-dir=$MPI_INC_DIR --mpi-lib-dir=$MPI_LIB_DIR --mpi-link-libs=$MPI_LINK_LIBS $ENABLE_ADIOS --adios-inc-dir=$ADIOS_INC_DIR --adios-lib-dir=$ADIOS_LIB_DIR --eigen-inc-dir=$EIGEN_INC_DIR $ENABLE_CUDA configure"
 echo $cmd
 $cmd
