5c9eceebbcd9d2d3968b9966d6e13b2b58dcb6aa
manauref <mfrancisquez@gmail.com>
2021-05-06 14:21:44 -0700

    Remove vestiges of old source approach in IncompEuler species.

--- App/Sources/SourceBase.lua
+++ App/Sources/SourceBase.lua
@@ -3,4 +3,16 @@ local Proto = require "Lib.Proto"
 -- Empty shell source base class.
 local SourceBase = Proto()
 
+function SourceBase:init(tbl) self.tbl = tbl end
+function SourceBase:fullInit(speciesTbl) end
+function SourceBase:setName(nm) self.name = nm end
+function SourceBase:setSpeciesName(nm) self.speciesName = nm end
+function SourceBase:setConfBasis(basis) self.confBasis = basis end
+function SourceBase:setConfGrid(grid) self.confGrid = grid end
+function SourceBase:createSolver(thisSpecies, extField) end
+function SourceBase:advance(tCurr, fIn, species, fRhsOut) end
+function SourceBase:createDiagnostics(thisSpecies, momTable) end
+function SourceBase:writeDiagnosticIntegratedMoments(tm, frame) end
+function SourceBase:write(tm, frame) end
+
 return SourceBase
