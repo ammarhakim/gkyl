0231352fb6470b0955bc7b2352f0bf07aed93ab3
Liang Wang <liang.wang.phys@gmail.com>
2021-06-06 22:33:25 -0400

    StairStepped: Name changes; rearrangement.

--- Updater/StairSteppedBc.lua
+++ Updater/StairSteppedBc.lua
@@ -12,42 +12,36 @@ local Proto        = require "Lib.Proto"
 local Range        = require "Lib.Range"
 local UpdaterBase  = require "Updater.Base"
 
--- Boundary condition updater
+local isGhost = function (inOutPtr) return inOutPtr[1] < 0 end
+
 local StairSteppedBc = Proto(UpdaterBase)
 
-local isGhost = function (inOutPtr)
-   return inOutPtr[1] < 0
-end
-
 function StairSteppedBc:init(tbl)
-   StairSteppedBc.super.init(self, tbl) -- setup base object
+   StairSteppedBc.super.init(self, tbl)
+   local pfx = "Updater.StairSteppedBc: "
 
-   self._grid = assert(tbl.onGrid, "Updater.StairSteppedBc: Must specify grid to use with 'onGrid''")
-   self._dir  = assert(tbl.dir, "Updater.StairSteppedBc: Must specify direction to apply BCs with 'dir'")
-
-   self._bcList = assert(
-   tbl.boundaryConditions, "Updater.StairSteppedBc: Must specify boundary conditions to apply with 'boundaryConditions'")
-
-   self._localRange = tbl.onGrid:localRange()
-   local localRange = self._localRange
-
-   self._inOut = assert(tbl.inOut, "Updater.StairSteppedBc: Must specify mask field with 'inOut'")
+   self._grid = assert(tbl.onGrid, pfx.."Must specify 'onGrid'.")
+   self._dir  = assert(tbl.dir,
+                       pfx.."Must specify direction to apply BCs with 'dir'.")
+   self._bcList = assert(tbl.boundaryConditions,
+                         pfx.."Must specify 'boundaryConditions'.")
+   self._inOut = assert(tbl.inOut, pfx.."Must specify mask field with 'inOut'.")
 
    self._perpRangeDecomp = LinearDecomp.LinearDecompRange {
-      range      = localRange:shorten(self._dir), -- range orthogonal to 'dir'
+      range      = self._grid:localRange():shorten(self._dir),
       numSplit   = self._grid:numSharedProcs(),
       threadComm = self:getSharedComm()
    }
 end
 
 function StairSteppedBc:_advance(tCurr, inFld, outFld)
+   local qOut = assert(outFld[1],
+                      "StairSteppedBc.advance: Must-specify an output field")
    local grid = self._grid
-   local qOut = assert(outFld[1], "StairSteppedBc.advance: Must-specify an output field")
-   
    local dir = self._dir
    local localRange = grid:localRange()
 
-   -- Pointer and indices to be (re)used.
+   -- Pointer and indices to be reused.
    local qGhost, qSkin = qOut:get(1), qOut:get(1)
    local idxL    = Lin.IntVec(grid:ndim())
    local idxR    = Lin.IntVec(grid:ndim())
@@ -55,10 +49,10 @@ function StairSteppedBc:_advance(tCurr, inFld, outFld)
 
    local inOut          = self._inOut
    local inOutL, inOutR = inOut:get(1), inOut:get(1)
-   local inOutIdxr      = inOut:genIndexer()
+   local inOutIndexer      = inOut:genIndexer()
 
-   local xcIn   = Lin.Vec(self._grid:ndim())
-   local xcOut  = Lin.Vec(self._grid:ndim())
+   local xcSkin   = Lin.Vec(self._grid:ndim())
+   local xcGhost  = Lin.Vec(self._grid:ndim())
 
    -- Loop over directions orthogonal to 'dir'.
    local tId = self._grid:subGridSharedId()
@@ -69,33 +63,33 @@ function StairSteppedBc:_advance(tCurr, inFld, outFld)
       -- Loop over 1D slice in `dir`.
       for i = localRange:lower(dir)-1, localRange:upper(dir)+1 do
          idxL[dir] = i
-         inOut:fill(inOutIdxr(idxL), inOutL)
+         inOut:fill(inOutIndexer(idxL), inOutL)
          local leftCellIsGhost = isGhost(inOutL)
 
          idxR[dir] = i + 1
-         inOut:fill(inOutIdxr(idxR), inOutR)
+         inOut:fill(inOutIndexer(idxR), inOutR)
          local rightCellIsGhost = isGhost(inOutR)
 
          local ghostSkin = leftCellIsGhost and (not rightCellIsGhost)
          local skinGhost = (not leftCellIsGhost) and rightCellIsGhost
 
-         local idxIn
+         local idxSkin
          if ghostSkin or skinGhost then
             if (ghostSkin) then
                qOut:fill(indexer(idxL), qGhost)
                qOut:fill(indexer(idxR), qSkin)
-               self._grid:setIndex(idxL);  self._grid:cellCenter(xcOut);
-               self._grid:setIndex(idxR);  self._grid:cellCenter(xcIn);
-               idxIn = idxR
+               self._grid:setIndex(idxL);  self._grid:cellCenter(xcGhost);
+               self._grid:setIndex(idxR);  self._grid:cellCenter(xcSkin);
+               idxSkin = idxR
             else
                qOut:fill(indexer(idxR), qGhost)
                qOut:fill(indexer(idxL), qSkin)
-               self._grid:setIndex(idxL);  self._grid:cellCenter(xcIn);
-               self._grid:setIndex(idxR);  self._grid:cellCenter(xcOut);
-               idxIn = idxL
+               self._grid:setIndex(idxL);  self._grid:cellCenter(xcSkin);
+               self._grid:setIndex(idxR);  self._grid:cellCenter(xcGhost);
+               idxSkin = idxL
             end
             for _, bc in ipairs(self._bcList) do
-               bc(dir, tCurr, idxIn, qSkin, qGhost, xcOut, xcIn)
+               bc(dir, tCurr, idxSkin, qSkin, qGhost, xcGhost, xcSkin)
             end
          end
       end
