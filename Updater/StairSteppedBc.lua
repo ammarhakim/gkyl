1bbcc3330a9c8e041d93d240b7fd3d22424bc1e9
Liang Wang <liang.wang.phys@gmail.com>
2021-06-06 22:44:30 -0400

    StairStepped: formatted.

--- Updater/StairSteppedBc.lua
+++ Updater/StairSteppedBc.lua
@@ -1,18 +1,13 @@
--- Gkyl ------------------------------------------------------------------------
---
--- Apply boundary conditions at stair-stepped boundaries.
---
---    _______     ___
--- + 6 @ |||| # P ||| +
---------------------------------------------------------------------------------
-local Alloc        = require "Lib.Alloc"
-local Lin          = require "Lib.Linalg"
+----------------------------------------------------------------------
+-- Updter to apply boundary conditions at stair-stepped boundaries. --
+----------------------------------------------------------------------
+local Lin = require "Lib.Linalg"
 local LinearDecomp = require "Lib.LinearDecomp"
-local Proto        = require "Lib.Proto"
-local Range        = require "Lib.Range"
-local UpdaterBase  = require "Updater.Base"
+local Proto = require "Lib.Proto"
+local Range = require "Lib.Range"
+local UpdaterBase = require "Updater.Base"
 
-local isGhost = function (inOutPtr) return inOutPtr[1] < 0 end
+local isGhost = function(inOutPtr) return inOutPtr[1] < 0 end
 
 local StairSteppedBc = Proto(UpdaterBase)
 
@@ -20,39 +15,40 @@ function StairSteppedBc:init(tbl)
    StairSteppedBc.super.init(self, tbl)
    local pfx = "Updater.StairSteppedBc: "
 
-   self._grid = assert(tbl.onGrid, pfx.."Must specify 'onGrid'.")
-   self._dir  = assert(tbl.dir,
-                       pfx.."Must specify direction to apply BCs with 'dir'.")
+   self._grid = assert(tbl.onGrid, pfx .. "Must specify 'onGrid'.")
+   self._dir = assert(tbl.dir,
+                      pfx .. "Must specify direction to apply BCs with 'dir'.")
    self._bcList = assert(tbl.boundaryConditions,
-                         pfx.."Must specify 'boundaryConditions'.")
-   self._inOut = assert(tbl.inOut, pfx.."Must specify mask field with 'inOut'.")
+                         pfx .. "Must specify 'boundaryConditions'.")
+   self._inOut = assert(tbl.inOut,
+                        pfx .. "Must specify mask field with 'inOut'.")
 
    self._perpRangeDecomp = LinearDecomp.LinearDecompRange {
-      range      = self._grid:localRange():shorten(self._dir),
-      numSplit   = self._grid:numSharedProcs(),
+      range = self._grid:localRange():shorten(self._dir),
+      numSplit = self._grid:numSharedProcs(),
       threadComm = self:getSharedComm()
    }
 end
 
 function StairSteppedBc:_advance(tCurr, inFld, outFld)
    local qOut = assert(outFld[1],
-                      "StairSteppedBc.advance: Must-specify an output field")
+                       "StairSteppedBc.advance: Must-specify an output field")
    local grid = self._grid
    local dir = self._dir
    local localRange = grid:localRange()
 
    -- Pointer and indices to be reused.
    local qGhost, qSkin = qOut:get(1), qOut:get(1)
-   local idxL    = Lin.IntVec(grid:ndim())
-   local idxR    = Lin.IntVec(grid:ndim())
+   local idxL = Lin.IntVec(grid:ndim())
+   local idxR = Lin.IntVec(grid:ndim())
    local indexer = qOut:genIndexer()
 
-   local inOut          = self._inOut
+   local inOut = self._inOut
    local inOutL, inOutR = inOut:get(1), inOut:get(1)
-   local inOutIndexer      = inOut:genIndexer()
+   local inOutIndexer = inOut:genIndexer()
 
-   local xcSkin   = Lin.Vec(self._grid:ndim())
-   local xcGhost  = Lin.Vec(self._grid:ndim())
+   local xcSkin = Lin.Vec(self._grid:ndim())
+   local xcGhost = Lin.Vec(self._grid:ndim())
 
    -- Loop over directions orthogonal to 'dir'.
    local tId = self._grid:subGridSharedId()
@@ -61,7 +57,7 @@ function StairSteppedBc:_advance(tCurr, inFld, outFld)
       idx:copyInto(idxR)
 
       -- Loop over 1D slice in `dir`.
-      for i = localRange:lower(dir)-1, localRange:upper(dir)+1 do
+      for i = localRange:lower(dir) - 1, localRange:upper(dir) + 1 do
          idxL[dir] = i
          inOut:fill(inOutIndexer(idxL), inOutL)
          local leftCellIsGhost = isGhost(inOutL)
@@ -99,8 +95,6 @@ function StairSteppedBc:_advance(tCurr, inFld, outFld)
    return true, GKYL_MAX_DOUBLE
 end
 
-function StairSteppedBc:getDir()
-   return self._dir
-end
+function StairSteppedBc:getDir() return self._dir end
 
 return StairSteppedBc
