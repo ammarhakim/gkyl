be5a5202a298ec4794dfe80a7cfcd4a43a834b1d
manauref <mfrancisquez@gmail.com>
2021-06-14 14:34:54 -0400

    Add option to provide spatially varying coefficient in Diffusion app. For higher dimensional sims one can/should provide a vector of numbers and/or functions. These spatially varying coefficients can only depend on the variable along that direction. Add a regression test. The results look smooth.

--- Eq/constDiffusionData/ConstDiffusionSer2xP1.cpp
+++ Eq/constDiffusionData/ConstDiffusionSer2xP1.cpp
@@ -85,3 +85,53 @@ double ConstHyperDiffusion4Vol2xSerP1_diffDirs2(const double *w, const double *d
   return (rdxFnu[0])*0.6666666666666666;
 
 } 
+double ConstDiffusionVarCoeffVol2xSerP1_diffDirs1(const double *w, const double *dx, const double *nu, const double *f, double *out) 
+{ 
+  // w[2]:   Cell-center coordinates.
+  // dx[2]:  Cell spacing.
+  // nu[8]:  diffusion coefficient.
+  // f:      Input distribution function.
+  // out:    Incremented output 
+  double rdxF[1]; 
+  rdxF[0] = 4.0/(dx[0]*dx[0]); 
+
+  out[1] += 1.5*f[0]*rdxF[0]*nu[1]; 
+  out[3] += 1.5*rdxF[0]*nu[1]*f[2]; 
+
+  return (rdxF[0]*0.5*nu[0])*0.6666666666666666;
+
+} 
+double ConstDiffusionVarCoeffVol2xSerP1_diffDirs12(const double *w, const double *dx, const double *nu, const double *f, double *out) 
+{ 
+  // w[2]:   Cell-center coordinates.
+  // dx[2]:  Cell spacing.
+  // nu[8]:  diffusion coefficient.
+  // f:      Input distribution function.
+  // out:    Incremented output 
+  double rdxF[2]; 
+  rdxF[0] = 4.0/(dx[0]*dx[0]); 
+  rdxF[1] = 4.0/(dx[1]*dx[1]); 
+
+  out[1] += 1.5*f[0]*rdxF[0]*nu[1]; 
+  out[2] += 1.5*f[0]*rdxF[1]*nu[6]; 
+  out[3] += 1.5*f[1]*rdxF[1]*nu[6]+1.5*rdxF[0]*nu[1]*f[2]; 
+
+  return (rdxF[0]*0.5*nu[0]+rdxF[1]*0.5*nu[4])*0.6666666666666666;
+
+} 
+double ConstDiffusionVarCoeffVol2xSerP1_diffDirs2(const double *w, const double *dx, const double *nu, const double *f, double *out) 
+{ 
+  // w[2]:   Cell-center coordinates.
+  // dx[2]:  Cell spacing.
+  // nu[8]:  diffusion coefficient.
+  // f:      Input distribution function.
+  // out:    Incremented output 
+  double rdxF[1]; 
+  rdxF[0] = 4.0/(dx[1]*dx[1]); 
+
+  out[2] += 1.5*f[0]*rdxF[0]*nu[6]; 
+  out[3] += 1.5*rdxF[0]*f[1]*nu[6]; 
+
+  return (rdxF[0]*0.5*nu[4])*0.6666666666666666;
+
+} 
