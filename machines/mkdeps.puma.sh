6a6d6d15c86861937bb31b50f89e6e1faffe3b3f
Emily Lichko <elichko@r5u29n1.puma.hpc.arizona.edu>
2021-10-12 18:47:39 -0700

    Adding machine files for Arizona HPC cluster. Bit of a quirky build environment but seems to work.

---
+++ machines/mkdeps.puma.sh
@@ -0,0 +1,9 @@
+# if we are in machines directory, go up a directory
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
+export GKYLSOFT='/groups/kgklein/gkylsoft'
+cd install-deps
+# now build rest of packages
+./mkdeps.sh CC=icc CXX=icpc MPICC=mpicc MPICXX=mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes
