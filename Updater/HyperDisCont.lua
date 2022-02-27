f8287ec456eb49b1737f8d0430517ee93f0f0d0c
ammarhakim <a.hakim777@gmail.com>
2022-02-27 09:57:04 -0500

    Fixed declarations of the hyper_dg_advance call to reflect that range is passed by pointer and not value. Code otherwise remains unchanged.

--- Updater/HyperDisCont.lua
+++ Updater/HyperDisCont.lua
@@ -95,15 +95,13 @@ gkyl_hyper_dg* gkyl_hyper_dg_new(const struct gkyl_rect_grid *grid,
  * @param cflrate CFL scalar rate (frequency) array (units of 1/[T])
  * @param rhs RHS output
  */
-void gkyl_hyper_dg_advance(gkyl_hyper_dg *hdg, struct gkyl_range update_rng,
+void gkyl_hyper_dg_advance(gkyl_hyper_dg *hdg, const struct gkyl_range *update_rng,
   const struct gkyl_array *fIn, struct gkyl_array *cflrate, struct gkyl_array *rhs);
 
-void gkyl_hyper_dg_advance_cu(gkyl_hyper_dg* hdg, const struct gkyl_range update_range,
+void gkyl_hyper_dg_advance_cu(gkyl_hyper_dg* hdg, const struct gkyl_range *update_range,
   const struct gkyl_array* fIn, struct gkyl_array* cflrate,
   struct gkyl_array* rhs);
 
-void gkyl_hyper_dg_advance_no_iter(gkyl_hyper_dg *hdg, struct gkyl_range update_range,
-  const struct gkyl_array *fIn, struct gkyl_array *cflrate, struct gkyl_array *rhs);
 ]]
 
 -- Hyperbolic DG solver updater object
