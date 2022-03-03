a18983a2c931d66833912af358a0a228e7391217
John Rodman <jrodman@vt.edu>
2022-03-03 12:51:42 -0500

    Fixed incorrect neutral kernel calls

--- Eq/Vlasov.lua
+++ Eq/Vlasov.lua
@@ -63,7 +63,7 @@ function Vlasov:init(tbl)
    -- Check if we have an electric and magnetic field.
    local hasElcField    = xsys.pickBool(tbl.hasElectricField and tbl.hasElectricField or false, true)
    self._hasMagField    = xsys.pickBool(tbl.hasMagneticField and tbl.hasMagneticField or false, true)
-   local hasExtForce    = xsys.pickBool(tbl.hasExtForce, true)
+   self._hasExtForce    = xsys.pickBool(tbl.hasExtForce, true)
    self._plasmaMagField = xsys.pickBool(tbl.plasmaMagField and tbl.plasmaMagField or false, true)
 
    -- Option to perform only the force volume update (e.g. for stochastic forces).
@@ -71,7 +71,7 @@ function Vlasov:init(tbl)
 
    self._hasForceTerm = false   -- Flag to indicate if we have any force terms at all.
    -- Turn on force term if there's an electric or magnetic field or if there's a body force.
-   if (self._qbym ~= 0. and (hasElcField or self._hasMagField or self._plasmaMagField)) or hasExtForce then
+   if (self._qbym ~= 0. and (hasElcField or self._hasMagField or self._plasmaMagField)) or self._hasExtForce then
       self._hasForceTerm = true
    end
 
@@ -83,7 +83,7 @@ function Vlasov:init(tbl)
    -- default is "penalty," supported options: "penalty," "recovery"
    self._numVelFlux = tbl.numVelFlux and tbl.numVelFlux or "penalty"
    if self._hasForceTerm then
-      if self._qbym == 0. and hasExtForce then
+      if self._qbym == 0. and self._hasExtForce then
          self._volUpdate = VlasovModDecl.selectVolNeutral(
             self._phaseBasis:id(), self._cdim, self._vdim, self._phaseBasis:polyOrder())
          self._surfForceUpdate = VlasovModDecl.selectSurfNeutral(
@@ -172,7 +172,7 @@ function Vlasov:volTerm(w, dx, idx, q, out)
    local cflFreq = 0.0
    if self._hasForceTerm then
       self._emField:fill(self._emIdxr(idx), self._emPtr)   -- Get pointer to EM field.
-      if self._plasmaMagField then 
+      if self._plasmaMagField or (self._qbym == 0. and self._hasExtForce) then 
          cflFreq = self._volUpdate(w:data(), dx:data(), self._emPtr:data(), q:data(), out:data())
       else
          self._phiField:fill(self._phiIdxr(idx), self._phiPtr)   -- Get pointer to the electrostatic potential.
@@ -198,7 +198,7 @@ function Vlasov:surfTerm(dir, cfll, cflr, wl, wr, dxl, dxr, maxs, idxl, idxr, ql
       if self._hasForceTerm then
 	 -- Force term.
 	 self._emField:fill(self._emIdxr(idxl), self._emPtr)   -- Get pointer to EM field.
-         if self._plasmaMagField then 
+         if self._plasmaMagField or (self._qbym == 0. and self._hasExtForce) then 
 	    amax = self._surfForceUpdate[dir-self._cdim](
 	       wl:data(), wr:data(), dxl:data(), dxr:data(), maxs,
 	       self._emPtr:data(), ql:data(), qr:data(), outl:data(), outr:data())
