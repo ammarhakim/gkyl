ac769db287e70bc4b6e038cce55fceb80c398971
tnbernard <tess.bernard@gmail.com>
2022-05-03 15:04:20 -0700

    Correcting CFL condition in gen geo volume kernel. Checking in cas scripts.

--- Eq/vlasovData/VlasovGenGeoSer1x3vP1.cpp
+++ Eq/vlasovData/VlasovGenGeoSer1x3vP1.cpp
@@ -24,5 +24,5 @@ __host__ __device__ double VlasovGenGeoVol1x3vSerP1(const double *w, const doubl
   out[13] += (0.8660254037844386*(alpha0[5]*f[15]+f[5]*alpha0[15]+alpha0[2]*f[14]+f[2]*alpha0[14]+alpha0[1]*f[13]+f[1]*alpha0[13]+alpha0[11]*f[12]+f[11]*alpha0[12]+alpha0[0]*f[10]+f[0]*alpha0[10]+alpha0[7]*f[9]+f[7]*alpha0[9]+alpha0[6]*f[8]+f[6]*alpha0[8]+alpha0[3]*f[4]+f[3]*alpha0[4]))/dxv[0]; 
   out[15] += (0.8660254037844386*(alpha0[1]*f[15]+f[1]*alpha0[15]+alpha0[0]*f[14]+f[0]*alpha0[14]+alpha0[5]*f[13]+f[5]*alpha0[13]+alpha0[6]*f[12]+f[6]*alpha0[12]+alpha0[8]*f[11]+f[8]*alpha0[11]+alpha0[2]*f[10]+f[2]*alpha0[10]+alpha0[3]*f[9]+f[3]*alpha0[9]+alpha0[4]*f[7]+f[4]*alpha0[7]))/dxv[0]; 
 
-  return std::abs(w1Ddx0)+0.5*dv1Ddx0;
+  return alpha_mid;
 } 
