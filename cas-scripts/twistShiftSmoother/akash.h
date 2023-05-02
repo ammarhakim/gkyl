0aaa41272119c868c445f0e3c3bf112e015ba760
Akash Shukla <akashukla@utexas.edu>
2023-05-01 20:04:23 -0500

    new kernel for phi twist-shift smoothing

---
+++ cas-scripts/twistShiftSmoother/akash.h
@@ -0,0 +1,2 @@
+void twist_shift_smoother_bc_mod_lower_3x_Ser_p1(double *confField, double *confFieldGhost);
+void twist_shift_smoother_bc_mod_upper_3x_Ser_p1(double *confField, double *confFieldGhost);
