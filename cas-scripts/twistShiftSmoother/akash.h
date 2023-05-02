13246a7ce6fb4fbc0682d2b0955184bd37e93ba9
Akash Shukla <akashukla@utexas.edu>
2023-05-01 21:04:00 -0500

    more descriptive file names. Add maxima scripts to generate header and cpp files. Still in progress.

--- cas-scripts/twistShiftSmoother/akash.h
+++
@@ -1,2 +0,0 @@
-void twist_shift_smoother_bc_mod_lower_3x_Ser_p1(double *confField, double *confFieldGhost);
-void twist_shift_smoother_bc_mod_upper_3x_Ser_p1(double *confField, double *confFieldGhost);
