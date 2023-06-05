8183ef2d38cc9ab11ee3d72e61c1bfe4f9a3522a
Akash Shukla <akashukla@utexas.edu>
2023-06-05 17:03:31 -0500

    trying to add option to smooth with different BCs in different regions to femparproj. Some problem with subranges right now.

--- Updater/FemParproj.lua
+++ Updater/FemParproj.lua
@@ -15,6 +15,7 @@ local xsys = require "xsys"
 local Proto       = require "Lib.Proto"
 local UpdaterBase = require "Updater.Base"
 local DataStruct  = require "DataStruct"
+local Grid        = require "Grid"
 local ffi         = require "ffi"
 
 local ffiC = ffi.C
@@ -83,6 +84,12 @@ function FemParproj:init(tbl)
 
    self._isParPeriodic = tbl.periodicParallelDir
    assert(self._isParPeriodic ~= nil, "Updater.FemParproj: Must specify if parallel direction is periodic with 'periodicParallelDir'.")
+   self.xLCFS = tbl.xLCFS
+   self.doubleBC = false
+
+   if self._grid:ndim() ==3 and self._grid:numCells(2) == 1 and self.xLCFS then
+      self.doubleBC = true
+   end
 
    local weightFld  = tbl.weight
    local isWeighted = weightFld ~= nil
@@ -94,12 +101,92 @@ function FemParproj:init(tbl)
       weightFld:clear(0.0)
    end
 
-   self._zero = ffi.gc(ffiC.gkyl_fem_parproj_new(self._grid._zero, self._basis._zero, self._isParPeriodic,
+
+   if self.doubleBC then
+      self._zero = ffi.gc(ffiC.gkyl_fem_parproj_new(self._grid._zero, self._basis._zero, false,
                                                  isWeighted, weightFld._zero, GKYL_USE_GPU or 0),
                        ffiC.gkyl_fem_parproj_release)
+
+      self._zeroCore = ffi.gc(ffiC.gkyl_fem_parproj_new(self._grid._zero, self._basis._zero, true,
+                                                 isWeighted, weightFld._zero, GKYL_USE_GPU or 0),
+                       ffiC.gkyl_fem_parproj_release)
+      self._advance = function(tCurr, inFld, outFld) return self:_avanceDoubleBC(tCurr,inFld, outFld) end
+      self._advanceOnDevice = function(tCurr, inFld, outFld) return self:_advanceOnDeviceAxisymmetric(tCurr,inFld, outFld) end
+      self.inSOL = DataStruct.Field { -- May need to add some arguments to this ield creation
+         onGrid = self._grid,  numComponents = self._basis:numBasis(),
+         ghost  = {1,1},       useDevice = false,
+         metaData      = {polyOrder = self._basis:polyOrder(),
+                          basisType = self._basis:id(),},
+      }
+      self.outSOL = DataStruct.Field { -- May need to add some arguments to this ield creation
+         onGrid = self._grid,  numComponents = self._basis:numBasis(),
+         ghost  = {1,1},       useDevice = false,
+         metaData      = {polyOrder = self._basis:polyOrder(),
+                          basisType = self._basis:id(),},
+      }
+      self:setSplitRanges(self.inSOL)
+   else
+      self._zero = ffi.gc(ffiC.gkyl_fem_parproj_new(self._grid._zero, self._basis._zero, self._isParPeriodic,
+                                                 isWeighted, weightFld._zero, GKYL_USE_GPU or 0),
+                       ffiC.gkyl_fem_parproj_release)
+      self._advance = function(tCurr, inFld, outFld)
+         return self:advanceSingleBC(tCurr,inFld, outFld)
+      end
+      self._advanceOnDevice = function(tCurr, inFld, outFld) return self:_advanceOnDeviceSingleBC(tCurr,inFld, outFld) end
+   end
+
 end
 
-function FemParproj:_advance(tCurr, inFld, outFld)
+function FemParproj:setSplitRanges(sampleFld)
+   local gridRange = self._grid:globalRange()
+   local xLCFS=self.xLCFS
+   local coordLCFS = {xLCFS-1.e-7}
+   local idxLCFS    = {-9}
+   local xGridIngr = self._grid:childGrid({1})
+   local xGrid = Grid.RectCart {
+      lower = xGridIngr.lower,  periodicDirs  = xGridIngr.periodicDirs,
+      upper = xGridIngr.upper,  decomposition = xGridIngr.decomposition,
+      cells = xGridIngr.cells,
+   }
+   xGrid:findCell(coordLCFS, idxLCFS)
+   local globalCore = gridRange:shorten(1, idxLCFS[1])
+   local globalSOLTemp = gridRange:shortenFromBelow(1, self._grid:numCells(1)-idxLCFS[1]+1)
+   local lv, uv = globalSOLTemp:lowerAsVec(), globalSOLTemp:upperAsVec()
+   self.globalSOL = gridRange:subRange(lv,uv)
+
+   local localRange = sampleFld:localRange()
+   local localSOLRangeTemp = sampleFld:localRange():intersect(self.globalSOL)
+   local localCoreRangeTemp = sampleFld:localRange():intersect(globalCore)
+   local lv, uv = localSOLRangeTemp:lowerAsVec(), localSOLRangeTemp:upperAsVec()
+   self.localSOLRange = localRange:subRange(lv,uv)
+   local lv, uv = localCoreRangeTemp:lowerAsVec(), localCoreRangeTemp:upperAsVec()
+   self.localCoreRange = localRange:subRange(lv,uv)
+end
+
+function FemParproj:_avanceDoubleBC(tCurr, inFld, outFld)
+   local rhoIn = assert(inFld[1], "FemParproj.advance: Must-specify an input field")
+   local qOut  = assert(outFld[1], "FemParproj.advance: Must-specify an output field")
+
+   self.inSOL:copy(rhoIn)
+   self.inSOL:write('inSOL.bp', 0.0, 2, false)
+
+   ffiC.gkyl_fem_parproj_set_rhs(self._zero, self.inSOL._zero)
+   ffiC.gkyl_fem_parproj_solve(self._zero, self.outSOL._zero)
+   self.outSOL:write('outSOL.bp', 0.0, 2, false)
+
+   rhoIn:periodicCopyInDir(3)
+   rhoIn:write('rhoIn.bp', 0.0, 2, false)
+   ffiC.gkyl_fem_parproj_set_rhs(self._zeroCore, rhoIn._zero)
+   ffiC.gkyl_fem_parproj_solve(self._zeroCore, qOut._zero)
+   qOut:write('qOut.bp', 0.0, 2, false)
+
+   self.outSOL:write('outSOL2.bp', 0.0, 2, false)
+   qOut:copyRangeToRange(self.outSOL,  self.localSOLRange, self.localSOLRange)
+   qOut:write('qOut2.bp', 0.0, 2, false)
+end
+
+
+function FemParproj:advanceSingleBC(tCurr, inFld, outFld)
    local rhoIn = assert(inFld[1], "FemParproj.advance: Must-specify an input field")
    local qOut  = assert(outFld[1], "FemParproj.advance: Must-specify an output field")
 
@@ -109,7 +196,8 @@ function FemParproj:_advance(tCurr, inFld, outFld)
    ffiC.gkyl_fem_parproj_solve(self._zero, qOut._zero)
 end
 
-function FemParproj:_advanceOnDevice(tCurr, inFld, outFld)
+
+function FemParproj:_advanceOnDeviceSingleBC(tCurr, inFld, outFld)
    local rhoIn = assert(inFld[1], "FemParproj.advance: Must-specify an input field")
    local qOut  = assert(outFld[1], "FemParproj.advance: Must-specify an output field")
 
