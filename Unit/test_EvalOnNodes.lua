043e88503e6d5ee85629a223aaa644b184801e31
manauref <mfrancisquez@gmail.com>
2023-09-07 17:07:59 -0700

    Wrap pars of g0's proj_on_basis and eval_on_nodes in ProjOnBasis and EvalOnNodes, respectively. The intention was to remove Eigen from these objects, and keep g2 in line with g0. Add a couple more unit tests, which pass. gk-sheath 3x2v p1 reg test also looks identical, although some O(e-12) differences exist now. Unfortunately we can't yet totally remove Eigen (specifically the EvalOnNodesImpl files) because those nod2mod matrices are being used in TwistShift BCs. We can remove that dependency, but we'll do that later (gkylzero 4dee997bf656243a4be0c5e62f46b943f1c7974e).

--- Unit/test_EvalOnNodes.lua
+++ Unit/test_EvalOnNodes.lua
@@ -31,9 +31,7 @@ function test_1d_1()
    local evaluateOnNodes = Updater.EvalOnNodes {
       onGrid   = grid,
       basis    = basis,
-      evaluate = function (t, xn)
-	 return xn[1]
-      end
+      evaluate = function (t, xn) return xn[1] end
    }
 
    -- Do evaluation.
@@ -69,9 +67,7 @@ function test_1d_2()
    local evaluateOnNodes = Updater.EvalOnNodes {
       onGrid   = grid,
       basis    = basis,
-      evaluate = function (t, xn)
-	 return xn[1]^2
-      end
+      evaluate = function (t, xn) return xn[1]^2 end
    }
 
    -- Do evaluation.
@@ -88,6 +84,44 @@ function test_1d_2()
       assert_equal(1.414213562373095*xc[1]^2+0.007365695637359865, fItr[1], "Checking cell average")
       assert_equal(0.2041241452319315*xc[1], fItr[2], "Checking slope")
       --assert_equal(0.0065880784586841, fItr[3], "Checking second-moment " .. i)
+   end
+
+end
+
+function test_1d_3()
+   local grid = Grid.RectCart {
+      lower = {0.0},
+      upper = {1.0},
+      cells = {4},
+   }
+   local basis = Basis.CartModalSerendipity { ndim = 1, polyOrder = 2 }
+   local distf = DataStruct.Field {
+      onGrid        = grid,
+      numComponents = basis:numBasis()*2,
+      ghost         = {0, 0},
+   }
+   local evaluateOnNodes = Updater.EvalOnNodes {
+      onGrid   = grid,
+      basis    = basis,
+      evaluate = function (t, xn) return xn[1], xn[1]^2 end
+   }
+
+   -- Do evaluation.
+   evaluateOnNodes:advance(0.0, {}, {distf})
+
+   local idx     = Lin.IntVec(grid:ndim())
+   local xc      = Lin.Vec(1)
+   local indexer = distf:indexer()
+   -- Check evaluation.
+   for i = 1, grid:numCells(1) do
+      grid:setIndex( idx:setValues {i} )
+      grid:cellCenter(xc)
+      local fItr = distf:get(indexer(i))
+      assert_equal(xc[1], fItr[1]/math.sqrt(2), "Checking cell average")
+      assert_equal(0.1020620726159657, fItr[2], "Checking slope")
+      assert_equal(0.0, fItr[3], "Checking second-moment")
+      assert_equal(1.414213562373095*xc[1]^2+0.007365695637359865, fItr[4], "Checking cell average")
+      assert_equal(0.2041241452319315*xc[1], fItr[5], "Checking slope")
    end
 
 end
@@ -232,12 +266,53 @@ function test_3()
 
 end
 
+function test_3d()
+   local grid = Grid.RectCart {
+      lower = {0.0, 0.0, 0.0},
+      upper = {1.0, 1.0, 1.0},
+      cells = {4, 4, 4},
+   }
+   local basis = Basis.CartModalSerendipity { ndim = grid:ndim(), polyOrder = 2 }
+   local distf = DataStruct.Field {
+      onGrid        = grid,
+      numComponents = basis:numBasis(),
+      ghost         = {0, 0},
+   }
+   local evaluateOnNodes = Updater.EvalOnNodes {
+      onGrid   = grid,
+      basis    = basis,
+      evaluate = function(t, xn) return xn[1] end
+   }
+
+   -- Do evaluation.
+   evaluateOnNodes:advance(0.0, {}, {distf})
+
+   local idx     = Lin.IntVec(grid:ndim())
+   local xc      = Lin.Vec(grid:ndim())
+   local indexer = distf:indexer()
+   -- Check evaluation.
+   for i = 1, grid:numCells(1) do
+      for j = 1, grid:numCells(2) do
+         for k = 1, grid:numCells(3) do
+            grid:setIndex( idx:setValues {i, j, k} )
+            grid:cellCenter(xc)
+            local fItr = distf:get(indexer(i,j,k))
+            assert_equal(xc[1], fItr[1]/2^(3/2), "Checking cell average")
+            assert_equal(1., (2./0.25)*(math.sqrt(3)/2^(3/2))*fItr[2], "Checking slope")
+            assert_equal(0.0, fItr[3], "Checking second-moment")
+         end
+      end
+   end
+end
+
 -- Run tests.
 test_1d_1()
 test_1d_2()
+test_1d_3()
 test_2d()
 test_2d_2() 
 test_3()
+test_3d()
 
 if stats.fail > 0 then
    print(string.format("\nPASSED %d tests", stats.pass))
