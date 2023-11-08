a1e52090c3368b10776b2066e6cf7433833d005a
Manaure Francisquez <mfrancisquez@gmail.com>
2023-11-08 12:19:10 -0500

    Mod machine files to use adios2 (@ammarhakim will test)

--- machines/mkdeps.linux.sh
+++ machines/mkdeps.linux.sh
@@ -3,9 +3,9 @@ if [ `dirname "$0"` == "." ]
   then
     cd ..
 fi
-export GKYLSOFT='~/gkylsoft'
+export GKYLSOFT=$HOME/gkylsoft
 cd install-deps
 # first build OpenMPI
-./mkdeps.sh CC=gcc CXX=g++ --build-openmpi=yes
+./mkdeps.sh CC=gcc CXX=g++ --build-openmpi=yes --prefix=$GKYLSOFT
 # now build rest of packages
 ./mkdeps.sh CC=gcc CXX=g++ MPICC=$GKYLSOFT/openmpi-4.0.5/bin/mpicc MPICXX=$GKYLSOFT/openmpi-4.0.5/bin/mpicxx --build-luajit=yes --build-adios=yes --build-eigen=yes
