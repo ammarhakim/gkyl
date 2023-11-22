13b4914a17f0b2c191ee76b02808e910e2282654
ammarhakim <a.hakim777@gmail.com>
2023-11-22 10:46:16 -0500

    We need to repopulate our poor MOAT list. For now as the fluid moment app is disabled we can't run it

--- Regression/moat.lua
+++ Regression/moat.lua
@@ -2,9 +2,5 @@
 -- test. (MOAT : Mother of All Tests)
 return {
    "./vm-weibel/rt-weibel-1x2v-p1.lua",
-   "./gk-etg/rt-etg4d.lua",
-   "./gk-sheath/rt-gk-sheath.lua",
-   "./mom-gem-challenge/rt-5m-gem.lua",
-   "./hyper-euler/rt-euler-reim-2d.lua",
-   "./mom-axisymmetric/rt-euler-axisymmetric.lua",
+   "./gk-sheath/rt-gk-sheath-1x2v-p1.lua",
 }
