88f9013330cfa6e0e5dcdd248a4c97240cad9fbf
manauref <mfrancisquez@gmail.com>
2023-11-30 08:48:24 -0800

    Changing default gkylsoft location in some machine files. Could cause conflict with those in gkylzero.

--- machines/mkdeps.della-gpu.sh
+++ machines/mkdeps.della-gpu.sh
@@ -7,7 +7,7 @@ if [ `dirname "$0"` == "." ]
   then
     cd ..
 fi
-export GKYLSOFT=$HOME/gkylsoft-gpu
+export GKYLSOFT=$HOME/gkylsoft
 export MACHINE_NAME='della-gpu'
 cd install-deps
 ./mkdeps.sh CC=mpicc CXX=mpicxx MPICC=mpicc MPICXX=mpicxx --prefix=$GKYLSOFT --build-gkylzero=yes --build-luajit=yes --build-adios=yes --build-openmpi=no
